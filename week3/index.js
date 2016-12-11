9cdea0a2be3c9becbccc62fc0f7ecaf67a347458
Laurens Rietveld <laurens.rietveld@gmail.com>
2016-12-11 16:03:47 +0100

    added express boilerplate

--- week3/index.js
+++ week3/index.js
@@ -1,39 +1,15 @@
 var fs = require('fs');
-var http = require('http');
+var express = require('express')
+
+var app = express();
+
+app.get('/todos', function(req, res) {
+	res.send({label:'sometodo'})
+})
 
 var port = 8080;
-var server = http.createServer(handleRequest);
 
-function handleRequest (req, res) {
-	// console.log(req.url);
-	switch (req.url) {
-		case '/':
-			fs.readFile(__dirname + '/index.html', 'utf-8', function (error, data) {
-				if (error) {
-					res.writeHead(500);
-					res.write('Could not open file');
-				} else {
-					res.writeHead(200, {'Content-Type': 'text/html'});
-					res.write(data);
-				}
-				res.end();
-			});			
-			console.log('root');
-			break;
-		default :
-			console.log("url: " + req.url + ", method: " + req.method);
-			res.writeHead(200, {'Content-Type': 'application/json'});
-			// res.write('{"1":"make tea","2":"do dishes","3":"go shopping"}');
-			// res.write('{}');
-			res.end();
-	}
-}
-
-server.listen(port, function (error) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log('listening on port 8080');
-	}
-
-});
+app.listen(port, function(err) {
+	if (err) return console.error(err)
+	console.log('Listing to port', port)
+})
