cf314128d6435161f8d027a1016f3a00c11f5aca
AbdulRahmanDbes <abdalrahmandbes@mail.ru>
2017-03-15 20:45:10 +0100

    add 404 not found error

--- week1/index.js
+++ week1/index.js
@@ -3,28 +3,29 @@
 var http = require('http'),
     server = http.createServer(),
     state = 10,
-    print = (res, req, message) => {
+    print = (status, res, req, message) => {
         console.log(`Request ${req.url} State = ${state}`);
+        res.writeHead(status, {'content-type': 'text/html'});
         res.write(`<html><head><body>${message}</body></head></html>`);
     };
 
 // Create a event handler for "request"
 // this is an alternative way
 server.on('request', (request, response) => {
-    response.writeHead(200, {'content-type': 'text/html'});
+    
 
-    if (request.url === '/') print(response, request, '<h1>Welcome to my Homepage</h1>');
-    else if (request.url === '/state') print(response, request, `<h1>Current State : ${state}</h1>`);
+    if (request.url === '/') print(200, response, request, '<h1>Welcome to my Homepage</h1>');
+    else if (request.url === '/state') print(200, response, request, `<h1>Current State : ${state}</h1>`);
     else if (request.url === '/add') {
         state += 1;
-        print(response, request, '<h1>OK</h1>');
+        print(200, response, request, '<h1>OK</h1>');
     } else if (request.url === '/remove') {
         state -= 1;
-        print(response, request, '<h1>OK</h1>');
+        print(200, response, request, '<h1>OK</h1>');
     } else if (request.url === '/reset') {
         state = 10;
-        print(response, request, '<h1>OK</h1>');
-    } else print(response, request, `<img src="https://goo.gl/uu60ns" alt="Error 404: Page Not Found">`);
+        print(200, response, request, '<h1>OK</h1>');
+    } else print(404, response, request, `<img src="https://goo.gl/uu60ns" alt="Error 404: Page Not Found">`);
     
     response.end();
 });
