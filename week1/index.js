628516420668fe736a23ddf8b91f3840b9893e37
massoodbarsa <amir.shakiba1980@gmail.com>
2017-05-31 21:56:30 +0200

    week1

--- week1/index.js
+++ week1/index.js
@@ -1,25 +1,45 @@
-// To run this file: node index.js
-// Then in your browser: http://localhost:8080
 var http = require('http');
 
-var port = 8080;
-
+var state = 10
 var server = http.createServer();
+const port = 8888;
 
-// Start the HTTP server, start listening for requests
 server.listen(port, function(error) {
   if (error) {
     console.log(error);
   } else {
-    console.log('api listening on port', port);
+    console.log(`go to http://localhost: ${port}`);
   }
 });
-
 // Create a event handler for "request"
 // this is an alternative way
 server.on('request', function(request, response) {
   console.log('New http request received', request.url);
+  if (request.url == '/state')
+    console.log('state is ' + state);
+
+  else if (request.url == '/add') {
+    state++
+    console.log('state is ' + state);
+
+  } else if (request.url == '/remove') {
+    state--
+    console.log('state is ' + state);
+
+  } else if (request.url == '/reset') {
+    state = 10
+    console.log(state);
+
+  } else return console.log('error code 404: Not found ');
   response.setHeader('content-type', 'text/html');
-  response.write('<html><head></head><body><h1>Hello world</h1></body></html>');
+  response.write(`
+    <html>
+      <head>
+      </head>
+      <body>
+        <h1 style="color:blue;background-color:powderblue;text-align:center;marfin-top:40px;padding:10px">
+        Current state is : ${state}</h1>
+      </body>
+    </html>`);
   response.end();
 });
