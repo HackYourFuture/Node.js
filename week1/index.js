84436f82019237fffa1d380def61ddd991ce8aab
nos111 <nsaffour@gmail.com>
2017-06-13 17:01:09 +0200

    completed assignment week1

--- week1/index.js
+++ week1/index.js
@@ -1,10 +1,71 @@
-// To run this file: node index.js
-// Then in your browser: http://localhost:8080
-var http = require('http');
+// Declare global variables
+const http = require('http');
+//start server 
+const server = http.createServer();
+// the state which will be increased or decreased on the user request
+let state = 10; 
+//This port was already chosen by the assignment maker.
+const port = 8080;
+//Object containing the respons text and the content type.
+let responsToSendBack = {
+    code:'<html></html>',
+    type:'text/html',
+    statusCode: 200
+};
 
-var port = 8080;
 
-var server = http.createServer();
+// Function that checks which URL input we got from the user 
+//Initiates the right action based on the user choice
+function checkInput (url) {
+    switch (url) {
+        case '/state':
+            stateResponsHtml();
+            break;
+        case '/add':
+            addOne();
+            operationsResponseHtml();
+            console.log(state);
+            break;
+        case '/remove':
+            subOne();
+            operationsResponseHtml();
+            console.log(state);
+            break;
+        case '/reset':
+            state = 10;
+            operationsResponseHtml();
+            break;
+        default:
+            otherUrls();
+    }
+}
+
+//Function to increase the state by 1
+function addOne (){
+    return state++;
+};
+//Function to decrease the state
+function subOne () {
+    return state--;
+};
+
+//Function to build the response HTML for subtraction and addition
+function operationsResponseHtml (){
+    responsToSendBack.code = '<html><body><h2>ok</h2></body></html>'
+    responsToSendBack.statusCode = 200;
+};
+
+//function to build state html respons
+function stateResponsHtml () {
+    responsToSendBack.code = '<html><body><h2>the current state is ' + state + '</h2></body></html>';
+    responsToSendBack.statusCode = 200;
+}
+//function to build HTML response for any other URL that doesn't exist
+function otherUrls () {
+    responsToSendBack.code = 
+        '<html><body><h2>Apparently, the page you are looking for doesn"t exist :)</h2></body></html>';
+    responsToSendBack.statusCode = 404;
+};
 
 // Start the HTTP server, start listening for requests
 server.listen(port, function(error) {
@@ -16,10 +77,11 @@ server.listen(port, function(error) {
 });
 
 // Create a event handler for "request"
-// this is an alternative way
 server.on('request', function(request, response) {
-  console.log('New http request received', request.url);
-  response.setHeader('content-type', 'text/html');
-  response.write('<html><head></head><body><h1>Hello world</h1></body></html>');
-  response.end();
+    //send the URL to be checked for the prober response
+    checkInput(request.url);
+    response.statusCode = responsToSendBack.statusCode;
+    response.setHeader('content-type', responsToSendBack.type);
+    response.write(responsToSendBack.code);
+    response.end();
 });
