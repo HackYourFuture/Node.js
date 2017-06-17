d3aa8b9e2bc405984e3f03dd49e929119d3f4fb9
gmamar <gmamar1@hotmail.com>
2017-06-17 02:57:26 +0200

    the code is working good

--- week1/index.js
+++ week1/index.js
@@ -1,27 +1,17 @@
-// To run this file: node index.js
 // Then in your browser: http://localhost:8000
 let http = require('http');
 
 //I added this package to deal with files.
 let fs = require('fs');
 
-//I think this not the best way to add style file, but it a good example to use 'fs' pakage.
-var style = fs.readFileSync("style.css")
+//I think this is not the best way to add style file, but it a good example to use 'fs' pakage.
+let style = fs.readFileSync("style.css")
 
 // This is the magic number :-)
 const port = 8000;
-
-let status = 10;
-
-//Define all my page content as object.
-var htmlObject = {
-
-	title: "<title>hallo NodeJs</title>",
-	acswerOk: "Your request is return: OK"
-
-}
-
-var server = http.createServer();
+let state = 10;
+let server = http.createServer();
+let info;
 
 // Start the HTTP server, start listening for requests
 server.listen(port, function (error) {
@@ -32,17 +22,40 @@ server.listen(port, function (error) {
 	}
 });
 
-// Create a event handler for "request"
+// Create a event handler for "request" and calling render function. 
 
 server.on('request', function (request, response) {
 	response.setHeader('content-type', 'text/html');
 	switch (request.url) {
+		case '/':
+			info = 'Create HTML server';
+			renderHtml();
+			break;
 		case '/add':
-			response.write('<html>' + htmlObject.title + style + '<head></head><body><h1>Hello world</h1>' + '<h1>' + ++status + '</h1>' + '</body></html>');
+			state++;
+			info = 'Add one to counter';
+			renderHtml();
+			break;
+		case '/remove':
+			state--;
+			info = 'Substract one from counter';
+			renderHtml();
+			break;
+		case '/reset':
+			state = 10;
+			info = 'Reset counter';
+			renderHtml();
 			break;
 		default:
-			response.write('<html>' + htmlObject.title + style + '<head></head><body><h1>Hello world</h1></body></html>' + '<h1 id="counter">' + status + '</h1>');
+			info = 'Error code 404: Not found';
+			renderHtml();
 
+
+	}
+//this function is responsible to show HTML.
+	function renderHtml() {	
+		response.write('<html><title>Hallo NodeJs</title>' + style + '<head></head><body><h1>' + info + 
+					   '</h1><h1 id="counter">' + state + '</h1></body></html>');
 	}
 	response.end();
 });
