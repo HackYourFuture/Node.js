f9dad37309ce3637006a932f7b48216d937a1c99
nos111 <nsaffour@gmail.com>
2017-06-18 23:04:41 +0200

    implemented remove task function

--- week2/index.js
+++ week2/index.js
@@ -29,7 +29,7 @@ switch (command) {
     //if the user enter the command remove, followed by the item number, the item will be removed.
     //the functions removeTodoItem will remove the item with the number the user entered
     case 'remove':
-        //removeTodoItem();
+        removeTodoItem(todosFile,options);
         break;
     //when the user asks for list, he should get the list of todos he has
     //the list should be numbered with human numbers
@@ -56,24 +56,44 @@ function showHelp() {
     console.log(helpText)
 }
 
+//add an item to the todo list
+//this functions takes the todoFile json and the commands array as parameters. 
+//It adds the new items to it and at the end it will write it the new object to the Todo file
+//this way we can save our new todos and keep our old ones
 function addTodoItem(jsonObject,commands) {
     //assign the task text to a variable.
     let itemToAdd = commands[1];
     //check if the user has entered text or not
-    if(itemToAdd.length < 1){
+    if(itemToAdd === undefined || itemToAdd.length < 1){
         console.log('Please enter a name for your task')
     }else{
         //push the text the user entered to our data file
         jsonObject.tasks.push(itemToAdd);
         //write the modified object to the todo File
-        console.log(jsonObject.tasks)
-        jsonObject = JSON.stringify(jsonObject)
-        return fs.writeFile('todo.json',jsonObject)
-        
+        console.log(jsonObject.tasks);
+        jsonObject = JSON.stringify(jsonObject);
+        return fs.writeFile('todo.json',jsonObject);
     }
-    
 }
 
+//removes an item from the todo list
+//takes the todosFile json and the commands array as parameters.
+//updates the todofile at the end when it's done removing the tasks
+function removeTodoItem (jsonObject,commands) {
+    //assign the task number to a variable
+    let itemToRemove = commands[1];
+    //checkc if the task number exists and makes sense
+    if(itemToRemove > jsonObject.tasks.length + 1) {
+        console.log('This item does not exist');
+    }else{
+        //remove the task from the jsonObject
+        jsonObject.tasks.splice(itemToRemove - 1,1)
+        console.log(jsonObject)
+        //write the changed object to our Todofile to save the changes
+        jsonObject = JSON.stringify(jsonObject);
+        return fs.writeFile('todo.json',jsonObject);
+    }
+}
 function splitStringByNewline(string) {
   return string.split('\n').filter(function(element) {
     element = element.trim();
