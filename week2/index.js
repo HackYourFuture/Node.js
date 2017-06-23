f9b4f067d7c90660fec2d851342020307145e945
gmamar <gmamar1@hotmail.com>
2017-06-23 03:31:21 +0200

    add line and delete a line

--- week2/index.js
+++ week2/index.js
@@ -5,61 +5,102 @@ var options = process.argv.slice(2);
 var command = options[0];
 
 switch (command) {
-  case 'help':
-  default:
-    showHelp();
-    break;
-  case 'list':
-    listTodos();
-    break;
+	case 'help':
+	default:
+		showHelp();
+		break;
+	case 'list':
+		listTodos();
+		break;
+	case 'add':
+		addLine();
+		break;
+	case 'delete':
+		deleteLine();
+		break;
 }
 
 function splitStringByNewline(string) {
-  return string.split('\n').filter(function(element) {
-    element = element.trim();
-    return element.length > 0;
-  });
+	return string.split('\n').filter(function (element) {
+		element = element.trim();
+		return element.length > 0;
+	});
 }
 
 function showHelp() {
-  openFile('help.txt', function(error, data) {
-    if (error) {
-      return console.log('Error: the help file could not be displayed', error);
-    }
-    console.log(data);
-  });
+	openFile('help.txt', function (error, data) {
+		if (error) {
+			return console.log('Error: the help file could not be displayed', error);
+		}
+		console.log(data);
+	});
 }
 
 function listTodos() {
-  openFile('todo.txt', function(error, data) {
-    if (error) {
-      if (error.code === 'ENOENT') {
-        return console.log('Nothing to do! (or your dog ate your todo list)');
-      } else {
-        return console.log('Error: Something went wrong', error);
-      }
-    }
+	openFile('todo.txt', function (error, data) {
+		if (error) {
+			if (error.code === 'ENOENT') {
+				return console.log('Nothing to do! (or your dog ate your todo list)');
+			} else {
+				return console.log('Error: Something went wrong', error);
+			}
+		}
 
-    var todos = splitStringByNewline(data);
+		var todos = splitStringByNewline(data);
 
-    if (todos.length === 0) {
-      return console.log('Nothing to do!')
-    }
+		if (todos.length === 0) {
+			return console.log('Nothing to do!')
+		}
 
-    console.log('Your todo list looks like this');
-    todos.forEach(function(element, index) {
-      index = (index + 1).toString();
-      console.log(index, element);
-    });
+		console.log('Your todo list looks like this');
+		todos.forEach(function (element, index) {
+			index = (index + 1).toString();
+			console.log(index, element);
+		});
 
-    if (todos.length > 5) {
-      console.log('You have too much to do!');
-    }
-  });
+		if (todos.length > 5) {
+			console.log('You have too much to do!');
+		}
+	});
 }
 
+
+function addLine() {
+
+	options.shift();
+	var allLine = options.join(' ');
+
+	fs.appendFileSync('todo.txt', allLine + "\n", 'utf8', function (error) {
+
+		console.log('Done!');
+	});
+}
+
+function deleteLine() {
+
+	if (options[1] <= 0) {
+		console.log('Please enter right line number!!! '+ '\n');
+		showHelp();
+	} else {
+		var lineNumber = options[1] - 1;
+		fs.readFile('todo.txt', 'utf8', function (error, data) {
+			if (error) {
+				console.log(error)
+			}
+			var dataRendering = data.split('\n');
+			dataRendering.splice(lineNumber, 1);
+			var newLines = dataRendering.join('\n');
+			fs.writeFile('todo.txt', newLines);
+			console.log ('The line is deleted!');
+			listTodos();
+
+		});
+	}
+}
+
+
 function openFile(fileName, callback) {
-  fs.readFile(__dirname + '/' + fileName, 'utf8', function(error, data) {
-    callback(error, data)
-  });
+	fs.readFile(__dirname + '/' + fileName, 'utf8', function (error, data) {
+		callback(error, data);
+	});
 }
