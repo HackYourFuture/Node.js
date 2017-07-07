45b0e3c7608df7ce544a13bd1aa13dd8a01373e8
Ahalhowidi <ah.alhowidi@gmail.com>
2017-07-07 14:03:19 +0200

    I did the homework but didnt test it with the postman

--- class7-week3/index.js
+++ class7-week3/index.js
@@ -5,11 +5,16 @@ const app = Express()
 
 app.use(bodyParser.json())
 
-const {list, create, update, remove} = require('./actions')
+const {list, create, update, remove, clear,markAsDone, markAsNotDone} = require('./actions')
 
 app.get('/todos', list)
 app.post('/todos', create)
 app.put('/todos/:id', update)
 app.delete('/todos/:id', remove)
+app.delete('/todos', clear)
+app.post('/todos/:id/done', markAsDone)
+app.delete('/todos/:id/done', markAsNotDone)
 
-app.listen(3000)
+let port = 3000;
+app.listen(port)
+console.log("listen to port" + port);
