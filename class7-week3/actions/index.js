6ca261af53e086af51d5b03586c957c94121282a
essamalsaloum <es.alsaloum@gmail.com>
2017-07-08 22:17:53 +0200

    Homework3 is finshed

--- class7-week3/actions/index.js
+++ class7-week3/actions/index.js
@@ -1,6 +1,9 @@
 module.exports = {
-  list:   require('./list'),
-  create: require('./create'),
-  update: require('./update'),
-  remove: require('./remove')
+	list:   require('./list'),
+	create: require('./create'),
+	update: require('./update'),
+	remove: require('./remove'),
+	clean: require('./clean'),
+	markAsDone: require('./markAsDone'),
+	markAsNotDone: require('./markAsNotDone')
 }
