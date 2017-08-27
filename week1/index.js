c589df4404257b0256f36884ed45df66cb731435
Joost Lubach <joostlubach@gmail.com>
2017-08-27 13:45:27 +0200

    Basic webserver

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,24 @@
-console.log("Hello world")
+import HTTP from 'http'
+
+import sendIndexHTML from './responses/sendIndexHTML'
+import sendStylesCSS from './responses/sendStylesCSS'
+import sendText from './responses/sendText'
+
+const server = HTTP.createServer((request, response) => {
+	console.log(request.method, request.url)
+
+	if (request.url === '/') {
+		sendIndexHTML(response)
+	} else if (request.url === '/styles.css') {
+		sendStylesCSS(response)
+	} else {
+		response.statusCode = 404
+		sendText(response, 'This page cannot be found')
+	}
+	
+	response.end()
+})
+
+server.listen(3001)
+
+console.log('Server started')
