7d8fb3478244a87cf51101f343d6dea6671e9710
2sma3il1991juve <ismail.abuserea@gmail.com>
2017-09-01 12:40:25 +0200

    node homework 1

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,34 @@
-console.log("Hello world")
+import HTTP from 'http'
+import Path from 'path'
+import stateHTML from './responses/stateHTML'
+import addHTML from './responses/addHTML'
+import removeHTML from './responses/removeHTML'
+import resetHTML from './responses/resetHTML'
+import notFound from './responses/notFound'
+
+
+var state = 10;
+
+const server = HTTP.createServer((request, response) => {
+    console.log(request.method, request.url)
+    switch (request.url) {
+        case '/state':
+            stateHTML(response, state)
+            break
+        case '/add':
+            addHTML(response, state)
+            break
+        case '/remove':
+            removeHTML(response, state)
+            break
+        case '/reset':
+            resetHTML(response, state)
+            break
+        default:
+            response.statusCode = 404
+            notFound(response, "File not found")
+            break
+    }
+    response.end()
+})
+server.listen(8080)
