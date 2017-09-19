62554b92bceb9ecdbf1b94a78fb732b8c0bf6576
mrbarri <kbtech@hotmail.com>
2017-09-20 00:54:17 +0200

    week1 nodeJS

--- week1/index.js
+++ week1/index.js
@@ -2,37 +2,38 @@ import HTTP from 'http'
 import Path from 'path'
 
 import sendIndexHTML from './responses/sendIndexHTML'
-import sendPage2HTML from './responses/sendPage2HTML'
-import sendStylesCSS from './responses/sendStylesCSS'
 import sendText from './responses/sendText'
+var state = 10;
 
 const server = HTTP.createServer((request, response) => {
-	console.log(request.method, request.url)
+	console.log(response.url)
+
+	
 
 	switch (request.url) {
-	case '/':
-		sendIndexHTML(response)
-		break
-	case '/page2':
-		sendPage2HTML(response)
-		break
-	case '/styles.css':
-		sendStylesCSS(response)
-		break
-	default:
-		const extension = Path.extname(request.url)
-		if (extension === '') {
-			response.statusCode = 302
-			response.setHeader('Location', '/')
-		} else {
-			response.statusCode = 404
-			sendText(response, "File not found")
-		}
+		case '/state':
+			sendIndexHTML(response)
+			break
+		case '/add':
+			state = 1 + 1
+			sendIndexHTML(response)
+			break
+		case '/remove':
+			state = 1 - 1
+			sendIndexHTML(response)
+			break
+		case '/reset':
+			state = 10
+			sendIndexHTML(response)
+			break
+		default:
+			response.state = 404
+			sendText(response, 'error')
 	}
-	
 	response.end()
 })
 
-server.listen(3001)
+server.listen(8080)
 
 console.log('Server started')
+
