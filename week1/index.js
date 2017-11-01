d396d38caf94b4df710b0adf25b21f060a54536a
pesh12 <ekocibar@hotmail.com>
2017-11-01 11:24:28 +0100

    project completed

--- week1/index.js
+++ week1/index.js
@@ -1,38 +1,44 @@
-import HTTP from 'http'
-import Path from 'path'
+//import HTTP from 'http'
+//import Path from 'path'
+//
+//import sendIndexHTML from './responses/sendIndexHTML'
+//import sendPage2HTML from './responses/sendPage2HTML'
+//import sendStylesCSS from './responses/sendStylesCSS'
+//import sendText from './responses/sendText'
 
-import sendIndexHTML from './responses/sendIndexHTML'
-import sendPage2HTML from './responses/sendPage2HTML'
-import sendStylesCSS from './responses/sendStylesCSS'
-import sendText from './responses/sendText'
+const http = require('http');
+const port = 3000
+let state = 10;
 
-const server = HTTP.createServer((request, response) => {
-	console.log(request.method, request.url)
 
-	switch (request.url) {
-	case '/':
-		sendIndexHTML(response)
-		break
-	case '/page2':
-		sendPage2HTML(response)
-		break
-	case '/styles.css':
-		sendStylesCSS(response)
-		break
-	default:
-		const extension = Path.extname(request.url)
-		if (extension === '') {
-			response.statusCode = 302
-			response.setHeader('Location', '/')
-		} else {
-			response.statusCode = 404
-			sendText(response, "File not found")
-		}
-	}
-	
-	response.end()
-})
+const server = http.createServer( (request, response) => {
 
-server.listen(3001)
+    console.log(`I have received a request for path: ${ request.url }`);
 
-console.log('Server started')
+    
+    
+    if ( request.url === "/" ) {
+        response.write("You requested root path.");
+    } else if ( request.url === "/state" ) {
+        response.write(`<h1>${state}</h1>`);
+         
+    }else if ( request.url === "/add" ) {
+        state++;
+        response.write(`<h1>${state}</h1>`);
+    }else if ( request.url === "/remove" ) {
+        state--;
+        response.write(`<h1>${state}</h1>`);
+    }else if ( request.url === "/reset" ) {
+        state = 10;
+        response.write(`<h1>${state}</h1>`);
+    }else {
+        response.write(`return error code 404: Not found with a friendly message`)        
+    }
+
+    response.end();
+});
+
+
+server.listen(port, () => {
+    console.log(`The nodejs server is now listening on port ${ port }`)
+});
