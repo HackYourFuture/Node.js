c2a172f15bd6320dc128f581c9be5c2f5ce834c6
Elias011 <elias.shkshb1991@gmail.com>
2017-11-01 01:41:19 +0100

    Update index.js
    Added homework week1

--- week1/index.js
+++ week1/index.js
@@ -1,38 +1,30 @@
-import HTTP from 'http'
-import Path from 'path'
-
-import sendIndexHTML from './responses/sendIndexHTML'
-import sendPage2HTML from './responses/sendPage2HTML'
-import sendStylesCSS from './responses/sendStylesCSS'
-import sendText from './responses/sendText'
-
-const server = HTTP.createServer((request, response) => {
-	console.log(request.method, request.url)
-
-	switch (request.url) {
-	case '/':
-		sendIndexHTML(response)
-		break
-	case '/page2':
-		sendPage2HTML(response)
-		break
-	case '/styles.css':
-		sendStylesCSS(response)
-		break
-	default:
-		const extension = Path.extname(request.url)
-		if (extension === '') {
-			response.statusCode = 302
-			response.setHeader('Location', '/')
-		} else {
-			response.statusCode = 404
-			sendText(response, "File not found")
-		}
-	}
+const http = require('http');
+const port = 3000;
+let state = 10;
+let server = http.createServer((request, response)=>{
 	
-	response.end()
-})
+	
+	if (request.url === "/add"){
+		state +=1
+		response.write(`<h1>The state is : </h1><h2>${state}</h2>`)
 
-server.listen(3001)
-
-console.log('Server started')
+	}else if (request.url === "/remove"){
+		state -=1
+		response.write(`<h1>The state is : </h1><h2>${state}</h2>`)
+	
+	}else if (request.url === "/reset"){
+		state = 10
+		response.write(`<h1>The state is : </h1><h2>${state}</h2>`)
+		
+	}else if (request.url === "/state"){
+		response.write(`<h1>The state is :</h1><h2>${state}</h2>`)
+		
+	}else{
+		response.write(`<h1> Error 404 Invalid Request!</h1>`)
+		
+	}
+	response.end();
+});
+server.listen(port, () => {
+     console.log(`Node.js web server at port ${ port } is running..`)
+});
