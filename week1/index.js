3054a83834086bb35eaa02b89128b0bd071d0075
Elias011 <elias.shkshb1991@gmail.com>
2017-11-02 20:30:12 +0100

    Update index.js
    I added more spaces to make the code easy to read 
    I added Content-Type header and full html for every response
    and I changed the error returned from Invalid Request to Not Found

--- week1/index.js
+++ week1/index.js
@@ -1,29 +1,84 @@
 const http = require('http');
 const port = 3000;
 let state = 10;
-let server = http.createServer((request, response)=>{
+let server = http.createServer( (request, response) => {
 	
 	
-	if (request.url === "/add"){
-		state +=1
-		response.write(`<h1>The state is : </h1><h2>${state}</h2>`)
+	if (request.url === "/add") {
+		state += 1
+		 response.writeHead(200, {"Content-Type":"text/html"});
+	response.end(`
+<!DOCTYPE html>
+<html>
+<head>
+<title>Html server</title>
+</head>
+<body>
+<h1>The state is : </h1><h2>${state}</h2>
+</body>
+`)
+	//	response.write(`<h1>The state is : </h1><h2>${state}</h2>`)
 
-	}else if (request.url === "/remove"){
-		state -=1
-		response.write(`<h1>The state is : </h1><h2>${state}</h2>`)
+	}else if (request.url === "/remove") {
+		state -= 1
+		 response.writeHead(200, {"Content-Type":"text/html"});
+	response.end(`
+<!DOCTYPE html>
+<html>
+<head>
+<title>Html server</title>
+</head>
+<body>
+<h1>The state is : </h1><h2>${state}</h2>
+</body>
+`)
+		//response.write(`<h1>The state is : </h1><h2>${state}</h2>`)
 	
-	}else if (request.url === "/reset"){
+	}else if (request.url === "/reset") {
 		state = 10
-		response.write(`<h1>The state is : </h1><h2>${state}</h2>`)
-		
-	}else if (request.url === "/state"){
-		response.write(`<h1>The state is :</h1><h2>${state}</h2>`)
+		 response.writeHead(200, {"Content-Type":"text/html"});
+	response.end(`
+<!DOCTYPE html>
+<html>
+<head>
+<title>Html server</title>
+</head>
+<body>
+<h1>The state is : </h1><h2>${state}</h2>
+</body>
+`)
+		//response.write(`<h1>The state is : </h1><h2>${state}</h2>`)
+		 
+	}else if (request.url === "/state") {
+		 response.writeHead(200, {"Content-Type":"text/html"});
+	response.end(`
+<!DOCTYPE html>
+<html>
+<head>
+<title>Html server</title>
+</head>
+<body>
+<h1>The state is : </h1><h2>${state}</h2>
+</body>
+`)
+		//response.write(`<h1>The state is :</h1><h2>${state}</h2>`)
 		
 	}else{
-		response.write(`<h1> Error 404 Invalid Request!</h1>`)
+		 response.writeHead(200, {"Content-Type":"text/html"});
+	response.end(`
+<!DOCTYPE html>
+<html>
+<head>
+<title>Html server</title>
+</head>
+<body>
+<h1> Error 404 Not Found!</h1>
+</body>
+`)
+	//	response.write(`<h1> Error 404 Invalid Request!</h1>`)
 		
 	}
-	response.end();
+	
 });
 server.listen(port, () => {
      console.log(`Node.js web server at port ${ port } is running..`)
