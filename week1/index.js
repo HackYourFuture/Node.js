409814392a18a0e0db96ec04fcd29878e210bedd
talalalamdar <talalalamdar@gmail.com>
2018-01-17 22:41:28 +0100

    submitting the homework

--- week1/index.js
+++ week1/index.js
@@ -1,38 +1,31 @@
-import HTTP from 'http'
-import Path from 'path'
 
-import sendIndexHTML from './responses/sendIndexHTML'
-import sendPage2HTML from './responses/sendPage2HTML'
-import sendStylesCSS from './responses/sendStylesCSS'
-import sendText from './responses/sendText'
+"use strict"
 
-const server = HTTP.createServer((request, response) => {
-	console.log(request.method, request.url)
 
-	switch (request.url) {
-	case '/':
-		sendIndexHTML(response)
-		break
-	case '/page2':
-		sendPage2HTML(response)
-		break
-	case '/styles.css':
-		sendStylesCSS(response)
-		break
-	default:
-		const extension = Path.extname(request.url)
-		if (extension === '') {
-			response.statusCode = 302
-			response.setHeader('Location', '/')
-		} else {
-			response.statusCode = 404
-			sendText(response, "File not found")
-		}
-	}
-	
-	response.end()
+let state = 10;
+let http = require("http");
+let server = http.createServer();
+
+server.on("connection", () => {
+    console.log("server is connected")
 })
 
-server.listen(3001)
-
-console.log('Server started')
+server.on("request", (request, response) => {
+    console.log("requesting", request.url)
+    function setResponse(header) {
+        response.setHeader("Content-Type", "Text-html");
+        response.write(header);
+        response.end();
+    }
+    switch (request.url) {
+        case "/add" : state++; setResponse(`<h1>${state}</h1>`); break;
+        case "/remove": state--; setResponse(`<h1>${state}</h1>`); break;
+        case "/reset": state = 10; setResponse(`<h1>${state}</h1>`); break;
+        case "/state": state; setResponse(`<h1>${state}</h1>`); break;
+        default: setResponse(`<h1>Error 404, page not found</h1>`); break;    
+    }
+})
+const port = 8080;
+server.listen(port, () => {
+    console.log("Listening on", port)
+})
