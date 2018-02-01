514bc7502396fd3ee125a96036766538fe58fa72
chileluk <32581924+chileluk@users.noreply.github.com>
2018-02-01 09:37:32 -0500

    add week3 express homework

--- week3/index.js
+++ week3/index.js
@@ -1,15 +1,119 @@
-const Express = require('express')
-const bodyParser = require('body-parser')
+"use strict";
 
-const app = Express()
+const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.json())
+const app = express();
+const todosFile = "todos.json";
+const PORT = 4000;
 
-const {list, create, update, remove} = require('./actions')
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
 
-app.get('/todos', list)
-app.post('/todos', create)
-app.put('/todos/:id', update)
-app.delete('/todos/:id', remove)
 
-app.listen(3000)
+
+function handleRequest() {
+
+    app.use(bodyParser.json());
+
+
+    app.get("/list", (req, res) => {
+        readTodosFile().then(data => {
+            if (!data || data === "[]") res.send("There are no items on the list");
+  
+            res.send(JSON.parse(data));
+
+        }).catch(err => console.error(err));   
+    });
+
+
+    app.post("/add", (req, res) => {
+
+        const newTodo = req.body.newTodo;
+
+        readTodosFile().then(data => {
+            const list = JSON.parse(data);
+            list.push(newTodo);
+            writeTodosFile(JSON.stringify(list));
+            return list;   
+        }).then(list => {
+            res.send(list);
+        }).catch (err => console.error(err)); 
+    });
+
+
+    app.put("/remove", (req, res) => {
+
+        const index = req.body.number - 1;
+
+        readTodosFile().then(data => {
+            const list = JSON.parse(data);
+            list.splice(index, 1);
+            writeTodosFile(JSON.stringify(list));
+            return list;   
+        }).then(list => {
+            res.send(list);
+        }).catch(err => console.error(err)); 
+    });
+
+
+    app.put("/update", (req, res) => {
+
+        const index = req.body.number - 1;
+        const newTodo = req.body.newTodo;
+        
+        readTodosFile().then(data => {
+            const list = JSON.parse(data);
+            list.splice(index, 1, newTodo);
+            writeTodosFile(JSON.stringify(list));
+            return list;
+        }).then(list => {
+            res.send(list);
+        }).catch(err => console.error(err)); 
+    });
+
+
+    app.delete("/reset", (req, res) => {
+        readTodosFile().then(() => {
+            const list = [];
+            writeTodosFile(JSON.stringify(list));
+        }).then(list => {
+            res.send("All items have been deleted from list");
+        }).catch(err => console.error(err)); 
+    });
+
+}   
+
+handleRequest();
+
+
+
+function readTodosFile() {
+
+    return new Promise((resolve, reject) => {
+        fs.readFile(todosFile, "utf8", (err, data) => {
+
+            if (err) {
+                reject(err);
+            }
+
+            resolve(data);
+        });
+    });    
+}
+
+
+function writeTodosFile(todosList) {
+
+    fs.writeFile(todosFile, todosList, (err) => {
+        if (err) {
+            console.error(err);
+        }
+
+        console.log("Successfully wrote to file");
+    });   
+}
+
+
