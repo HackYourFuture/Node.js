0eb8f7eb54104bd684254402e3df79f2ba30a7e3
Kemirdin <qemirdin@gmail.com>
2018-02-27 22:04:33 +0100

    update file

---
+++ week1/homework/index.js
@@ -0,0 +1,37 @@
+'use strict';
+let state = 10;
+let http = require('http');
+let server = http.createServer();
+
+server.on('connection', () => {
+  console.log('server is connected');
+});
+
+server.on('request', (request, response) => {
+  console.log('requesting', request.url);
+  function setResponse(header) {
+    response.setHeader('Content-Type', 'Text-html');
+    response.write(`<h1>${header}</h1>`);
+    response.end();
+  }
+  switch (request.url) {
+    case '/add':
+      setResponse(state++);
+      break;
+    case '/remove':
+      setResponse(state--);
+      break;
+    case '/reset':
+      setResponse((state = 10));
+      break;
+    case '/state':
+      setResponse(state);
+      break;
+    default:
+      setResponse('Error 404, page not found');
+  }
+});
+const port = 8080;
+server.listen(port, () => {
+  console.log('Listening on', port);
+});
