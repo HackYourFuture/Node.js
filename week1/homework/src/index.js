42686d64be7fc71a948d8e0c99e039a4d7ef5818
unknown <fadikurdi00@gmail.com>
2018-03-01 16:45:52 +0100

    week1 homework

--- week1/homework/src/index.js
+++ week1/homework/src/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // Write the homework code in this file
+const http = require('http');
+
+const handleRequest = require('./handleRequest');
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000');
+});
