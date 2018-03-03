13562baa15643adccdabb4e807f446327b9a8b88
anasmos <anasmos@gmail.com>
2018-03-03 17:47:40 +0100

    create server in node js and use state

--- week1/homework/src/index.js
+++ week1/homework/src/index.js
@@ -1,3 +1,36 @@
 'use strict';
-
-// Write the homework code in this file
+const http = require('http');
+let state = 10;
+function holdState(req, res) {
+  console.log('you are in the url:' + req.url);
+  switch (req.url) {
+    case '/state':
+      res.writeHead(200, { 'Context-type': 'text/html' });
+      res.write(`<h1>the state is ${state}</h1>`);
+      break;
+    case '/add':
+      state++;    
+      res.writeHead(200, { 'Context-type': 'text/html' });
+      res.write(`<p><strong>ok</strong> the state is ${state}</p>`);
+      break;
+    case '/subtract':
+      state--;
+      res.writeHead(200, { 'Context-type': 'text/html' });
+      res.write(`<p><strong>ok</strong> the state is ${state}</p>`);
+      break;
+    case '/reset':
+      state = 10;
+      res.writeHead(200, { 'Context-type': 'text/html' });
+      res.write(`<p><strong>ok</strong> the state is ${state}</p>`);
+      break;
+    default:
+      res.writeHead(404, { 'Context-type': 'text/html' });
+      res.write(`<p><strong>error 404</strong> the page in not found be sour to input the right url</p>`);
+      break;
+  }
+  res.end(); 
+}
+const server = http.createServer(holdState);
+server.listen(8080, () => {
+  console.log('you are listning to the port 8080');
+});
