dd9f64042a1ee0dd9bd3d725c89ae279ef9a18e0
Kemirdin <qemirdin@gmail.com>
2018-03-04 15:23:14 +0100

    update my homework

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,23 @@
+import { resolve } from 'url';
+
 'use strict';
 
 // Write the homework code in this file
+const fs = require('fs');
+const STORE_FILE_NAME = 'store.txt';
+
+function readFile() {
+    return new Promise(resolve => fs.readFile(STORE_FILE_NAME, (err, Date) => resolve(err ? '' : date.toString()))
+    )
+
+}
+function WriteFile(...text) {
+    return new Promise(
+        (resolve, reject) => fs.appendFile(
+            STORE_FILE_NAME, `${text.join('')}\n`,
+            (err, date) => err
+                ? reject(err)
+            :resolve (date)   
+        )
+    )
+}
