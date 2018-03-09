8492847a4bf7b0c730dbcdcc3432049a5f8c539d
Semih21 <semihkasimoglu@gmail.com>
2018-03-09 14:56:53 +0100

    Homework 1 and 2 are completed

--- week1/homework/src/index.js
+++ week1/homework/src/index.js
@@ -1,3 +1,93 @@
 'use strict';
 
-// Write the homework code in this file
+'use strict';
+const http = require('http');
+//const path = require('path');
+let state = 10;
+
+const PORT = 3000;
+
+function handleRequest(req, res) {
+    console.log('on request', req.url);
+    switch (req.url) {
+        case '/':
+            res.setHeader('Content-Type', 'text/html')
+            res.write(`<h3>OK<h3> ${state}`);
+            break;
+        case '/add':
+            state++;
+            res.setHeader('Content-Type', 'text/html')
+            res.write(`<h3>OK<h3> ${state}`);
+            break;
+        case '/subtract':
+            state--;
+            res.setHeader('Content-Type', 'text/html')
+            res.write(`<h3>OK<h3> ${state}`);
+            break;
+        case '/reset':
+            state = '';
+            res.setHeader('Content-Type', 'text/html')
+            res.end(`<h3>OK<h3> ${state}`);
+            break;
+        default:
+
+            /* const extension = path.extname(req.url);
+             if (extension === '') {
+                 res.statusCode = 302;
+                 res.setHeader('Location', '/');
+             } else {*/
+            res.statusCode = 404;
+            res.write(res, 'File not found');
+            break;
+    }
+
+    res.end();
+}
+const server = http.createServer(handleRequest);
+server.listen(PORT, () => {
+    console.log(`Server started http://localhost:${PORT}`);
+});
+
+
+/*II. WAY with If ...else
+'use strict';
+const http = require('http');
+
+const PORT = 8080;
+
+function handleRequest(req, res) {
+    let state = 10;
+    console.log('on request', req.url);
+    res.setHeader('Content-Type', 'text/html');
+    if (req.url === '/') {
+
+        res.write(`<h3>OK<h3> ${state}`);
+    }
+    else if (req.url === '/add') {
+        state++;
+        res.write(`<h3>OK<h3> ${state}`);
+
+    } else if (req.url === '/subtract') {
+        state--;
+        res.write(`<h3>OK<h3> ${state}`);
+
+    }
+    else if (req.url === '/reset') {
+        state = '';
+        res.write(`<h3>OK<h3> ${state}`);
+
+    } else {
+        res.statusCode = 404;
+        res.write(res, 'File not found');
+
+    }
+
+
+
+    res.end();
+}
+const server = http.createServer(handleRequest);
+server.listen(PORT, () => {
+    console.log(`Server started http://localhost:${PORT}`);
+});
+*/
--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,3 @@
-'use strict';
+
 
 // Write the homework code in this file
