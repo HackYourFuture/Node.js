1c753bbfcfa06dde684cc6f1a6e016c729f818f4
Kemirdin <qemirdin@gmail.com>
2018-03-16 17:25:57 +0100

    Using built-in JSON parser

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+const Express = require('express');
+const createTodo = require('nedb');
+const TODO = require('./toDos');
+const port = Number(process.env.port || 3000);
+
+const db = new createTodo ({filename: 'toDos', autoload: true});
+express ()
+  .use (cors ())
+  .get ('/', function (req, res) {
+    res.redirect (302, 'https://github.com/HackYourFuture/nodejs.git');
+  })
+  .use ('/toDos', toDos (db))
+  .listen (port);
+console.log ('listening on ' + port);
