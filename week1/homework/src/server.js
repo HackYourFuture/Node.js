e83c1802268215ff56120fce4b62dda0f42d12e1
Zaher <zaherqasqas@gmail.com>
2018-04-19 23:35:49 +0200

    Implement the endpoints

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,9 +10,33 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
-    console.log('Request was made: ' + request.url);
-    response.writeHead(200, { 'content-Type': 'application/json' });
-    response.end();
+    const sendJsonData = (statusCode, jsonMessage) => {
+      response.writeHead(statusCode, {
+        'Content-Type': 'application/json'
+      });
+      response.end(JSON.stringify(jsonMessage));
+    };
+
+    switch (request.url) {
+      case '/state':
+        sendJsonData(200, { state: state });
+        break;
+      case '/add':
+        state++;
+        sendJsonData(200, { state: state });
+        break;
+      case '/subtract':
+        state--;
+        sendJsonData(200, { state: state });
+        break;
+      case '/reset':
+        state = 10;
+        sendJsonData(200, { state: state });
+        break;
+      default:
+        sendJsonData(404, { error: 'Not found' });
+        break;
+    }
   });
 
   return server;
