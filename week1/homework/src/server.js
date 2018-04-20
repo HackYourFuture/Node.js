811681512a6acb58c8f124f6233811a7ceefc447
Jason <jason.h.w.tan@gmail.com>
2018-04-21 01:33:40 +0200

    initial commit

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,6 +10,30 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    switch (request.url) {
+      case 'state':
+        response.setHeader('Content-Type', 'application/json');
+        response.write(`The state is ${state}`);
+        break;
+      case '/add':
+        response.setHeader('Content-Type', 'application/json');
+        response.write(`The state is ${state + 1}`);
+        break;
+      case '/subtract':
+        response.setHeader('Content-Type', 'application/json');
+        response.write(`The state is ${state - 1}`);
+        break;
+      case '/reset':
+        response.setHeader('Content-Type', 'application/json');
+        response.write(`The state is ${state}`);
+        break;
+      default:
+        response.statusCode = 404; // otherwise, by default is is code 200.
+        break;
+    }
+    // never modifying state, and should return a json object - setting content type, but never actually returning a JSON thing.
+    // return a JSON first, then modify.
+    return response.end();
   });
 
   return server;
