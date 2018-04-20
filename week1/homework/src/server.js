6247280aaadb2f07ae33ac67553cedc91b89277d
AYA AL-ABRASH <35225101+Aya-Alabrash@users.noreply.github.com>
2018-04-20 18:26:22 +0200

    Update server.js

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,6 +10,34 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    console.log(request.url);
+    function switchStates(x) {
+      response.setHeader(`Content-Type`, `application/json`);
+      response.write(`status: ${x}`);
+      response.end();
+    }
+    switch (request.url) {
+      case '/state':
+        switchStates(state);
+        break;
+      case '/add':
+        // state++;
+        switchStates(state++);
+        break;
+      case '/subtract':
+        // state--;
+        switchStates(state--);
+        break;
+      case '/reset':
+        // state = 10;
+        switchStates(state = 10);
+        break;
+      default:
+        response.statusCode = 404;
+        response.setHeader(`content-Type`, `application/json`);
+        response.write(`error: Not found`);
+        response.end();
+    }
   });
 
   return server;
