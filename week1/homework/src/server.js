c98f74878f78d074f7be514d02e8b5d961b4655b
Hadeel Alabrash <35224912+hadeel43@users.noreply.github.com>
2018-04-20 22:45:56 +0200

    Update server.js

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,9 +9,36 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    // TODO: Write your homework code heres
+    let States = (state) => {
+      response.setHeader('Content-Type', 'application/json');
+      const num = JSON.stringify({ state: state });
+      response.write(num);
+    };
+    switch (request.url) {
+      case '/state':
+        States(state);
+        break;
+      case '/add':
+        state++;
+        States(state);
+        break;
+      case '/subtract':
+        state--;
+        States(state);
+        break;
+      case '/reset':
+        state = 10;
+        States(state);
+        break;
+      default:
+        response.setHeader(`content-Type`, `application/json`);
+        response.statusCode = 404;
+        const errorMessage = { 'error': 'Not found' };
+        response.write(JSON.stringify(errorMessage));
+    }
+    response.end();
   });
-
   return server;
 }
 
