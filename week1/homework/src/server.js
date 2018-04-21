61b747bff534c37087871dfa4221971c4e7658a2
Sob7i <sub7i.alkhuder@gmail.com>
2018-04-22 00:28:06 +0200

    Sobhi HM

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,8 +10,43 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    if (request.url === '/state') {
+      response.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      response.end(JSON.stringify({
+        state: state
+      }));
+    } else if (request.url === '/add') {
+      response.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      response.end(JSON.stringify({
+        state: state++
+      }));
+    } else if (request.url === '/subtract') {
+      response.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      response.end(JSON.stringify({
+        state: state--
+      }));
+    } else if (request.url === '/reset') {
+      response.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      response.end(JSON.stringify({
+        state: 10
+      }));
+    } else {
+      response.writeHead(404, {
+        'Content-Type': 'application/json'
+      });
+      response.end(JSON.stringify({
+        'error': 'Not found'
+      }));
+    }
   });
-
   return server;
 }
 
