50f2bb629562c0fa6e62cdc98547d870fc7cdd38
Gaorieh Isaac <gaorieh@hotmail.com>
2018-04-29 01:38:35 +0200

    corrections and add function.js

--- week2/homework/src/app.js
+++ week2/homework/src/app.js
@@ -1,21 +1,27 @@
 'use strict';
+
 let program = require('commander');
-const {
-  promisify
-} = require('util');
 
 const {
+
+  addTodo,
+  listTodos,
+  removeTodo,
+  resetTodo,
+  updateTodo,
+  help,
+  promisify,
   readFile,
   writeFile,
-  appendFile
-} = require('fs');
-const TODO_FILE = 'todo.json';
+  appendFile,
+  readFileWithPromise,
+  writeFileWithPromise,
+  appendFileWithPromise,
+  TODO_FILE,
+  args,
+  cmd
 
-
-const readFileWithPromise = promisify(readFile);
-const writeFileWithPromise = promisify(writeFile);
-const appendFileWithPromise = promisify(appendFile);
-
+} = require('./functions');
 
 program
   .version('0.1.0')
@@ -27,72 +33,24 @@ program
   .parse(process.argv);
 
 async function main() {
-  const [, , cmd, ...args] = process.argv;
 
   if (program.add) {
-    const data = await readFileWithPromise(TODO_FILE, 'utf-8').catch(() => '[]');
-    const todos = JSON.parse(data);
-    const newTodo = args.join(' ');
-    todos.push(newTodo);
-    console.log(newTodo);
-    await writeFileWithPromise(TODO_FILE, JSON.stringify(todos));
+    addTodo();
 
   }
   if (program.list) {
-    const data = await readFileWithPromise(TODO_FILE, 'utf-8').catch(() => '[]');
-    const todos = JSON.parse(data);
-    console.info(todos);
+    listTodos();
   }
   if (program.remove) {
-    const data = await readFileWithPromise(TODO_FILE, 'utf-8').catch(() => '[]');
-    const todos = JSON.parse(data);
-    const index = args - 1;
-    const result = todos[index];
-
-    if (index <= todos.length - 1 && index > -1) {
-      todos.splice(index, 1);
-      await writeFileWithPromise(TODO_FILE, JSON.stringify(todos));
-      console.info(result + ' : has been REMOVED!!!');
-      if (todos.length === 0) {
-        console.info('TODOS is EMPTY NOW!!!');
-      }
-
-    } else {
-      console.info('The TODO-ITEM is not exist!!');
-    }
+    removeTodo();
   }
 
   if (program.reset) {
-    await writeFileWithPromise(TODO_FILE, JSON.stringify([]));
-    console.info('ToDo\'s is EMPTY now!!!');
+    resetTodo();
   }
 
   if (program.update) {
-    const [, , cmd, index, itemToUpdated, ...args] = process.argv;
-    const data = await readFileWithPromise(TODO_FILE, 'utf-8').catch(() => '[]');
-    const todos = JSON.parse(data);
-    const indexTodo = parseInt(index)
-    const arrTodo = [];
-    arrTodo.push(todos);
-    let sizeOfTodos = Object.keys(todos).length;
-
-    if ((typeof indexTodo === 'number' && !isNaN(indexTodo))) {
-
-      if (indexTodo <= 0 || indexTodo > sizeOfTodos) {
-
-        console.info('The TODO-ITEM is not exist...!\nYou must enter index between 1 and ' + sizeOfTodos);
-
-      } else {
-        arrTodo[0].splice(indexTodo - 1, 1, itemToUpdated);
-        let newTodo = arrTodo[0];
-        await writeFileWithPromise(TODO_FILE, JSON.stringify(newTodo));
-        const result = 'index ' + index + ' has been UPDATED by: ' + newTodo[indexTodo - 1];
-        console.info(result);
-
-      }
-    } else {
-      console.info('Your index is not a number, YOU MUST ENTER a number !');
-    }
+    updateTodo();
   }
 
 }
