06d91b4457dc8d3172cc8433ff88ca88e866b9c7
Jason <jason.h.w.tan@gmail.com>
2018-04-29 15:57:59 +0200

    updated help menu to add line separators.

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -56,12 +56,12 @@ async function main() {
     }
     case 'help':
     default:
-      console.info('These are valid options: \n
-        \"node . help\"  - displays help menu \n
-        \"node .\" - displays current to-do list \n
-        \"node . add [to do-list item]\" - adds to-do list item \n
-        \"node . remove [index number]\" - removes a to-do item by index number. \n
-        \"node . update [index number, task]\" - updates a to-do item at index wth something else.\n
+      console.info('These are valid options: \n\
+        \"node . help\"  - displays help menu \n\
+        \"node . list\" - displays current to-do list \n\
+        \"node . add [to do-list item]\" - adds to-do list item \n\
+        \"node . remove [index number]\" - removes a to-do item by index number. \n\
+        \"node . update [index number, task]\" - updates a to-do item at index wth something else.\n\
         \"node . reset\" - removes all items from list.');
       break;
   }
