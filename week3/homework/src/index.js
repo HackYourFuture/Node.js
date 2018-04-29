d72de7cdf87e80d0332df9fbf51f3c7bd0929492
Sob7i <sub7i.alkhuder@gmail.com>
2018-04-29 15:13:57 +0200

    Express install

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+
+const express = require('express');
+const uuid = require('uuid/v4');
+
+const {
+    readFile: _readFile,
+    writeFile: _writeFile
+} = require('fs');
+
+const {
+    promisify
+} = require('util');
+
+const readFile = promisify(_readFile);
+const writeFile = promisify(_writeFile);
+
+const app = express();
