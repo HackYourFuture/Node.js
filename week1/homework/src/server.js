a69c99e345b03d1db737d9f6edf8c5ca02b5dba0
Dumie1 <dumiedumara@gmail.com>
2018-06-27 17:06:43 +0200

    installed dependencies using npm install command

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -8,8 +8,35 @@ const http = require('http');
 function createServer(port) {
   let state = 10;
 
+  function respond(response) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({ state: 'bar' }));
+  }
+
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        respond(response);
+        break;
+
+      case '/add':
+        state = state + 1;
+        break;
+
+      case '/subtract':
+        state = state - 1;
+        break;
+
+      case '/reset':
+        state = state - 1;
+        break;
+
+      default:
+        response.writeHead(404, { 'Content-Type': 'application.json' });
+        response.end(JSON.stringify({ state: 'bar' }));
+        break;
+    }
   });
 
   return server;
