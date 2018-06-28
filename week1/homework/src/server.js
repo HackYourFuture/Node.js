39439907525ed957e78519f7bf15a33d141cfd6e
neveenatik <nevine.atike@gmail.com>
2018-06-28 05:26:23 +0200

    Added homework week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,6 +10,30 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    function sendResponse(status, stateValue, contentType) {
+      state = stateValue;
+      response.writeHead(status, { 'Content-Type': contentType });
+      response.write(`{ "state": ${state}}`);
+    }
+    switch (request.url) {
+      case '/state':
+        sendResponse(200, state, 'application/json');
+        break;
+      case '/add':
+        sendResponse(200, state + 1, 'application/json');
+        break;
+      case '/subtract':
+        sendResponse(200, state - 1, 'application/json');
+        break;
+      case '/reset':
+        sendResponse(200, 10, 'application/json');
+        break;
+      default:
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.write(`{ "error": "Not found" }`);
+        break;
+    }
+    response.end();
   });
 
   return server;
