568ef8554df708afeb6c63dadb901d8f6a91a9f8
AllouhM <mahmoud.allouh0@gmail.com>
2018-06-28 22:26:19 +0200

    add homework week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -7,9 +7,34 @@ const http = require('http');
  */
 function createServer(port) {
   let state = 10;
-
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    let selectTransaction = (value) => {
+      state = value;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ 'state': state }));
+    };
+    switch (request.url) {
+      case ('/state'):
+        selectTransaction(state);
+        break;
+      case ('/add'):
+        state++;
+        selectTransaction(state);
+        break;
+      case ('/subtract'):
+        state--;
+        selectTransaction(state);
+        break;
+      case ('/reset'):
+        state = 10;
+        selectTransaction(state);
+        break;
+      default:
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({ error: 'Not found' }));
+    }
+    response.end();
   });
 
   return server;
