afd3ed6a96bffbb4797866eb50c938e0a5e742ab
alejandra <aztecgoddess@hotmail.com>
2018-06-29 02:28:46 +0200

    Node.js Week1, please review, feedback welcome

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,7 +9,39 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    const handleRequest = (statusCode, jsonMessage) => {
+      response.writeHead(statusCode, {
+        'Content-Type': 'application/json'
+      });
+      response.end(JSON.stringify(jsonMessage));
+    };
+
+    switch (request.url) {
+
+      case '/state';
+        handleRequest(response);
+        break;
+
+      case '/add';
+        state++;
+        handleRequest(response);
+        break;
+
+      case '/subtract';
+        state--;
+        handleRequest(response);
+        break;
+
+      case '/reset';
+        state = 10;
+        handleRequest(response);
+        break;
+
+      default:
+        response.statusCode = 404;
+        sendText(response, 'File not found');
+
+    }
   });
 
   return server;
