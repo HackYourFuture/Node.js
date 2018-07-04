a4750fa70974993970e1de721498aae4a9916f5d
Meazer <meazer.m@gmail.com>
2018-07-04 19:08:09 +0200

    add indexCommander.js using commander library

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,53 +1,20 @@
 'use strict';
 
 const { readTODOs, writeTODOs } = require('./fileOperations');
-const filePath = 'TODOs2.txt';
+const { displayTodos, editAndWrite } = require('./listOperations');
 
+const filePath = 'TODOs2.txt';
 const cmd = process.argv[2];
 const parameters = process.argv.slice(3);
 
-function displayHelp() {
-  console.log(`Usage: node . [options] 
-
-Options:
-
-  list                    display all to-dos
-  add [to-do]             add to-do to the list
-  remove [index]          delete to-do at index
-  update [index] [to-do]  update list at index with to-do
-  reset                   delete all to-dos
-  help                    show this help text
-  default                 show this help text
-  `);
-}
-
-function displayTodos(todos) {
-  if (todos.length === 0)
-    return console.log('TODOs list is now empty, use ADD command to add some!');
-  todos.forEach((element, index) => {
-    console.log(`${index + 1}- ${element}`);
-  });
-}
-
-async function editAndWrite(list, index, item) {
-  if (isNaN(index)) return displayHelp();
-  if (index > list.length || index < 1)
-    return console.log(`TODO #${index} isn't in the list`);
-  item ? list.splice(index - 1, 1, item) : list.splice(index - 1, 1);
-  try {
-    await writeTODOs(filePath, list);
-    displayTodos(list);
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
+main();
 
 async function main() {
   const todos = await readTODOs(filePath);
   switch (cmd) {
     case 'add':
-      if (!parameters[0]) return displayHelp();
+      if (!parameters[0])
+        return console.log('error: invalid required argument <to-do>');
       try {
         todos.push(parameters[0]);
         await writeTODOs(filePath, todos);
@@ -58,11 +25,12 @@ async function main() {
       }
       break;
     case 'remove':
-      editAndWrite(todos, parameters[0]);
+      editAndWrite(filePath, parameters[0]);
       break;
     case 'update':
-      if (!parameters[1]) return displayHelp();
-      editAndWrite(todos, parameters[0], parameters[1]);
+      if (!parameters[1])
+        return console.log('error: invalid required argument <to-do>');
+      editAndWrite(filePath, parameters[0], parameters[1]);
       break;
     case 'list':
       displayTodos(todos);
@@ -81,4 +49,18 @@ async function main() {
   }
 }
 
-main();
+function displayHelp() {
+  console.log(`Usage: node . [options] 
+to use commander version: node indexCommander.js [options]
+
+Options:
+
+  list                    output all to-dos
+  add <to-do>             add to-do to the list
+  remove <index>          delete to-do at index
+  update <index> <to-do>  update list at index with to-do
+  reset                   delete all to-dos
+  help                    output usage information
+  default                 output usage information
+  `);
+}
