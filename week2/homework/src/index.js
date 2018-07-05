7391bf356bec545d707a32f79e399389191c1376
alejandra <aztecgoddess@hotmail.com>
2018-07-05 23:59:09 +0200

    Work in progress, do not review

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+const { appendFile, readFile, writeFile } = require('fs');
+const { promisify } = require('util');
+const readFileWithPromise = promisify(readFile);
+const writeFileWithPromise = promisify(writeFile);
+const args = process.argv.slice(2);
+const cmd = args[0];
+const TODO_PATH = 'todo.json';
+
+
+switch (cmd) {
+  case 'add':
+    break;
+  case 'reset':
+    break;
+  case 'remove':
+    state--;
+    handleRequest(state);
+    break;
+  case '/reset': ;
+    break;
+  default:
+}
