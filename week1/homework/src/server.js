68a386f6469bff649068d69a6cb4ed093cc08be5
neveenatik <nevine.atike@gmail.com>
2018-07-07 11:19:36 +0200

    Made sendResponse function a global function to not occupy the stack

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,28 +5,30 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
-function createServer(port) {
-  let state = 10;
 
+let state = 10;
+
+function sendResponse(response, status, stateValue, contentType) {
+  state = stateValue;
+  response.writeHead(status, { 'Content-Type': contentType });
+  response.write(`{ "state": ${state}}`);
+}
+
+function createServer(port) {
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
-    function sendResponse(status, stateValue, contentType) {
-      state = stateValue;
-      response.writeHead(status, { 'Content-Type': contentType });
-      response.write(`{ "state": ${state}}`);
-    }
     switch (request.url) {
       case '/state':
-        sendResponse(200, state, 'application/json');
+        sendResponse(response, 200, state, 'application/json');
         break;
       case '/add':
-        sendResponse(200, state + 1, 'application/json');
+        sendResponse(response, 200, state + 1, 'application/json');
         break;
       case '/subtract':
-        sendResponse(200, state - 1, 'application/json');
+        sendResponse(response, 200, state - 1, 'application/json');
         break;
       case '/reset':
-        sendResponse(200, 10, 'application/json');
+        sendResponse(response, 200, 10, 'application/json');
         break;
       default:
         response.writeHead(404, { 'Content-Type': 'application/json' });
