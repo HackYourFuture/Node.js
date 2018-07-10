9c4cdc82e9b42e7d195638b9cfdf422be1083aa5
Dumie1 <dumiedumara@gmail.com>
2018-07-11 01:50:49 +0200

    rewriting the delete function

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -2,11 +2,7 @@
 
 // TODO: Write the homework code in this file
 const { readFile, writeFile } = require('fs');
-
 const { promisify } = require('util');
-
-// const appendFileWithPromise = promisify(appendFile);
-
 const readFileWithPromise = promisify(readFile);
 const writeFileWithPromise = promisify(writeFile);
 
@@ -26,32 +22,60 @@ function writeTodos(todos) {
   return writeFileWithPromise(TODO_PATH, JSON.stringify(todos, null, 2));
 }
 
-async function main() {
-  switch (cmd) {
-    case 'add':
-      const text = args[1];
-      const todos = await readTodos();
-      todos.push({
-        text: text,
-        done: false
-      });
-
-      await writeTodos();
-      const newTodos = await readTodos();
-      console.log(newTodos);
-      break;
-    case 'delete':
-
-      break;
-    case 'update':
-
-      break;
-    case 'list':
-      readTodos().then(console.log);
-      break;
-    default:
-      console.log('Some help');
-      break;
-  }
+function myHelper() {
+  console.log('Get help from these commands:1- node.add \'New todo\'   = Add a todo 2- node . delete [Delete item]  = Delete item from the list 3- node . list  = List all items 4- node . update [Item to be updated]  \'New todo\'  = Updating a todo. 5- node . reset  = Reset the list');
 }
-main();
+
+switch (cmd) {
+  case 'add':
+    const text = args[1];
+    readTodos()
+      .then(todos => {
+        todos.push({ todo: text });
+        return todos;
+      })
+      .then(writeTodos)
+      .then(readTodos)
+      .then(console.log);
+    break;
+
+  case 'delete':
+    let item = args[1];
+    readTodos()
+      .then(todos => {
+        if (item > 0) {
+          todos.splice(item, 1);
+        }
+        return todos;
+      }).then(writeTodos);
+    break;
+
+  case 'update':
+    let updateItem = args[1];
+    let newItem = args[2];
+    readTodos()
+      .then(todos => {
+        if (updateItem >= 0 && typeof newItem === 'string') {
+          todos.splice(updateItem, 1);
+          todos[updateItem] = { todos: newItem };
+        }
+        return todos;
+      }).then(writeTodos);
+    break;
+
+  case 'list':
+    readTodos().then(console.log);
+    break;
+
+  case 'reset':
+    readTodos()
+      .then(todos => {
+        todos.splice(0, todos.length);
+        return todos;
+      }).then(writeTodos);
+    break;
+
+  default:
+    myHelper();
+    break;
+}
