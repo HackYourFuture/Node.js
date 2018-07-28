f105d71eba4e8c5d8a37c0d4a4989c3446367a62
Sam <hackoor83@gmail.com>
2018-07-28 02:26:40 +0200

    Sam Week 1 NodeJS

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const http = require('http');
+var path = require('path');
 
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
@@ -10,10 +11,41 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+
+    switch (request.url) {
+      case '/state':
+        console.log(state);
+        response.write(`${{ 'state': state }}`);
+        break;
+      case '/add':
+        response.write(`${{ 'state': state += 1 }}`);
+        break;
+      case '/subtract':
+        response.write(`${{ 'state': state -= 1 }}`);
+        break;
+      case '/reset':
+        state = 10;
+        response.write(`${{ 'state': state }}`);
+        break;
+      default:
+        const extension = path.extname(request.url);
+        if (extension === '') {
+          response.statusCode = 302;
+          // response.setHeader('Location', '/');
+          response.write('This is invalid path!!');
+        }
+    }
+
+    response.end();
+
   });
 
   return server;
 }
+
+createServer(3000);
 
 module.exports = {
   createServer
