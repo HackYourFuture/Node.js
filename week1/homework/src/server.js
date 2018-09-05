64f2620eb535da75e3bf7ea9f01f761f6066a75e
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-09-05 13:04:02 +0200

    creating server for add, subtract, reset and state

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -2,14 +2,47 @@
 
 const http = require('http');
 
-/* `createServer` MUST return an instance of `http.Server` otherwise the tests
- * will fail.
+/* `createServer` MUST return an instance of `http.Server` 
+otherwise the tests will fail.
  */
-function createServer(port) {
+function createServer() {
   let state = 10;
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        const stateJson1 = { 'state': state };
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(stateJson1));
+        break;
+
+      case '/add':
+        state++;
+        const stateJson2 = { 'state': state };
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(stateJson2));
+        break;
+
+      case '/subtract':
+        state--;
+        const stateJson3 = { 'state': state };
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(stateJson3));
+        break;
+
+      case '/reset':
+        state = 10;
+        const stateJson4 = { 'state': state };
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(stateJson4));
+        break;
+
+      default:
+        const errorMessage = { 'error': 'Not found' };
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(errorMessage));
+    }
   });
 
   return server;
