9839e6c14731f9901b56e87bec68f84bf632bcfa
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-09-05 20:40:22 +0200

    edit after first comment

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,43 +5,42 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` 
 otherwise the tests will fail.
  */
+
+
+
 function createServer() {
   let state = 10;
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    function writeContent(status, content) {
+      response.writeHead(status, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify(content));
+      response.end();
+    }
+
     switch (request.url) {
       case '/state':
-        const stateJson1 = { 'state': state };
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify(stateJson1));
+        writeContent(200, { 'state': state });
         break;
 
       case '/add':
         state++;
-        const stateJson2 = { 'state': state };
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify(stateJson2));
+        writeContent(200, { 'state': state });
         break;
 
       case '/subtract':
         state--;
-        const stateJson3 = { 'state': state };
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify(stateJson3));
+        writeContent(200, { 'state': state });
         break;
 
       case '/reset':
         state = 10;
-        const stateJson4 = { 'state': state };
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify(stateJson4));
+        writeContent(200, { 'state': state });
         break;
 
       default:
-        const errorMessage = { 'error': 'Not found' };
-        response.writeHead(404, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify(errorMessage));
+        writeContent(404, { 'error': 'Not found' });
     }
   });
 
