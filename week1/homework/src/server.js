e5a966b3b08ff6f235107fa5e69b6f820257aeb9
genegr88 <genegr888@gmail.com>
2018-09-05 14:31:36 +0200

    changed Found to found

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -15,28 +15,28 @@ function createServer(port) {
     // TODO: Write your homework code here
     switch (request.url) {
       case '/state':
-        body = JSON.stringify({state});
+        body = JSON.stringify({state}, null, 2);
         console.log('route /state', state);
         break;
       case '/add':
         state++;
-        body = JSON.stringify({state});
+        body = JSON.stringify({state}, null, 2);
         console.log('route /add', state);
         break;
       case '/subtract':
         state--;
-        body = JSON.stringify({state});
+        body = JSON.stringify({state}, null, 2);
         console.log('route /subtract', state);
         break;
       case '/reset':
         state = 10;
-        body = JSON.stringify({state});
+        body = JSON.stringify({state}, null, 2);
         console.log('route /reset', state);
         break;
       default:
         statusCode = 404;
-        message = 'Not Found';
-        body = JSON.stringify({error: 'Not Found'});
+        message = 'Not found';
+        body = JSON.stringify({error: 'Not found'}, null, 2);
     }
     response.setHeader('Content-Type', 'application/json');
     response.statusMessage = message;
