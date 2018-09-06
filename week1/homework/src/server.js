eab281ded49dafb810d5126866143716c496f8f7
Ernesto Navarro <nestico0620@gmail.com>
2018-09-06 14:31:03 +0200

    new version deleting looping

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -7,32 +7,34 @@ const http = require('http');
  */
 
 const responseError = { "error": "Not found"};
-const urls = ['/state', '/add', '/subtract', '/reset'];
 
 function createServer(port) {
   let responseObject = {'state': 10};
   const server = http.createServer((request, response) => {
-    const status = urls.includes(request.url)? 200 : 404;
-    response.writeHead(status, {"Content-type": "application/json"})
 
     switch(request.url)
   	{        
       case '/state':
+        response.writeHead(200, {"Content-type": "application/json"});
         response.write(JSON.stringify(responseObject, null, 2));
         break;
       case '/add':
         responseObject.state = responseObject.state + 1;
+        response.writeHead(200, {"Content-type": "application/json"});
         response.write(JSON.stringify(responseObject, null, 2));
         break;
       case '/subtract':
         responseObject.state = responseObject.state - 1;
+        response.writeHead(200, {"Content-type": "application/json"});
         response.write(JSON.stringify(responseObject, null, 2));
         break;
       case '/reset':
         responseObject.state = 10;
+        response.writeHead(200, {"Content-type": "application/json"});
         response.write(JSON.stringify(responseObject, null, 2));
         break;
       default:
+      response.writeHead(404, {"Content-type": "application/json"});
         response.write(JSON.stringify(responseError, null, 2));
         break;
     }
