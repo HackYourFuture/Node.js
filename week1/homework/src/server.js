21feb2672c2ed38c8ae6a3bd41fbfc49f606c573
marwanaboudaoud <marwanaboudoud@gmail.com>
2018-09-06 16:43:14 +0200

    node.js week1 homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -2,19 +2,40 @@
 
 const http = require('http');
 
-/* `createServer` MUST return an instance of `http.Server` otherwise the tests
- * will fail.
- */
 function createServer(port) {
-  let state = 10;
-
+  let state = { 'state': 10 };
+  const pageState = '/state';
+  const pageAdd = '/add';
+  const pageSubtract = '/subtract';
+  const pageReset = '/reset';
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    if (request.url === pageState) {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(state, null, 2));
+    }
+    else if (request.url === pageAdd) {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      state.state++;
+      response.end(JSON.stringify(state, null, 2));
+    }
+    else if (request.url === pageSubtract) {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      state.state--;
+      response.end(JSON.stringify(state, null, 2));
+    }
+    else if (request.url === pageReset) {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      state.state = 10;
+      response.end(JSON.stringify(state, null, 2));
+    }
+    else {
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      const notFound = { 'error': 'Not found' };
+      response.end(JSON.stringify(notFound, null, 2));
+    }
   });
-
   return server;
 }
-
 module.exports = {
   createServer
 };
