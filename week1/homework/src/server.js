f20a45efb9d41dd66749cc2f5d655232bf8f3912
Ernesto Navarro <nestico0620@gmail.com>
2018-09-06 14:06:04 +0200

    changes done

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,43 +5,38 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
-let responseObject = {'state': 10};
+
 const responseError = { "error": "Not found"};
 const urls = ['/state', '/add', '/subtract', '/reset'];
+
 function createServer(port) {
   let responseObject = {'state': 10};
   const server = http.createServer((request, response) => {
-    const status = (urls.some(elm => elm === request.url))? 200 : 404;
+    const status = urls.includes(request.url)? 200 : 404;
     response.writeHead(status, {"Content-type": "application/json"})
+
     switch(request.url)
   	{        
+      case '/state':
+        response.write(JSON.stringify(responseObject, null, 2));
+        break;
       case '/add':
-      {
         responseObject.state = responseObject.state + 1;
+        response.write(JSON.stringify(responseObject, null, 2));
         break;
-      }
       case '/subtract':
-      {
         responseObject.state = responseObject.state - 1;
+        response.write(JSON.stringify(responseObject, null, 2));
         break;
-      }
       case '/reset':
-      {
         responseObject.state = 10;
+        response.write(JSON.stringify(responseObject, null, 2));
         break;
-      }
       default:
+        response.write(JSON.stringify(responseError, null, 2));
         break;
     }
-    
-    if(urls.some(elm => elm === request.url))
-      response.write(JSON.stringify(responseObject, null, 2));
-    else
-      response.write(JSON.stringify(responseError, null, 2));
-
-    
     response.end();
-  	
   });
 
   return server;
