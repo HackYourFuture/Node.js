a4d8d801541d4f5235b0302454268fff76d966b5
Donald Zodeh <donald_leb@hotmail.com>
2018-09-08 00:58:04 +0200

    Dona's homework1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,7 +9,34 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    response.setHeader('Content-Type', 'application/json', 'charset = utf - 8');
+    switch (request.url) {
+      case '/state':
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/add':
+        state += 1;
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/subtract':
+        state -= 1;
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/reset':
+        state = 10;
+        response.write(JSON.stringify({ state }));
+        break;
+      default:
+        if (request.url === '/') {
+          response.write(JSON.stringify({ state }));
+        }
+        else {
+          response.statusCode = 404;
+          const notFound = { 'error': 'Not found' };
+          response.end(JSON.stringify(notFound, null, 2));
+        }
+    }
+    response.end();
   });
 
   return server;
