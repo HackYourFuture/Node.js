e8ab6263f1aa5fdeb7c9e970d52008c56dd65826
ward <vakpko@gmail.com>
2018-09-08 14:04:15 +0200

    Refactor code_set status 200

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -6,24 +6,26 @@ function createServer(port) {
     let state = 10;
 
     const server = http.createServer((request, response) => {
-        response.setHeader('Content-Type', 'application/json', 'charset = utf - 8');
+        response.setHeader('200', { 'Content-Type': 'application/json'}, 'charset = utf - 8');
 
         if (request.url === '/state') {
-
             response.write(JSON.stringify({ state }, null, 3));
-        } else if (request.url === '/add') {
+        }
+        else if (request.url === '/add') {
             state += 1;
             response.write(JSON.stringify({ state }));
-        } else if (request.url === '/subtract') {
+        }
+        else if (request.url === '/subtract') {
             state -= 1;
             response.write(JSON.stringify({ state }));
-        } else if (request.url === '/reset') {
+        }
+        else if (request.url === '/reset') {
             state = 10;
             response.write(JSON.stringify({ state }));
-        } else {
-            const error = 'Not found';
-            response.writeHead(404);
-            response.write(JSON.stringify({ error }));
+        }
+        else {
+            response.writeHead('400', { 'Content-Type': 'text/plain' });
+            response.write('Not found ' + 400 );
         }
 
         response.end();
