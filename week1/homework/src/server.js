407bc0b761954c761e00b6c17625d78d9742b3c6
Gaffari02 <gaffarioguz@hotmail.com>
2018-09-08 19:05:33 +0900

    week1homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,6 +10,32 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    if (request.url === '/state') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      const main = { 'state': state };
+      response.end(JSON.stringify(main));
+    }
+    else if (request.url === '/add') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      const add = { 'state': ++state };
+      response.end(JSON.stringify(add));
+    }
+    else if (request.url === '/subtract') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      const subtract = { 'state': --state };
+      response.end(JSON.stringify(subtract));
+    }
+    else if (request.url === '/reset') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      state = 10;
+      const reset = { 'state': state };
+      response.end(JSON.stringify(reset));
+    }
+    else {
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      const err = { 'error': 'Not found' };
+      response.end(JSON.stringify(err));
+    }
   });
 
   return server;
