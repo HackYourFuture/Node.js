84f97f1b3c52ac004ceeed80b455c38c6f7f9744
Ozgur Aslan <alpozguraslan@gmail.com>
2018-09-08 10:34:02 +0200

    homework week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -6,15 +6,37 @@ const http = require('http');
  * will fail.
  */
 function createServer(port) {
-  let state = 10;
+	let state = 10;
+	const increment= (i)=>{ i++;return i;};
+	const decrement= (i)=> {i--;return i;};
+	let director = new Object;
+	director['/state'] = {'state':state};
+	director['/'] = {'state':state};
+	director['/reset'] = {'state':state};
+	director['/add'] = {'state':increment(state)};
+	director['/subtract'] = {'state':decrement(state)};
 
-  const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
-  });
-
-  return server;
+	function displayContent(request, response){
+		response.writeHead(200, { 'Content-Type': 'application/json'});
+		response.write(JSON.stringify(request, null, 2));
+		response.end();
+	}
+	function displayError(request, response){
+		response.writeHead(404, { 'Content-Type': 'application/json'});
+		response.write(JSON.stringify({ 'error': 'Not found' }, null, 2));
+		response.end();
+	}
+	const server = http.createServer((request, response) => {
+		// TODO: Write your homework code here
+		if(request.url in director){
+			displayContent(director[request.url], response);
+		}else{
+			displayError(request,response);
+		}    
+	}); 
+	return server;
 }
 
 module.exports = {
-  createServer
+	createServer
 };
