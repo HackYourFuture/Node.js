f328cc8496ddf1b7f9e544bbd6fe0e438007c77b
Ernesto Navarro <nestico0620@gmail.com>
2018-09-14 17:39:25 +0200

    hw version2

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,17 +1,15 @@
 'use strict';
 
 let fs = require('fs');
-
 let program = require('commander');
 
 program
-	.version('0.0.1');
+	.version('2.18.0');
 program
-	.command('add <index>')
+	.command('add <task>')
 	.description('add an element to your to-do list')
-	.action(function(index){
-		
-		handleRequest(0, 0, index);
+	.action(function(task){
+		add(task);
 	});
 program
 	.command('')
@@ -20,28 +18,28 @@ program
 		printHelp();
 	});
 program
-	.command('remove <index>')
+	.command('remove <removeIndex>')
 	.description('remove a to-do element from your list')
-	.action(function(index){
-		handleRequest(1, index);
+	.action(function(removeIndex){
+		remove(removeIndex);
 	});
 program
 	.command('reset')
 	.description('remove a to-do element from your list')
 	.action(function(){
-		handleRequest(2)
+		reset();
 	});
 program
-	.command('update <index> <newElement>')
+	.command('update <updateIndex> <newElement>')
 	.description('update one element from your list')
-	.action(function(index, newElement){
-		handleRequest(3, index, newElement);
+	.action(function(updateIndex, newElement){
+		update(updateIndex, newElement);
 	});
 program
 	.command('list')
 	.description('list the elements to-do from your list')
 	.action(function(){
-		handleRequest(4);
+		list();
 	});
 program
 	.command('help')
@@ -55,6 +53,65 @@ program.parse(process.argv);
 if(process.argv.length <= 2)
 	printHelp();
 
+function writeToFile(buffer){
+	fs.writeFile('to-do.json', buffer, function(error, data){
+		if(error){
+			throw Error(error);
+		}
+	});
+}
+
+function getData(processData){
+	fs.readFile('to-do.json', 'utf8', function(error, data) {
+		if(error){
+			throw Error(error);
+		}
+		else{
+			processData(JSON.parse(data));
+		}
+	});
+}
+
+function add(task){
+	getData(function(toDoData){
+		toDoData.count++;
+		toDoData.tasks.push(task);
+		writeToFile(JSON.stringify(toDoData, null, 2));
+	});
+}
+
+function list(){
+	getData(function(toDoData){
+		console.log(toDoData.tasks.join('\n'));
+	});
+}
+
+function remove(index){
+	if(index <= 0 ){
+		throw Error('the index of the element to remove must be positive(greater than 0)');
+	}
+	getData(function(toDoData){
+		toDoData.tasks.splice(index - 1, 1);
+		toDoData.count--;
+		writeToFile(JSON.stringify(toDoData, null, 2));
+	});
+}
+
+function reset(){
+	getData(function(toDoData){
+		toDoData.tasks = [];
+		toDoData.count = 0;
+		writeToFile(JSON.stringify(toDoData, null, 2));
+	});
+}
+
+function update(index, newElement){
+	getData(function(toDoData){
+		toDoData.tasks.splice(index - 1, 1, newElement);
+		writeToFile(JSON.stringify(toDoData, null, 2));
+	});
+}
+
 function printHelp(){
 	console.log('Commands Help');
 	console.log('help   -------> show help');
@@ -63,57 +120,4 @@ function printHelp(){
 	console.log('list   -------> list the elements to-do from your list');
 	console.log('reset  -------> clean your to-do list');
 	console.log('update -------> update one element from your list');
-}
-
-function writeToFile(buffer){
-	fs.writeFile('to-do.json', buffer, function(error, data){
-		if(error)
-			console.log(error);
-	});
-}
-
-function handleRequest(request, index, updatedElem){ //request = (add, remove, reset, update, list)
-	if(index && index <= 0){
-		throw Error('The index of the element to ' + (request == 1 ? 'remove' : 'update') + ' must be grater than 0.');
-	}
-	fs.readFile('to-do.json', 'utf8', function(error, data) {
-		if(error){
-			console.log(error);
-		}
-		else{
-			let infoObject = JSON.parse(data);
-			let corrected = infoObject.tasks.split('\n');
-			switch(request){
-				case 0://add
-					infoObject.count++;
-					infoObject.tasks += updatedElem + '\n';
-					writeToFile(JSON.stringify(infoObject, null, 2));
-					break;
-				case 1: //remove
-					corrected.splice(index - 1, 1);
-					corrected = corrected.join('\n');
-					infoObject.tasks = corrected;
-					infoObject.count--;
-					writeToFile(JSON.stringify(infoObject, null, 2));
-					break;
-				case 2: //reset
-					infoObject.tasks = '';
-					infoObject.count = 0;	
-					writeToFile(JSON.stringify(infoObject, null, 2));
-					break;
-				case 3: //update
-					corrected.splice(index - 1, 1, updatedElem);
-					corrected = corrected.join('\n');
-					infoObject.tasks = corrected;
-					writeToFile(JSON.stringify(infoObject, null, 2));
-					break;
-				case 4: //list
-					console.log(infoObject.tasks);
-					break;
-				case 5:
-					printHelp();
-					break;
-			}
-		}
-	});
 }
