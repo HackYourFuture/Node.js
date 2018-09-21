9ba2dd2467201c54e8bb7663da0addb7968ed3c0
genegr88 <genegr888@gmail.com>
2018-09-21 13:21:40 +0200

    raw version of homework

---
+++ week3/homework/src/functions.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+
+function add(data, item) {
+    const to_do = JSON.parse(data);
+    const newTaskObject = {
+        index: to_do.length + 1,
+        description: item
+    };
+    to_do.push(newTaskObject);
+
+    fs.writeFile('./to-do.json', JSON.stringify(to_do), (error) => {
+        if (error) { console.log(error); }
+    });
+
+}
+
+function update(data, index, type) {
+    const to_do = JSON.parse(data);
+    to_do[index - 1].done = type;
+
+    fs.writeFile('./to-do.json', JSON.stringify(toDoList), (error) => {
+        if (error) { 
+            console.log(error); 
+        }
+    });
+
+}
+
+function showTask(data, index) {
+    const to_do = JSON.parse(data);
+
+    if (to_do.length === 0) {
+        return { "error": "Your list is empty" };
+    } else if (index > to_do.length) {
+        return { "error": "This task doesn't exist yet" };
+    } else {
+        return to_do[index - 1];
+    }
+}
+
+function reset() {
+    fs.writeFile('./to-do.json', '[]', (error) => {
+        if (error) { 
+            console.log(error); 
+        }
+    });;
+}
+function removeTask(data, index) {
+    const to_do = JSON.parse(data);
+    to_do.splice(index-1, 1)
+    fs.writeFile('./to-do.json', JSON.stringify(to_do)), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    }
+}
+
+
+module.exports = {
+    add: add,
+    update: update,
+    showTask: showTask,
+    reset: reset,
+    removeTask: removeTask
+}
--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,63 @@
 'use strict';
 
-// TODO: Write the homework code in this file
+const fs = require('fs');
+const express =  require('express');
+const bodyParser = require('body-parser');
+const functions = require('./functions.js')
+const app = express();
+app.use(bodyParser.json());
+
+//we need to read the file first in order to use data but it doesn't work...
+
+app.get('/todos/:id', (request, response) => {
+    const index = request.params.id;
+    response
+        .status(200)
+        .json(functions.showTask(data, index));
+});
+app.delete('/todos', (request, response) => {
+    functions.reset();
+    response
+        .status(301)
+        .json({ result: "Your list has been cleaned" });
+});
+app.post('/todos', (request, response) => {
+    functions.add(data, request.body.task);
+    response
+        .status(201)
+        .json({ result: "added new task" });
+
+});
+app.put('/todos/:id/done', (request, response) => {
+    const index = request.params.id;
+    functions.update(data, index, true);
+    response
+        .status(201)
+        .json({ result: "Your task is done" });
+
+});
+app.put('/todos/:id/undone', (request, response) => {
+    const index = request.params.id;
+    manipulate.update(data, index, false);
+    response
+        .status(201)
+        .json({ result: "Your task is undone" });
+
+});
+app.get('/todos', (request, response) => {
+    fs.readFile("to-do.json", "utf8", (error,data) => {
+        if (error) {
+            console.log(error)
+            response
+            .status(500)
+            .json({result: error.message})
+        } else {
+            response
+            .status(200)
+            .json(data)
+        }
+    })
+})
+app.listen(3030, () => {
+    console.log("Listening on the port 3030")
+})
