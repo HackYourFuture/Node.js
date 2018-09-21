c0c410519d481bd71e2bffc258c57f1b2d9ab933
ossama <vx.dn@live.com>
2018-09-21 02:17:12 +0200

    feedback fixed

--- week2/homework/src/index1.js
+++ week2/homework/src/index1.js
@@ -1,78 +1,94 @@
 'use strict';
 
-// TODO: Write the homework code in this file
 const fs = require('fs');
 const readFile = fs.readFileSync('./data.json', 'utf8');
 const readindex = fs.readFileSync('./index.txt', 'utf8');
 var json = JSON.parse(readFile);
-
-
-const cmd = process.argv[2]
+const cmd = process.argv[2];
 const todo = process.argv[3];
 const todo2 = process.argv[4];
+
 switch (cmd) {
     case 'add':
         add(todo);
         break;
     case 'remove':
-        remove(todo)
+        remove(todo);
         break;
     case 'list':
-        todolist()
+        todolist();
         break;
     case 'reset':
-        reset()
+        reset();
         break;
     case 'update':
-        update(todo, todo2)
+        update(todo, todo2);
         break;
     case 'help':
-        help()
+        help();
         break;
-
-
 }
 
 function add(todo) {
     const index = parseInt(readindex) + 1;
-    const key = index;
     const obj = {};
-    obj[key] = todo;
-    Object.assign(json.todo, obj);
-    fs.writeFileSync('./data.json', JSON.stringify(json, null, 2))
-    fs.writeFileSync('index.txt', index)
+    obj.text = todo;
+    json.todo.push(obj);
+    fs.writeFileSync('./data.json', JSON.stringify(json, null, 2));
+    fs.writeFileSync('index.txt', index);
 
 };
 
-
-function remove(todo) {
-    delete json.todo[todo]
-    fs.writeFileSync('./data.json', JSON.stringify(json, null, 2))
-    console.log('todo ' + todo + ' has been deleted')
-}
+function remove(key) {
+    if (todo < 0) {
+        console.log('input should be positive number');
+    } else if (json.todo.length === 0) {
+        console.log('there is nothing in your todo list !')
+    } else if (json.todo.length < key) {
+        console.log('incorrect todo number')
+    } else {
+        json.todo.splice(0, 0, '1');
+        json.todo.splice(todo, 1);
+        json.todo.splice(0, 1);
+        fs.writeFileSync('./data.json', JSON.stringify(json, null, 2));
+        console.log('todo ' + todo + ' has been deleted');
+    };
+};
 
 function todolist() {
-    const todosValues = Object.values(json.todo)
-
-    if (todosValues.length === 0) {
+    if (json.todo.length === 0) {
         console.log('you have nothing to do :P ! ');
     } else {
-        console.log(todosValues.join('\n'))
+        json.todo.forEach((element, index) => {
+            index = index + 1;
+            console.log(index + ' ' + element.text);
+        });
     }
 }
 
 function reset() {
-    let emptyTodoFile = { "todo": {} }
+    let emptyTodoFile = { "todo": [] }
     fs.writeFileSync('./data.json', JSON.stringify(emptyTodoFile, null, 2))
     fs.writeFileSync('./index.txt', 0)
+    console.log('you have reset your todo list')
 }
 
-
 function update(key, todo) {
-    const obj = {};
-    obj[key] = todo;
-    Object.assign(json.todo, obj);
-    fs.writeFileSync('./data.json', JSON.stringify(json, null, 2))
+    if (key < 0) {
+        console.log('input should be positive number');
+    } else if (json.todo.length === 0) {
+        console.log('there is nothing in your todo list !')
+    } else if (json.todo.length < key) {
+        console.log('there is no todo with the same input number')
+    } else {
+        const obj = {};
+        obj.text = todo;
+        json.todo.splice(0, 0, '1')
+        json.todo.splice(key, 1, obj)
+        json.todo.splice(0, 1);
+        fs.writeFileSync('./data.json', JSON.stringify(json, null, 2))
+        console.log('todo ' + key + 'updated')
+    }
 }
 
 function help() {
