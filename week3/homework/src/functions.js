976eab4234f555620c27d79c818ef52042ffa527
genegr88 <genegr888@gmail.com>
2018-09-22 11:56:54 +0200

    homework week3 done

--- week3/homework/src/functions.js
+++ week3/homework/src/functions.js
@@ -1,73 +1,69 @@
 const fs = require('fs');
 
 function add(data, item) {
-    const to_do = JSON.parse(data);
-    const lastObject =to_do[to_do.length - 1];
-        let counter = 0;
-        if (lastObject === undefined) {
-            counter;
-        } else {
-            counter = parseInt(lastObject.id) + 1;
-        }
-   const newTaskObject = {
-       index: counter,
-       description: item
-   };
-   to_do.push(newTaskObject);
+  const lastObject = data[data.length - 1];
+  let counter = 0;
+  if (lastObject === undefined) {
+    counter = 0;
+  }
+  else {
+    counter = parseInt(lastObject.id) + 1;
+  }
+  const newTaskObject = {
+    index: counter,
+    description: item
+  };
+  data.push(newTaskObject);
 
-   fs.writeFile('./to-do.json', JSON.stringify(to_do), (error) => {
-            if (err) throw err;
-            console.log(data);
-   });
-
+  fs.writeFile('./to-do.json', JSON.stringify(data), (error) => {
+    if (error) throw error;
+    console.log(data);
+  });
 }
 
 function update(data, index, type) {
-    const to_do = JSON.parse(data);
-    to_do[index - 1].done = type;
+  const todos = JSON.parse(data);
+  todos[index - 1].done = type;
 
-    fs.writeFile('./to-do.json', JSON.stringify(toDoList), (error) => {
-        if (error) { 
-            console.log(error); 
-        }
-    });
-
+  fs.writeFile('./to-do.json', JSON.stringify(todos), (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
 }
 
 function showTask(data, index) {
-    const to_do = JSON.parse(data);
-
-    if (to_do.length === 0) {
-        return { "error": "Your list is empty" };
-    } else if (index > to_do.length) {
-        return { "error": "This task doesn't exist yet" };
-    } else {
-        return to_do[index - 1];
-    }
+  if (data.length === 0) {
+    return { 'error': 'Your list is empty' };
+  }
+  else if (index > data.length) {
+    return { 'error': "This task doesn't exist yet" };
+  }
+  else {
+    return data[index - 1];
+  }
 }
 
 function reset() {
-    fs.writeFile('./to-do.json', '[]', (error) => {
-        if (error) { 
-            console.log(error); 
-        }
-    });;
+  fs.writeFile('./to-do.json', '[]', (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
 }
 function removeTask(data, index) {
-    const to_do = JSON.parse(data);
-    to_do.splice(index-1, 1)
-    fs.writeFile('./to-do.json', JSON.stringify(to_do)), (error) => {
-        if (error) {
-            console.log(error);
-        }
+  data.splice(index - 1, 1);
+  fs.writeFile('./to-do.json', JSON.stringify(data), (error) => {
+    if (error) {
+      console.log(error);
     }
+  });
 }
-
 
 module.exports = {
-    add: add,
-    update: update,
-    showTask: showTask,
-    reset: reset,
-    removeTask: removeTask
-}
+  add: add,
+  update: update,
+  showTask: showTask,
+  reset: reset,
+  removeTask: removeTask
+};
--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,63 +1,99 @@
 'use strict';
 
 const fs = require('fs');
-const express =  require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const functions = require('./functions.js')
+const functions = require('./functions.js');
 const app = express();
 app.use(bodyParser.json());
 
-//we need to read the file first in order to use data but it doesn't work...
-
 app.get('/todos/:id', (request, response) => {
-    const index = request.params.id;
+  const index = request.params.id;
+  fs.readFile('./src/to-do.json', 'utf8', (error, data) => {
+    const todos = data ? JSON.parse(data) : [];
+    if (error) {
+      console.log(error);
+      response
+        .status(500)
+        .json({ result: 'error' });
+      return;
+    }
     response
-        .status(200)
-        .json(functions.showTask(data, index));
+      .status(200)
+      .json(functions.showTask(todos, index));
+  });
 });
 app.delete('/todos', (request, response) => {
-    functions.reset();
-    response
-        .status(301)
-        .json({ result: "Your list has been cleaned" });
+  functions.reset();
+  response
+    .status(301)
+    .json({ result: 'Your list has been cleaned' });
 });
 app.post('/todos', (request, response) => {
-    functions.add(data, request.body.task);
+  fs.readFile('./src/to-do.json', 'utf8', (error, data) => {
+    const todos = data ? JSON.parse(data) : [];
+    if (error) {
+      console.log(error);
+      response
+        .status(500)
+        .json({ result: 'error' });
+      return;
+    }
+    functions.add(todos, request.body.task);
     response
-        .status(201)
-        .json({ result: "added new task" });
-
+      .status(201)
+      .json({ result: 'added new task' });
+  });
 });
 app.put('/todos/:id/done', (request, response) => {
-    const index = request.params.id;
-    functions.update(data, index, true);
+  const index = request.params.id;
+  fs.readFile('./src/to-do.json', 'utf8', (error, data) => {
+    const todos = data ? JSON.parse(data) : [];
+    if (error) {
+      console.log(error);
+      response
+        .status(500)
+        .json({ result: 'error' });
+      return;
+    }
+    functions.update(todos, index, true);
     response
-        .status(201)
-        .json({ result: "Your task is done" });
-
+      .status(201)
+      .json({ result: 'Your task is done' });
+  });
 });
 app.put('/todos/:id/undone', (request, response) => {
-    const index = request.params.id;
-    manipulate.update(data, index, false);
+  const index = request.params.id;
+  fs.readFile('./src/to-do.json', 'utf8', (error, data) => {
+    const todos = data ? JSON.parse(data) : [];
+    if (error) {
+      console.log(error);
+      response
+        .status(500)
+        .json({ result: 'error' });
+      return;
+    }
+    functions.update(todos, index, false);
     response
-        .status(201)
-        .json({ result: "Your task is undone" });
-
+      .status(201)
+      .json({ result: 'Your task is undone' });
+  });
 });
 app.get('/todos', (request, response) => {
-    fs.readFile("to-do.json", "utf8", (error,data) => {
-        if (error) {
-            console.log(error)
-            response
-            .status(500)
-            .json({result: error.message})
-        } else {
-            response
-            .status(200)
-            .json(data)
-        }
-    })
-})
+  fs.readFile('./src/to-do.json', 'utf8', (error, data) => {
+    const todos = data ? JSON.parse(data) : [];
+    if (error) {
+      console.log(error);
+      response
+        .status(500)
+        .json({ result: 'error' });
+      return;
+    }
+    response
+      .status(200)
+      .json(todos);
+  });
+});
 app.listen(3030, () => {
-    console.log("Listening on the port 3030")
-})
+  console.log('Listening on the port 3030');
+});
