74045422bd481b14515096eee2a1cb84f2d59f02
salar <salar.demok@gmail.com>
2018-11-14 12:21:46 +0100

    week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,8 +10,36 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
-  });
 
+    if (request.url === '/state') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ 'state': state }));
+      response.end();
+    }
+    else if (request.url === '/add') {
+      state++;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ 'state': state }));
+      response.end();
+    }
+    else if (request.url === '/subtract') {
+      state--;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ 'state': state }));
+      response.end();
+    }
+    else if (request.url === '/reset') {
+      state = 10;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ 'state': state }));
+      response.end();
+    }
+    else {
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ 'error': 'Not found' }));
+      response.end();
+    }
+  });
   return server;
 }
 
