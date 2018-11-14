d38a458608f5091cec869a8179ee6ab57018a176
joudi6 <joudi.marjana2@gmail.com>
2018-11-14 23:05:45 +0100

    h.w week 1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const http = require('http');
-
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
@@ -10,8 +9,35 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    if (request.url === '/state') {
+      response.writeHead(200, { 'content-type': 'application/json' });
+      response.write(JSON.stringify({ state }));
+      response.end();
+    }
+    else if (request.url === '/add') {
+      response.writeHead(200, { 'content-type': 'application/json' });
+      state++;
+      response.write(JSON.stringify({ state }));
+      response.end();
+    }
+    else if (request.url === '/subtract') {
+      response.writeHead(200, { 'content-type': 'application/json' });
+      state--;
+      response.write(JSON.stringify({ state }));
+      response.end();
+    }
+    else if (request.url === '/reset') {
+      response.writeHead(200, { 'content-type': 'application/json' });
+      state = 10;
+      response.write(JSON.stringify({ state }));
+      response.end();
+    }
+    else {
+      response.writeHead(404, { 'content-type': 'application/json' });
+      response.write(JSON.stringify({ 'error': 'Not found' }));
+      response.end();
+    }
   });
-
   return server;
 }
 
