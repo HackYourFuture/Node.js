ffab92bd01d4f965c8e1b0316889d2f083c81a79
joudi6 <joudi.marjana2@gmail.com>
2018-11-15 19:03:29 +0100

    updating week1 H.w

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,34 +9,34 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
-    if (request.url === '/state') {
-      response.writeHead(200, { 'content-type': 'application/json' });
-      response.write(JSON.stringify({ state }));
+    function GivResponse(dataValue, state) {
+      response.writeHead(dataValue, { 'content-type': 'application/json' });
+      response.write(JSON.stringify(state));
       response.end();
     }
-    else if (request.url === '/add') {
-      response.writeHead(200, { 'content-type': 'application/json' });
-      state++;
-      response.write(JSON.stringify({ state }));
-      response.end();
+    switch (request.url) {
+      case '/state':
+        GivResponse(200, { 'state': state });
+        break;
+
+      case '/add':
+        state++;
+        GivResponse(200, { 'state': state });
+        break;
+
+      case '/subtract':
+        state--;
+        GivResponse(200, { 'state': state });
+        break;
+
+      case '/reset':
+        state = 10;
+        GivResponse(200, { 'state': state });
+        break;
+      default:
+        GivResponse(404, { 'error': 'Not found' });
     }
-    else if (request.url === '/subtract') {
-      response.writeHead(200, { 'content-type': 'application/json' });
-      state--;
-      response.write(JSON.stringify({ state }));
-      response.end();
-    }
-    else if (request.url === '/reset') {
-      response.writeHead(200, { 'content-type': 'application/json' });
-      state = 10;
-      response.write(JSON.stringify({ state }));
-      response.end();
-    }
-    else {
-      response.writeHead(404, { 'content-type': 'application/json' });
-      response.write(JSON.stringify({ 'error': 'Not found' }));
-      response.end();
-    }
+
   });
   return server;
 }
