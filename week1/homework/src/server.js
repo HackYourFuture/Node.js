9aca24d2a31c04f9f8a86fab3e3d8298e358d521
heboshka <heboshka@hotmail.com>
2018-11-17 18:11:53 +0100

    Hiba HW1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -2,14 +2,40 @@
 
 const http = require('http');
 
-/* `createServer` MUST return an instance of `http.Server` otherwise the tests
- * will fail.
- */
 function createServer(port) {
   let state = 10;
 
+  function showState(response, state) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify({ 'state': state }));
+    response.end();
+  }
+
+  function showError(response) {
+    response.writeHead(404, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify({ 'error': 'Not found' }));
+    response.end();
+  }
+
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        showState(response, state);
+        break;
+      case '/add':
+        showState(response, ++state);
+        break;
+      case '/subtract':
+        showState(response, --state);
+        break;
+      case '/reset':
+        state = 10;
+        showState(response, state);
+        break;
+      default:
+        showError(response);
+        break;
+    }
   });
 
   return server;
