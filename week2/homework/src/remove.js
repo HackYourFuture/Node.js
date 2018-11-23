81d8314a59dfc07649c9145a033a8c25e6bea4cc
RimSebai <40184200+RimSebai@users.noreply.github.com>
2018-11-23 19:56:30 +0100

    Update remove.js

--- week2/homework/src/remove.js
+++ week2/homework/src/remove.js
@@ -1,19 +1,14 @@
 const fs = require('fs');
-const stringTo_dos = fs.readFileSync("./to-dos.json", "utf-8");
-const to_dos = JSON.parse(stringTo_dos);
-const writeData = require('./data');
+const data = require('./data');
 
 let r = [];
 const removeData = a =>
-  a == 1 || a == to_dos.length ?
-    r = to_dos.splice((a - 1), 1) &&
-    writeData.writeData(to_dos)
+  a == 1 || a == data.to_dos.length ?
+    r = data.to_dos.splice((a - 1), 1) &&
+    data.writeData(data.to_dos)
     :
     console.log("there is no item has this index to remove!")
 
-module.exports = {
-  removeData,
-  stringTo_dos,
-  to_dos
-}
+module.exports = removeData;
+
 
