82b5073d79b052fe41d51207cb75c05b3d58bf92
RimSebai <40184200+RimSebai@users.noreply.github.com>
2018-11-23 19:57:10 +0100

    Update update.js

--- week2/homework/src/update.js
+++ week2/homework/src/update.js
@@ -1,19 +1,14 @@
 const fs = require('fs');
-const stringTo_dos = fs.readFileSync("./to-dos.json", "utf-8");
-const to_dos = JSON.parse(stringTo_dos);
-const writeData = require('./data');
+const data = require('./data');
 
 let d = [];
 const updateData = (a, b) =>
-  a == 1 || a == to_dos.length ?
-    d = to_dos.splice((a - 1), 1, b) &&
-    writeData.writeData(to_dos)
+  a == 1 || a == data.to_dos.length ?
+    d = data.to_dos.splice((a - 1), 1, b) &&
+    data.writeData(data.to_dos)
     :
     console.log("there is no item to update!")
 
-module.exports = {
-  updateData,
-  stringTo_dos,
-  to_dos
-}
+module.exports = updateData;
+
 
