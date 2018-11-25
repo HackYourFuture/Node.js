5686d764009325a7f3756000af94079805f9070e
RimSebai <40184200+RimSebai@users.noreply.github.com>
2018-11-25 21:24:38 +0100

    Update remove.js
    I changed the condition

--- week2/homework/src/remove.js
+++ week2/homework/src/remove.js
@@ -3,7 +3,7 @@ const data = require('./data');
 
 let r = [];
 const removeData = a =>
-  a == 1 || a == data.to_dos.length ?
+  a > 1 && a <= data.to_dos.length ?
     r = data.to_dos.splice((a - 1), 1) &&
     data.writeData(data.to_dos)
     :
