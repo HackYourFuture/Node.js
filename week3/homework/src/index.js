54ac1373516964e6c13e730265822cf3115678a2
karam <karam.madrid.7@gmail.com>
2018-12-04 14:18:21 +0100

    fixed hw

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -15,83 +15,79 @@ const PORT = 3000;
 app.use(bodyParser());
 
 app.get('/' || '/help', (function (req, res) {
-  res.send(help);
+   res.send(help);
 }))
 
 app.route('/todo')
 
-  .get(function (req, res) {
-    res.send(file);
-  })
+   .get(function (req, res) {
+      res.send(list);
+   })
 
-  .post(function (req, res) {
-    res.send(file);
-    let newToDo = req.body;
-    newToDo.id = uuid();
-    newToDo.done = false;
-    list.push(newToDo);
-    writeData(list, FILENAME);
-  })
+   .post(function (req, res) {
+      res.send(file);
+      let newToDo = req.body;
+      newToDo.id = uuid();
+      newToDo.done = false;
+      list.push(newToDo);
+      writeData(list, FILENAME);
+   })
 
-  .delete(function (req, res) {
-    res.send(file);
-    list.length = 0;
-    writeData(list, FILENAME);
-  })
+   .delete(function (req, res) {
+      res.send(list);
+      list.length = 0;
+      writeData(list, FILENAME);
+   })
 
 app.route('/todo/:id')
 
-  .delete(function (req, res) {
-    let newToDo = req.body;
-    res.send(file);
-    if (req.params.id) {
-      newToDo = list.filter(list => list.id != req.params.id);
-      writeData(newToDo, FILENAME);
-    } else (console.log('item does not exist'));
-  })
+   .delete(function (req, res) {
+      res.send(list);
+      if (req.params.id) {
+         list.filter(list => list.id != req.params.id);
+         writeData(list, FILENAME);
+      } else (console.log('item does not exist'));
+   })
 
-  .put(function (req, res) {
-    let newToDo = req.body;
-    res.send(list);
-    if (req.params.id) {
+   .put(function (req, res) {
+      res.send(list);
+      if (req.params.id) {
+         const objIndex = list.findIndex(obj => obj.id == req.params.id);
+         list[objIndex].description = req.body.description;
+         writeData(list, FILENAME);
+      } else (console.log('item does not exist!'));
+   })
+
+   .get(function (req, res) {
       const objIndex = list.findIndex(obj => obj.id == req.params.id);
-      list[objIndex].description = req.body.description;
-      writeData(list, FILENAME);
-    } else (console.log('item does not exist!'));
-  })
-
-  .get(function (req, res) {
-    const objIndex = list.findIndex(obj => obj.id == req.params.id);
-    res.send(list[objIndex]);
-  })
+      res.send(list[objIndex]);
+   })
 
 app.route('/todo/:id/done')
 
-  .post(function (req, res) {
-    let newToDo = req.body;
-    res.send(list);
-    if (req.params.id) {
-      const objIndex = list.findIndex(obj => obj.id == req.params.id);
-      list[objIndex].done = !list[objIndex].done;
-      writeData(list, FILENAME);
-    } else (console.log('item does not exist!'));
+   .post(function (req, res) {
+      res.send(list);
+      if (req.params.id) {
+         const objIndex = list.findIndex(obj => obj.id == req.params.id);
+         list[objIndex].done = !list[objIndex].done;
+         writeData(list, FILENAME);
+      } else (console.log('item does not exist!'));
 
-  })
+   })
 
-  .delete(function (req, res) {
-    let newToDo = req.body;
-    res.send(list);
-    if (req.params.id) {
-      const objIndex = list.findIndex(obj => obj.id == req.params.id);
-      list[objIndex].done = false;
-      writeData(list, FILENAME);
-    } else (console.log('item does not exist!'));
+   .delete(function (req, res) {
+      res.send(list);
+      if (req.params.id) {
+         const objIndex = list.findIndex(obj => obj.id == req.params.id);
+         list[objIndex].done = false;
+         writeData(list, FILENAME);
+      } else (console.log('item does not exist!'));
 
-  })
+   })
 
 app.listen(PORT, error => {
-  if (error)
-    return console.error(error);
+   if (error)
+      return console.error(error);
 
-  console.log(`Server started on http://localhost:${PORT}`);
+   console.log(`Server started on http://localhost:${PORT}`);
 });
