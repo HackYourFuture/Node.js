3102bcb804dfb11859f904af0c25a6caf98cc88f
Wael-Alhomsi <waelhom2@gmail.com>
2019-01-09 00:36:45 +0100

    added week1 homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,11 +10,43 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+
+    function sendResponse(stateValue) {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ state: stateValue }));
+      response.end();
+    }
+
+    switch (request.url) {
+      case '/state':
+        sendResponse(state);
+        break;
+
+      case '/add':
+        state += 1;
+        sendResponse(state);
+        break;
+
+      case '/subtract':
+        state -= 1;
+        sendResponse(state);
+        break;
+
+      case '/reset':
+        state = 10;
+        sendResponse(state);
+        break;
+
+      default:
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({ error: 'Not found' }));
+        response.end();
+    }
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
