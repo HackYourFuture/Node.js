8e82c40e16a2f587f7f3416edae3839dce420295
mohamad monir <mohamadmonirhamid@gmail.com>
2019-01-08 15:08:45 +0100

    node.js-cls18-wk1

--- week1/homework/src/index.js
+++ week1/homework/src/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const {
-  createServer
-} = require('./server');
+const { createServer } = require('./server');
 
 const PORT = 3000;
 
-createServer().listen(PORT, () => {
+createServer(PORT).listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,9 +10,34 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
-  });
 
+    switch (request.url) {
+      case '/':
+        sendResponse(200, `this is ${state}`, response);
+        break;
+      case '/add':
+        sendResponse(200, `the state now is ${++state}`, response);
+        break;
+      case '/subtract':
+        sendResponse(200, `the state now is ${--state}`, response);
+        break;
+      case '/reset':
+        sendResponse(200, `the state return to ${(state = 10)}`, response);
+        break;
+      default:
+        sendResponse(404, `not found`, response);
+    }
+  });
   return server;
+}
+function sendResponse(statusNum, content, response) {
+  response.statusCode = statusNum;
+  response.setHeader('content-type', 'application/json');
+  response.write(content);
+  let myObj = {
+    content
+  };
+  response.end(JSON.stringify(myObj));
 }
 
 module.exports = {
