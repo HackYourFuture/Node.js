63f5636a4922a814f541e23e85ce93459b8ee58c
Yaacob <yyaacob0@gmail.com>
2019-01-08 21:14:25 +0100

    commiting homework for the week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,11 +10,29 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    function getResponse(statusCode, state) {
+      response.writeHead(statusCode, { 'content-type': 'application/json' });
+      response.write(JSON.stringify(state));
+      response.end();
+    }
+    if (request.url === '/state') {
+      getResponse(200, { state });
+    } else if (request.url === '/add') {
+      state++;
+      getResponse(200, { state });
+    } else if (request.url === '/subtract') {
+      state--;
+      getResponse(200, { state });
+    } else if (request.url === '/reset') {
+      state = 10;
+      getResponse(200, { state });
+    } else {
+      getResponse(404, { error: 'Not found' });
+    }
   });
-
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer: createServer,
 };
