0a90216c956b8a94715acda864e94ca09faa834b
ZerSolo <yosiami15@gmail.com>
2019-01-09 00:23:27 +0100

    HW

--- week1/homework/src/index.js
+++ week1/homework/src/index.js
@@ -4,7 +4,7 @@ const {
   createServer
 } = require('./server');
 
-const PORT = 3000;
+const PORT = 4000;
 
 createServer().listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,16 +5,44 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
+
+function sendResponse(response, state, statuscode) {
+  response.statusCode = statuscode;
+  response.setHeader('Content-type', 'application/json');
+  response.write(state.toString());
+  response.end();
+}
+
 function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/':
+        sendResponse(response, 'Home', 200);
+        break;
+      case '/state':
+        sendResponse(response, state, 200);
+        break;
+      case '/add':
+        state++;
+        sendResponse(response, state, 200);
+        break;
+      case '/subtract':
+        state--;
+        sendResponse(response, state, 200);
+        break;
+      case '/reset':
+        state = 10;
+        sendResponse(response, state, 200);
+        break;
+      default:
+        sendResponse(response, '404, Not Found ', 404);
+        break;
+    }
   });
-
   return server;
 }
-
 module.exports = {
   createServer
 };
