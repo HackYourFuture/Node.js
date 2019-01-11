690b94f3bf24cefd0c3bd20a9b40053179783926
mohamad monir <mohamadmonirhamid@gmail.com>
2019-01-11 12:38:52 +0100

    minor adjustement

--- week1/homework/src/index.js
+++ week1/homework/src/index.js
@@ -4,6 +4,6 @@ const { createServer } = require('./server');
 
 const PORT = 3000;
 
-createServer(PORT).listen(PORT, () => {
+createServer().listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,36 +10,34 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
-
+    function sendResponse(statusNum, content) {
+      response.statusCode = statusNum;
+      response.writeHeader(200, { 'content-type': 'application/json' });
+      statusNum === 200
+        ? response.write(JSON.stringify({ state: content }))
+        : response.write(JSON.stringify({ 404: 'notfound' }));
+      response.end();
+    }
     switch (request.url) {
       case '/':
-        sendResponse(200, `this is ${state}`, response);
+        sendResponse(200, state);
         break;
       case '/add':
-        sendResponse(200, `the state now is ${++state}`, response);
+        sendResponse(200, ++state);
         break;
       case '/subtract':
-        sendResponse(200, `the state now is ${--state}`, response);
+        sendResponse(200, --state);
         break;
       case '/reset':
-        sendResponse(200, `the state return to ${(state = 10)}`, response);
+        sendResponse(200, (state = 10));
         break;
       default:
-        sendResponse(404, `not found`, response);
+        sendResponse(404, `not found`);
     }
   });
   return server;
 }
-function sendResponse(statusNum, content, response) {
-  response.statusCode = statusNum;
-  response.setHeader('content-type', 'application/json');
-  response.write(content);
-  let myObj = {
-    content
-  };
-  response.end(JSON.stringify(myObj));
-}
 
 module.exports = {
-  createServer
+  createServer,
 };
