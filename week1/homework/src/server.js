e1ae93082189a1f2f05bc092ca43002234b14d9d
kaniwar103 <kaniwar103@gamil.com>
2019-01-12 22:18:25 +0100

    homework week 1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,6 +10,36 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    const requestUrl = request.url;
+    if (request.url === '/state') {
+      response.statusCode = 200;
+      response.setHeader('content-Type', 'application/json');
+      response.end(JSON.stringify({ state }));
+      // console.log(JSON.stringify({ state: state + 1 }));
+    }
+    else
+    if (request.url === '/add') {
+      response.statusCode = 200;
+      response.setHeader('content-Type', 'application/json');
+      response.end(JSON.stringify({ state: ++state }));
+    }
+    else
+    if (request.url === '/subtract') {
+      response.statusCode = 200;
+      response.setHeader('content-Type', 'application/json');
+      response.end(JSON.stringify({ state: --state }));
+    }
+    else
+    if (request.url === '/reset') {
+      response.statusCode = 200;
+      response.setHeader('content-Type', 'application/json');
+      response.end(JSON.stringify({ state: state = 10 }));
+    }
+    else {
+      response.statusCode = 404;
+      response.setHeader('content-Type', 'application/json');
+      response.end(JSON.stringify({ 'error': 'Not found' }));
+    }
   });
 
   return server;
