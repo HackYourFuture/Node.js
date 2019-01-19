1483856f1c9f93ccc3a9db96f1eb3e4a3df89e62
mohamad monir <mohamadmonirhamid@gmail.com>
2019-01-19 16:22:24 +0100

    some modifications

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -4,19 +4,27 @@
 const fs = require('fs');
 let command = process.argv[2];
 let command1 = process.argv[3];
-let add = () => {
-  fs.readFileSync('todo.txt', 'utf8');
-  fs.appendFileSync('todo.txt', `${command1}\n`);
+function readFiles(fileName) {
+  const file = fs.readFileSync(fileName, 'utf8');
+  return file;
+}
+function writeFiles(fileName, data) {
+  const file = fs.writeFileSync(fileName, data);
+  return file;
+}
+let add = targetedFile => {
+  readFiles(targetedFile);
+  fs.appendFileSync(targetedFile, `${command1}\n`);
   return command1;
 };
 
-let list = () => {
-  let file = fs.readFileSync('todo.txt', 'utf8');
+let list = targetedFile => {
+  let file = readFiles(targetedFile);
   console.log(file);
 };
 
-let remove = () => {
-  let file = fs.readFileSync('todo.txt', 'utf8');
+let remove = targetedFile => {
+  let file = readFiles(targetedFile);
   let arr = file.split('\n');
   if (command1 > arr.length) {
     console.log(`check please the number of the to-dos
@@ -26,31 +34,32 @@ let remove = () => {
  else {
     arr.splice(command1 - 1, 1).join('\n');
     console.log(arr);
-    fs.writeFileSync('todo.txt', arr.join('\n'));
+    writeFiles(targetedFile, arr.join('\n'));
   }
 };
-let reset = () => {
-  return fs.writeFileSync('todo.txt', '');
+let reset = targetedFile => {
+  return writeFiles(targetedFile, '');
 };
-
-if (command === 'help') {
-  console.log(
-    `welcome to the CLI to-dos list, you can use the following command :
-     1- list (to view the to-do list)
-     2- add (add to the list) 
-     3- remove (to remove one of the to-do) 
-     4- reset (to remove all)`
-  );
-}
- else if (command === 'list') {
-  console.log(list());
-}
- else if (command === 'add') {
-  console.log(add());
-}
- else if (command === 'remove') {
-  console.log(remove());
-}
- else if (command === 'reset') {
-  console.log(reset());
+switch (command) {
+  case 'help':
+    console.log(
+      `welcome to the CLI to-dos list, you can use the following command :
+       1- list (to view the to-do list)
+       2- add (add to the list) 
+       3- remove (to remove one of the to-do) 
+       4- reset (to remove all)`
+    );
+    break;
+  case 'list':
+    console.log(list('todo.txt'));
+    break;
+  case 'add':
+    console.log(add('todo.txt'));
+    break;
+  case 'remove':
+    remove('todo.txt');
+    break;
+  case 'reset':
+    console.log(reset('todo.txt'));
+    break;
 }
