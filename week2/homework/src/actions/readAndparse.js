28dccb0aa1cb84ace2a62875270257418196a143
ZerSolo <yosiami15@gmail.com>
2019-01-20 22:39:29 +0100

    modify readAndparse.js

--- week2/homework/src/actions/readAndparse.js
+++ week2/homework/src/actions/readAndparse.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 const readAndParseTodos = () => {
   const todosList = fs.readFileSync('todos.json', 'utf8');
   return JSON.parse(todosList);
-}
-module.exports = readAndParseTodos
+};
+module.exports = readAndParseTodos;
