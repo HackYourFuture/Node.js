4cf3c5005a1fba4a1dd3645c6116f97502bc5397
Wael-Alhomsi <waelhom2@gmail.com>
2019-01-24 22:36:40 +0100

    added homework

---
+++ week3/homework/src/actions.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const fs = require('fs');
+const uuid = require('uuid');
+
+function stringifyAndWrite(arr) {
+  fs.writeFileSync('./data.json', JSON.stringify(arr));
+}
+
+function readAndParseData() {
+  const string = fs.readFileSync('./data.json', 'utf8');
+  return JSON.parse(string);
+}
+
+function readF() {
+  return fs.readFileSync('./data.json', 'utf8');
+}
+
+function addToList(toDo) {
+  const toDoObject = {
+    task: toDo,
+    id: uuid(),
+    done: false,
+  };
+  const toDosArray = readAndParseData();
+  toDosArray.push(toDoObject);
+  stringifyAndWrite(toDosArray);
+}
+
+function findIndex(id, arr) {
+  return arr.findIndex(obj => obj.id === id);
+}
+
+function removeFromList(toDoId) {
+  const toDosArray = readAndParseData();
+  const index = findIndex(toDoId, toDosArray);
+  if (index === -1) {
+    return 'The list has no corresponding "to-do element"';
+  } else {
+    toDosArray.splice(index, 1);
+    stringifyAndWrite(toDosArray);
+  }
+}
+
+function readSingleToDo(toDoId) {
+  const toDosArray = readAndParseData();
+  const index = findIndex(toDoId, toDosArray);
+  if (index === -1) {
+    return 'The list has no corresponding "to-do element"';
+  } else {
+    return JSON.stringify(toDosArray[index]);
+  }
+}
+
+function markDone(toDoId) {
+  const toDosArray = readAndParseData();
+  const index = findIndex(toDoId, toDosArray);
+  if (index === -1) {
+    return 'The list has no corresponding "to-do element"';
+  } else {
+    if (toDosArray[index].done === 'true') {
+      return 'The task is already done.';
+    } else {
+      toDosArray[index].done = 'true';
+      stringifyAndWrite(toDosArray);
+    }
+  }
+}
+
+function markUnDone(toDoId) {
+  const toDosArray = readAndParseData();
+  const index = findIndex(toDoId, toDosArray);
+  if (index === -1) {
+    return 'The list has no corresponding "to-do element"';
+  } else {
+    if (toDosArray[index].done === 'false') {
+      return 'The task is already not done.';
+    } else {
+      toDosArray[index].done = 'false';
+      stringifyAndWrite(toDosArray);
+    }
+  }
+}
+
+function updateList(toDoId, updatedTask) {
+  const toDosArray = readAndParseData();
+  const index = findIndex(toDoId, toDosArray);
+  if (index === -1) {
+    return 'The list has no corresponding "to-do element"';
+  } else {
+    toDosArray[index].task = updatedTask;
+    stringifyAndWrite(toDosArray);
+  }
+}
+
+module.exports = {
+  addToList,
+  removeFromList,
+  stringifyAndWrite,
+  readF,
+  markUnDone,
+  markDone,
+  readSingleToDo,
+  updateList,
+};
--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,83 @@
 'use strict';
 
-// TODO: Write the homework code in this file
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {
+  addToList,
+  removeFromList,
+  stringifyAndWrite,
+  readF,
+  markUnDone,
+  markDone,
+  readSingleToDo,
+  updateList,
+} = require('./actions.js');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/todos', (req, res) => {
+  res.send(readF());
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(readSingleToDo(id));
+});
+
+app.post('/todos', (req, res) => {
+  addToList(req.body.todo.description);
+  res.send(readF());
+});
+
+app.post('/todos/:id/:action', (req, res) => {
+  const id = req.params.id;
+  if (req.params.action === 'done') {
+    const returnedValue = markDone(id);
+    if (returnedValue === undefined) {
+      res.send(readF());
+    } else {
+      res.send(returnedValue);
+    }
+  }
+});
+
+app.put('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const returnedValue = updateList(id, req.body.todo.description);
+  if (returnedValue === undefined) {
+    res.send(readF());
+  } else {
+    res.send(returnedValue);
+  }
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const returnedValue = removeFromList(id);
+  if (returnedValue === undefined) {
+    res.send(readF());
+  } else {
+    res.send(returnedValue);
+  }
+});
+
+app.delete('/todos', (req, res) => {
+  stringifyAndWrite([]);
+  res.send(readF());
+});
+
+app.delete('/todos/:id/:action', (req, res) => {
+  const id = req.params.id;
+  if (req.params.action === 'done') {
+    const returnedValue = markUnDone(id);
+    if (returnedValue === undefined) {
+      res.send(readF());
+    } else {
+      res.send(returnedValue);
+    }
+  }
+});
+
+app.listen(3000);
