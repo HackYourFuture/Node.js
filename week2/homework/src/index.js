8febbcc0730b1f3f7953126894f137e9ebcf3229
Wael-Alhomsi <waelhom2@gmail.com>
2019-01-26 17:36:33 +0100

    put data.txt in a const variable

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -4,22 +4,22 @@
 const fs = require('fs');
 const commandsArray = [process.argv[2], process.argv[3], process.argv[4]];
 const itemIndex = parseInt(commandsArray[1]) - 1;
-const dataFile = './data.txt';
+const dataFilePath = './data.txt';
 
 function readF(file) {
   return fs.readFileSync(file, 'utf8');
 }
 
 function writeF(string) {
-  fs.writeFileSync(dataFile, string);
+  fs.writeFileSync(dataFilePath, string);
 }
 
 function appendF(string) {
-  fs.appendFileSync(dataFile, string);
+  fs.appendFileSync(dataFilePath, string);
 }
 
 function showList() {
-  const list = readF(dataFile);
+  const list = readF(dataFilePath);
   if (!list) {
     console.log('There are no "to-do items" to be displayed.');
   } else {
@@ -32,7 +32,7 @@ function showHelp() {
 }
 
 function addToList(item) {
-  const previousString = readF(dataFile);
+  const previousString = readF(dataFilePath);
   if (previousString) {
     appendF('\n' + item);
   } else {
@@ -41,7 +41,7 @@ function addToList(item) {
 }
 
 function removeFromList(index) {
-  const str = readF(dataFile);
+  const str = readF(dataFilePath);
   if (str === '') {
     console.log('The list has no corresponding "to-do element" to be removed.');
   } else {
@@ -56,7 +56,7 @@ function removeFromList(index) {
 }
 
 function updateList(index, addedItem) {
-  const str = readF(dataFile);
+  const str = readF(dataFilePath);
   if (str === '') {
     console.log('The list has no corresponding "to-do element" to be updated.');
   } else {
