355df9006bb3c9bdcc821197a68137d1096b2103
reza <reza_ranjbar94@yahoo.com>
2019-01-31 11:56:32 +0100

    Reza week3- problems with DONE part

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,81 @@
 'use strict';
 
-// TODO: Write the homework code in this file
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+const port = 3000;
+
+app.use(bodyParser.json());
+
+const tasks = JSON.parse(data);
+let id = uuid();
+
+app.get('/todos', (request, response) => {
+  fs.readFile('./todos.json', 'utf8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      response.json(tasks);
+    }
+  });
+});
+
+app.get('/todos/:id', (request, response) => {
+  fs.readFile('./todos.json', 'utf8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      response.json(tasks[id]);
+    }
+  });
+});
+
+app.delete('/todos', (request, response) => {
+  fs.readFile('./todo.json', 'utf8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      fs.writeFileSync('todo.json', '[]');
+    }
+  });
+});
+
+app.post('/todos/:item', (request, response) => {
+  fs.readFile('./todo.json', 'utf8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const newTask = { text: request.params.item };
+      tasks.push(newTask);
+      const newTasks = JSON.stringify(tasks);
+      fs.writeFileSync('todo.json', newTasks);
+      response.status(201);
+    }
+  });
+});
+
+app.delete('/todos/:id', (request, response) => {
+  fs.readFile('./todo.json', 'utf8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      tasks.splice(id, 1);
+      const newTasks = JSON.stringify(tasks);
+      fs.writeFileSync('todo.json', newTasks);
+    }
+  });
+});
+
+app.post('/todos/:id/done', (request, response) => {
+  fs.readFile('./todo.json', 'utf8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      // not sure how to finish this part!
+    }
+  });
+});
+
+app.listen(port);
