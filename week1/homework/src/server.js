b51a4c571b1a89bffdd0be2aaed585f788a3d707
u61 <utkukopuz.nl@outlook.com>
2019-02-18 22:56:09 +0100

    Utku / Class 19 / Homework Week 1 Done

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,11 +5,40 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
+
 function createServer(port) {
   let state = 10;
 
+  // Created a function in order to not repeating myself.
+  function showResponse(response) {
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify({ state }));
+    response.end();
+  }
+
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        showResponse(response);
+        break;
+      case '/add':
+        state++;
+        showResponse(response);
+        break;
+      case '/subtract':
+        state--;
+        showResponse(response);
+        break;
+      case '/reset':
+        state = 10;
+        showResponse(response);
+        break;
+      default:
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({ error: 'Not found' }));
+        response.end();
+    }
   });
 
   return server;
