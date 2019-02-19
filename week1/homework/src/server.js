bd472db885b0998b63b85d323e1594252cdb75da
ufukcalimli <ufk.calimli@gmail.com>
2019-02-19 14:22:00 +0100

    Homework of week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -8,8 +8,36 @@ const http = require('http');
 function createServer(port) {
   let state = 10;
 
+  function respond(response) {
+    response.writeHead(200, {'Content-Type':'application/json'});
+    response.write(JSON.stringify({state}));
+    response.end();
+  }
+
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        respond(response);
+        break;
+      case '/add':
+        state++;
+        respond(response);
+        break;
+      case '/subtract':
+        state--;
+        respond(response);
+        break;
+      case '/reset':
+        state = 10;
+        respond(response);
+        break;
+      default:
+        response.writeHead(404, {'Content-Type':'application/json'});
+        response.write(response.statusCode);
+        response.end();
+        break;
+    }
   });
 
   return server;
