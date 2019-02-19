77e5b723980f9d734a455f252f75d1f48763fc32
AhmadAbdulhaleem <ahmadhaleem1992@gmail.com>
2019-02-19 18:17:32 +0100

    week1 homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,7 +9,32 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    response.setHeader('Content-Type', 'application/json');
+    if (request.url === '/state') {
+      response.write(JSON.stringify({ state }));
+      response.end();
+    }
+ else if (request.url === '/add') {
+      state += 1;
+      response.write(JSON.stringify({ state }));
+      response.end();
+    }
+ else if (request.url === '/subtract') {
+      state -= 1;
+      response.write(JSON.stringify({ state }));
+      response.end();
+    }
+ else if (request.url === '/reset') {
+      state = 10;
+      response.write(JSON.stringify({ state }));
+      response.end();
+    }
+ else {
+      response.statusCode = 404;
+      const error = response.statusCode;
+      response.write(JSON.stringify({ error: 'Not found' }));
+      response.end();
+    }
   });
 
   return server;
