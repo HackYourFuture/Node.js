e0c2e6a0f58007e4fbad672f4ca6d8d8a43f4e73
alquinex <alquinex2010@hotmail.com>
2019-02-20 21:35:36 +0100

    week1 homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,12 +9,45 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({ state }));
+        response.end();
+        break;
+
+      case '/add':
+        state += 1;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({ state }));
+        response.end();
+        break;
+
+      case '/subtract':
+        state -= 1;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({ state }));
+        response.end();
+        break;
+
+      case '/reset':
+        state = 10;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({ state }));
+        response.end();
+        break;
+
+      default:
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({ error: 'Not found' }));
+        response.end();
+    }
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
