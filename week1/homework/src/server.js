90027984bac808a83e7c45386edd31122349a9c6
mohamdmahdi <mohamd.mahdi@gmail.com>
2019-02-21 14:43:36 +0100

    homework week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,12 +9,37 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    if (request.url === '/state') {
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify({ state }));
+      response.end();
+    } else if (request.url === '/add') {
+      state += 1;
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify({ state }));
+      response.end();
+    } else if (request.url === '/subtract') {
+      state -= 1;
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify({ state }));
+      response.end();
+    } else if (request.url === '/reset') {
+      state = 10;
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify({ state }));
+      response.end();
+    } else {
+      response.setHeader('Content-Type', 'application/json');
+      const error = 'Not found';
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ error }));
+      response.end();
+    }
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
