31830f5ff437ef0283852dca0aa3da7bd71c5395
Ayham.k <ayham.koutiney@hotmail.com>
2019-02-24 00:53:50 +0100

    week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,11 +10,35 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    const createResponse = function(status, state) {
+      response.writeHead(status, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(state));
+    };
+    switch (request.url) {
+      case '/':
+        createResponse(200, { state });
+        break;
+      case '/state':
+        createResponse(200, { state });
+        break;
+      case '/add':
+        state++;
+        createResponse(200, { state });
+        break;
+      case '/subtract':
+        state--;
+        createResponse(200, { state });
+        break;
+      case '/reset':
+        state = 10;
+        FinalResponse(200, { state });
+        break;
+    }
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
