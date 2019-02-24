d708468cf99f60716e44e324af3dae0257186f72
ahmetbibi <ahmetbibi@gmail.com>
2019-02-25 00:43:32 +0100

    ahmetbibi-week2-homework

--- week2/homework/src/functions.js
+++ week2/homework/src/functions.js
@@ -1,120 +1,116 @@
 'use strict';
+
 const fs = require('fs');
 
-const list = () => {
-  fs.readFile('./to-do.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    }
+const functions = {
+  list: () => {
+    fs.readFile('./to-do.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
  else if (data === '') {
-      console.log('There is nothing in to-do list!');
-    }
- else {
-      let newData = data.split('\n');
-      newData.pop();
-      newData = newData.join('\n');
-      console.log(newData);
-    }
-  });
-};
-
-const add = item => {
-  fs.appendFile('./to-do.txt', item + '\n', err => {
-    if (err) {
-      console.log(err);
-    }
- else {
-      console.log('New item added!');
-    }
-  });
-};
-
-const remove = index => {
-  fs.readFile('./to-do.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    }
- else {
-      if (index > data.split('\n').length) {
-        console.log(`Please write a number from 1 to ${data.split('\n').length}`);
+        console.log('There is nothing in to-do list!');
       }
  else {
         let newData = data.split('\n');
-        newData.splice(index - 1, 1);
+        newData.pop();
         newData = newData.join('\n');
-
-        fs.writeFile('./to-do.txt', newData, err => {
-          if (err) {
-            console.log(err);
-          }
- else {
-            console.log('Selected item removed!');
-          }
-        });
+        console.log(newData);
       }
-    }
-  });
-};
+    });
+  },
 
-const reset = () => {
-  const resetData = '';
-  fs.writeFile('./to-do.txt', resetData, err => {
-    if (err) {
-      console.log(err);
-    }
- else {
-      console.log('The to-do list has been reset!');
-    }
-  });
-};
-
-const update = (index, newItem) => {
-  fs.readFile('./to-do.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    }
- else {
-      if (data === '') {
-        console.log(`There is no item to update. Please add items.`);
+  add: item => {
+    fs.appendFile('./to-do.txt', item + '\n', err => {
+      if (err) {
+        console.log(err);
       }
+ else {
+        console.log('New item added!');
+      }
+    });
+  },
+
+  remove: index => {
+    fs.readFile('./to-do.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+ else {
+        if (index > data.split('\n').length) {
+          console.log(`Please write a number from 1 to ${data.split('\n').length}`);
+        }
+ else {
+          let newData = data.split('\n');
+          newData.splice(index - 1, 1);
+          newData = newData.join('\n');
+
+          fs.writeFile('./to-do.txt', newData, err => {
+            if (err) {
+              console.log(err);
+            }
+ else {
+              console.log('Selected item removed!');
+            }
+          });
+        }
+      }
+    });
+  },
+
+  reset: () => {
+    const resetData = '';
+    fs.writeFile('./to-do.txt', resetData, err => {
+      if (err) {
+        console.log(err);
+      }
+ else {
+        console.log('The to-do list has been reset!');
+      }
+    });
+  },
+
+  update: (index, newItem) => {
+    fs.readFile('./to-do.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+ else {
+        if (data === '') {
+          console.log(`There is no item to update. Please add items.`);
+        }
  else if (index > data.split('\n').length) {
-        console.log(`Please write a number from 1 to ${data.split('\n').length}`);
+          console.log(`Please write a number from 1 to ${data.split('\n').length}`);
+        }
+ else {
+          let newData = data.split('\n');
+          newData.splice(index - 1, 1, newItem);
+          newData = newData.join('\n');
+
+          fs.writeFile('./to-do.txt', newData, err => {
+            if (err) {
+              console.log(err);
+            }
+ else {
+              console.log('Selected item updated!');
+            }
+          });
+        }
+      }
+    });
+  },
+
+  help: () => {
+    fs.readFile('./help.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
       }
  else {
-        let newData = data.split('\n');
-        newData.splice(index - 1, 1, newItem);
-        newData = newData.join('\n');
-
-        fs.writeFile('./to-do.txt', newData, err => {
-          if (err) {
-            console.log(err);
-          }
- else {
-            console.log('Selected item updated!');
-          }
-        });
+        console.log(data);
       }
-    }
-  });
+    });
+    console.log(`You can use these commands to make a to-do list: \n`);
+  }
 };
 
-const help = () => {
-  fs.readFile('./help.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    }
- else {
-      console.log(data);
-    }
-  });
-  console.log(`You can use these commands to make a to-do list: \n`);
-};
-
-module.exports = {
-  list,
-  add,
-  remove,
-  reset,
-  help,
-  update
-};
+module.exports = functions;
--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -12,47 +12,52 @@ const reset = functions.reset;
 const help = functions.help;
 const update = functions.update;
 
-program
-  .command('help')
-  .alias(' ')
-  .description('to get help')
-  .action(() => {
-    help();
-  });
+if (!process.argv[2]) {
+  help();
+  // console.log(process.argv[2]);
+}
+ else {
+  program
+    .command('help')
+    .description('to get help')
+    .action(() => {
+      help();
+    });
 
-program
-  .command('list')
-  .description('to list to-dos')
-  .action(() => {
-    list();
-  });
+  program
+    .command('list')
+    .description('to list to-dos')
+    .action(() => {
+      list();
+    });
 
-program
-  .command('add <newToDo>')
-  .description('to add a new to-do')
-  .action(newToDo => {
-    add(newToDo);
-  });
+  program
+    .command('add <newToDo>')
+    .description('to add a new to-do')
+    .action(newToDo => {
+      add(newToDo);
+    });
 
-program
-  .command('remove <toDo>')
-  .description('to remove an existing to-do')
-  .action(toDo => {
-    remove(toDo);
-  });
+  program
+    .command('remove <toDo>')
+    .description('to remove an existing to-do')
+    .action(toDo => {
+      remove(toDo);
+    });
 
-program
-  .command('reset')
-  .description('to reset all to-dos')
-  .action(() => {
-    reset();
-  });
+  program
+    .command('reset')
+    .description('to reset all to-dos')
+    .action(() => {
+      reset();
+    });
 
-program
-  .command('update <existingToDo> <updatedToDo>')
-  .description('to list to-dos')
-  .action((existingToDo, updatedToDo) => {
-    update(existingToDo, updatedToDo);
-  });
+  program
+    .command('update <existingToDo> <updatedToDo>')
+    .description('to list to-dos')
+    .action((existingToDo, updatedToDo) => {
+      update(existingToDo, updatedToDo);
+    });
 
-program.parse(process.argv);
+  program.parse(process.argv);
+}
