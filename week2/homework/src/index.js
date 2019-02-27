f52e4501069ecd766bda7cc706aa6438f51ed7b2
ammarvehbi <merakli.vehbi@gmail.com>
2019-02-27 12:40:35 +0100

    week2 homework without commander (with switch)

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// TODO: Write the homework code in this file
+const printHelp = require('./printHelp');
+const listToDos = require('./listToDos');
+const addToDos = require('./addToDos');
+const removeToDo = require('./removeToDo');
+const removeAllToDos = require('./removeAllToDos');
+const updateToDo = require('./updateToDo');
+
+const command = process.argv[2];
+const args = process.argv.slice(3);
+const callCommand = (action, callback) => {
+  action
+    .then(callback)
+    .then(() => listToDos.listToDos())
+    .then(data => console.log(`\nTo-Dos:\n${data}`))
+    .catch(console.error);
+};
+
+switch (command) {
+  case 'list':
+    callCommand(listToDos.listToDos(), data => JSON.stringify(data, null, 2));
+    break;
+  case 'add':
+    callCommand(addToDos.addToDos(...args), () => console.log('To-do is added to the file.'));
+    break;
+  case 'remove':
+    callCommand(removeToDo.removeToDo(...args), () =>
+      console.log('To-do is removed from the file.')
+    );
+    break;
+  case 'reset':
+    callCommand(removeAllToDos.removeAllToDos(), () =>
+      console.log('All the to-dos are removed from the file.')
+    );
+    break;
+  case 'update':
+    callCommand(updateToDo.updateToDo(...args), () => console.log('To-do is updated.'));
+    break;
+  case 'help':
+  default:
+    printHelp.printHelp();
+    break;
+}
---
+++ week2/homework/src/updateToDo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const updateToDo = (index, ...text) => {
+  return readFile('to-dos.json', 'utf8').then(data => {
+    const inputData = JSON.parse(data);
+    inputData.splice(index - 1, 1, `\n${text.join(' ')}`);
+    return writeFile('to-dos.json', JSON.stringify(inputData), 'utf8');
+  });
+};
+
+module.exports = { updateToDo };
