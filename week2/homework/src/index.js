01d5a271287adf928ad4712c7c9dfc17848235b3
YaseirAlkhwalda <43831623+YaseirAlkhwalda@users.noreply.github.com>
2019-03-07 08:29:07 +0100

    Update index.js

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,19 +1,4 @@
+
 'use strict';
 
-const actions = require('./actions');
-
-const args = process.argv;
-
-args.splice(0, 2);
-
-const retrieve = argument => args.includes(argument);
-
-if (retrieve('add')) actions.add(process.argv[1]);
-
-if (retrieve('remove')) actions.remove(process.argv[1]);
-
-if (retrieve('list')) actions.list(process.argv);
-
-if (retrieve('reset')) actions.reset(process.argv);
-
-if (retrieve('help')) actions.help(process.argv);
+// TODO: Write the homework code in this file
