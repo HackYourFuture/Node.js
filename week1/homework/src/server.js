dfc65c1ae16ee0e36672788b556a12c8a70c2db6
unknown <muhannadjudah@gmail.com>
2019-05-12 21:09:44 +0200

    modify index.js week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,11 +10,32 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    if (request.url === '/state') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ key: state }));
+    } else if (request.url === '/add') {
+      state = state + 1;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ key: state }));
+    } else if (request.url === '/subtract') {
+      state = state - 1;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ key: state }));
+    } else if (request.url === '/reset') {
+      state = 10;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ key: state }));
+    } else {
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ error: 'Error' }));
+    }
+
+    response.end();
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
