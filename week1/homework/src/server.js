138ed3c9bbfad74dc2b302879767b2ae298f4dde
Akif <akbu2709@gmail.com>
2019-05-14 16:42:48 +0200

    Akif_NodeW1_Homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,12 +9,40 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    switch (request.url) {
+      case '/state':
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/add':
+        state++;
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/subtract':
+        state--;
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/reset':
+        state = 10;
+        response.write(JSON.stringify({ state }));
+        break;
+      default:
+        if (request.url === '' || request.url === '/') {
+          response.statusCode = 302;
+          response.write(JSON.stringify({ state }));
+        } else {
+          const error = 'Not found';
+          response.writeHead(404, { 'Content-Type': 'application/json' });
+          response.write(JSON.stringify({ error }));
+        }
+    }
+    response.end();
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  // eslint-disable-next-line comma-dangle
+  createServer,
 };
