6813900d52c51216b6a80763204d383a83ef3a79
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2019-05-15 19:10:17 +0200

    update function comment

--- week1/homework/src/responseHandler.js
+++ week1/homework/src/responseHandler.js
@@ -1,6 +1,6 @@
 /**
  * handle the response of the server
- * @param {*} response the response
+ * @param {response} response the response
  * @param {number} status the value of the status code
  * @param {variable or string} value the value of the state or the error
  */
