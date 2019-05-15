3f7b539a5efe458449923238c7ef97f3b48df062
unknown <muhannadjudah@gmail.com>
2019-05-15 11:09:01 +0200

    change state object

--- week1/homework/src/responses/sendValues.js
+++ week1/homework/src/responses/sendValues.js
@@ -1,11 +1,11 @@
 'use strict';
 
-function handleResponse(response, status, value) {
+function handleResponse(response, status, state) {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   if (status === 200) {
-    response.write(JSON.stringify({ state: value }));
+    response.write(JSON.stringify({ state }));
   } else {
-    response.write(JSON.stringify({ error: value }));
+    response.write(JSON.stringify({ error: state }));
   }
 }
 
