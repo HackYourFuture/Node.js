2e756443ac90844a0d8fca1e1dddcaec91419ba9
eneserdek <enesf.erdek@gmail.com>
2019-05-15 17:10:00 +0200

    if/else blokes are changed with ?: and switch block to have a prettier design

--- week1/homework/src/urlExtension.js
+++ week1/homework/src/urlExtension.js
@@ -3,26 +3,29 @@ module.exports = {
   myStateFunction: function(request, response, state) {
     function writeHeadAndStringify(code, value) {
       response.writeHead(code, { 'Content-Type': 'application/json' });
-      if (typeof value === 'number') {
-        response.write(JSON.stringify({ state: value }));
-      } else {
-        response.write(JSON.stringify({ error: value }));
-      }
+      typeof value === 'number'
+        ? response.write(JSON.stringify({ state: value }))
+        : response.write(JSON.stringify({ error: value }));
     }
-    if (request.url === `/state`) {
-      writeHeadAndStringify(200, state);
-    } else if (request.url === '/add') {
-      state = ++state;
-      writeHeadAndStringify(200, state);
-    } else if (request.url === '/subtract') {
-      state = --state;
-      writeHeadAndStringify(200, state);
-    } else if (request.url === '/reset') {
-      state = 10;
-      writeHeadAndStringify(200, state);
-    } else {
-      const error = 'Not found';
-      writeHeadAndStringify(404, error);
+    switch (request.url) {
+      case '/state':
+        writeHeadAndStringify(200, state);
+        break;
+      case '/add':
+        state = ++state;
+        writeHeadAndStringify(200, state);
+        break;
+      case '/subtract':
+        state = --state;
+        writeHeadAndStringify(200, state);
+        break;
+      case '/reset':
+        state = 10;
+        writeHeadAndStringify(200, state);
+        break;
+      default:
+        const error = 'Not found';
+        writeHeadAndStringify(404, error);
     }
     return state;
   },
