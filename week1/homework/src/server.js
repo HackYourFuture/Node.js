70ea8e913a4517b2d0a4b6bf41e499ca9c8f8ef6
Ghufran <ghufran@MacBook-Pro-van-Ghufran.local>
2019-05-16 22:39:33 +0200

    first node code

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,11 +10,39 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+
+    switch (request.url) {
+      case '/state':
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/add':
+        state++;
+        response.write(JSON.stringify({ state }));
+        break;
+
+      case '/subtract':
+        state--;
+        response.write(JSON.stringify({ state }));
+        break;
+
+      case '/reset':
+        state = 10;
+        response.write(JSON.stringify({ state }));
+        break;
+
+      default:
+        const error = 'Not found';
+        response.writeHead(404, { 'content-Type': 'application/json' });
+        response.write(JSON.stringify({ error }));
+        break;
+    }
+    response.end();
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
