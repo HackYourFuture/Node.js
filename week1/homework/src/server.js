c160dd6b10c0f5df230cefef00dbd2306b115943
saidcetinkaya <saidcetinkaya@github.com>
2019-05-19 12:36:33 +0200

    week1 homework said

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,7 +9,34 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    if (request.url === '/state') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      const stateJSON = JSON.stringify({ state });
+      response.write(stateJSON);
+    }
+ else if (request.url === '/add') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      const stateJSON = JSON.stringify({ state: ++state });
+      response.write(stateJSON);
+    }
+ else if (request.url === '/subtract') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      const stateJSON = JSON.stringify({ state: --state });
+      response.write(stateJSON);
+    }
+ else if (request.url === '/reset') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      state = 10;
+      const stateJSON = JSON.stringify({ state });
+      response.write(stateJSON);
+    }
+ else {
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      const error = 'Not found';
+      const err = JSON.stringify({ error });
+      response.write(err);
+    }
+    response.end();
   });
 
   return server;
