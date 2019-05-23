4dce7ff62dad4ede77cdb22b75fb4db3c23744fa
unknown <muhannadjudah@gmail.com>
2019-05-23 18:49:49 +0200

    add update function and check validation of number

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -6,10 +6,12 @@ const fileName = `${__dirname}/todoList.txt`;
 const command = process.argv[2];
 const args = process.argv.slice(3);
 
+const newItem = process.argv.slice(4);
+
 const number = parseInt(args);
 
 if (command === 'add') {
-  add(...args).then(() => console.log(`a new item has been saved...`));
+  add(...args).then(() => console.log(`a new item has been added`));
 } else if (command === 'help' || command === undefined) {
   help();
 } else if (command === 'list') {
@@ -17,15 +19,37 @@ if (command === 'add') {
 } else if (command === 'remove') {
   remove(number);
 } else if (command === 'reset') {
-  reset().then(() => console.log('All to-do has been deleted'));
+  reset().then(() => console.log('All to-do items has been deleted'));
+} else if (command === 'update') {
+  update(number, newItem);
+}
+
+async function update(index, text) {
+  try {
+    const data = await list();
+    let arr = data.split('\n');
+    write(arr);
+    if (index <= arr.length && index > 0) {
+      arr.splice(index - 1, 1, text);
+      write(arr).then(console.log('an item has been removed'));
+    } else {
+      console.log('invalid number');
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function remove(index) {
   try {
     const data = await list();
     let arr = data.split('\n');
-    arr.splice(index - 1, 1);
-    write(arr);
+    if (index <= arr.length && index > 0) {
+      arr.splice(index - 1, 1);
+      write(arr).then(console.log('an item has been modified'));
+    } else {
+      console.log('invalid number');
+    }
   } catch (err) {
     console.error(err);
   }
