1f2f6209d44a0505666fd45d02df28fd8f8e404f
sigma21 <semihkocakel@hotmail.com>
2019-05-31 12:13:05 +0200

    semih-week3 pr

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,26 @@
-'use strict';
+"use strict";
 
-// TODO: Write the homework code in this file
+const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const myPackage = require("./myPackage");
+app.use(bodyParser.json());
+
+app.get("/todos/:id", (request, response) => {
+  myPackage.readToDo(request, response);
+});
+
+app.delete("/todos/", (request, response) => {
+  myPackage.clearToDos(response);
+});
+
+app.post("/todos/:id/done", (request, response) => {
+  myPackage.markAsDoneOrNotDone(request, response, true);
+});
+
+app.delete("/todos/:id/done", (request, response) => {
+  myPackage.markAsDoneOrNotDone(request, response, false);
+});
+
+app.listen(3000);
---
+++ week3/homework/src/myPackage.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const bodyParser = require("body-parser");
+
+function readToDo(request, response) {
+  fs.readFile("./todos.json", "utf8", (error, todoList) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send("some problem.");
+    } else {
+      const index = request.params.id;
+      response.json(JSON.parse(todoList)[index - 1]);
+      console.log(todoList);
+    }
+  });
+}
+
+function clearToDos(response) {
+  fs.truncate("./todos.json", 0, (error, todoList) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send("Problem Occured.");
+    } else {
+      console.log("Todo list cleared.");
+      response.json();
+    }
+  });
+}
+
+function markAsDoneOrNotDone(request, response, boolean) {
+  fs.readFile("./todos.json", "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send("some problem.");
+    } else {
+      const index = request.params.id;
+      let newData = JSON.parse(data);
+      newData[index - 1].done = boolean;
+
+      fs.writeFile("./todos.json", JSON.stringify(newData), error => {
+        if (error) {
+          console.log(error);
+          response.status(500).send("problem occured while writing data.");
+        } else {
+          console.log("Status of todo item changed.");
+          response.json(newData);
+        }
+      });
+    }
+  });
+}
+
+module.exports = {readToDo, clearToDos, markAsDoneOrNotDone}
