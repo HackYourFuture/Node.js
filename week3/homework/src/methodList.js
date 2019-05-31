4c47ffe846fb0c0a9bcfebf883f7fce5928e3268
eneserdek <enesf.erdek@gmail.com>
2019-05-31 18:55:01 +0200

    minor changes are being made

--- week3/homework/src/methodList.js
+++ week3/homework/src/methodList.js
@@ -28,21 +28,25 @@ function sendResponse(data, response) {
 function listToDo(path, response) {
   readFile(path)
     .then(data => sendResponse(data, response))
-    .catch(error => console.log(error));
+    .catch(error => response.status(404).send({ error: 'there is an error' }););
 }
 
 async function addToDo(todoItem, path, response) {
-  await readFile(path).then(data => {
-    const todoList = JSON.parse(data);
-    const newTodo = {
-      id: uuid(),
-      name: todoItem.item,
-      done: false,
-    };
-    todoList.push(newTodo);
-    writeFile(path, todoList);
-  });
-  listToDo(path, response);
+  try {
+    await readFile(path).then(data => {
+      const todoList = JSON.parse(data);
+      const newTodo = {
+        id: uuid(),
+        name: todoItem.item,
+        done: false,
+      };
+      todoList.push(newTodo);
+      writeFile(path, todoList);
+    });
+    listToDo(path, response);
+  } catch {
+    response.status(404).send({ error: 'there is an error' });
+  }
 }
 
 function readToDo(number, path, response) {
@@ -57,65 +61,81 @@ function readToDo(number, path, response) {
         : sendResponse('todo not found', response);
     })
     .catch(error => {
-      console.log(error);
+      response.status(404).send({ error: 'there is an error' });;
     });
 }
 
 async function markAsDone(number, path, response) {
-  await readFile(path)
-    .then(data => {
-      const todoList = JSON.parse(data);
-      const todo = todoList.find(todo => {
-        return todo.id == number;
+  try {
+    await readFile(path)
+      .then(data => {
+        const todoList = JSON.parse(data);
+        const todo = todoList.find(todo => {
+          return todo.id == number;
+        });
+        todo.done = true;
+        writeFile(path, todoList);
+      })
+      .catch(error => {
+        console.log(error);
       });
-      todo.done = true;
-      writeFile(path, todoList);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  listToDo(path, response);
+    listToDo(path, response);
+  } catch {
+    response.status(404).send({ error: 'there is an error' });
+  }
 }
-
 async function markAsNotDone(number, path, response) {
-  await readFile(path)
-    .then(data => {
-      const todoList = JSON.parse(data);
-      const todo = todoList.find(todo => {
-        return todo.id == number;
+  try {
+    await readFile(path)
+      .then(data => {
+        const todoList = JSON.parse(data);
+        const todo = todoList.find(todo => {
+          return todo.id == number;
+        });
+        todo.done = false;
+        writeFile(path, todoList);
+      })
+      .catch(error => {
+        console.log(error);
       });
-      todo.done = false;
-      writeFile(path, todoList);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  listToDo(path, response);
+    listToDo(path, response);
+  } catch {
+    response.status(404).send({ error: 'there is an error' });
+  }
 }
 
 function clearToDos(path, response) {
-  writeFile(path, []).then(response.json('You now have a brand new todo list'));
+  writeFile(path, []).then(response.json('You now have a brand new todo list'))
+  .catch(error=>response.status(404).send({ error: 'there is an error' });)
 }
 
 async function removeToDo(todoItem, path, response) {
-  await readFile(path).then(data => {
-    const todoList = JSON.parse(data);
-    const filteredOutElement = todoItem - 1;
-    const filteredList = todoList.filter((elem, index, arr) => {
-      return index !== filteredOutElement;
+  try {
+    await readFile(path).then(data => {
+      const todoList = JSON.parse(data);
+      const filteredOutElement = todoItem - 1;
+      const filteredList = todoList.filter((elem, index, arr) => {
+        return index !== filteredOutElement;
+      });
+      writeFile(path, filteredList);
     });
-    writeFile(path, filteredList);
-  });
-  listToDo(path, response);
+    listToDo(path, response);
+  } catch {
+    response.status(404).send({ error: 'there is an error' });
+  }
 }
 
 async function updateToDo(todoItem, todo, path, response) {
-  await readFile(path).then(data => {
-    const todoList = JSON.parse(data);
-    todoList[--todoItem].name = todo.item;
-    writeFile(path, todoList);
-  });
-  listToDo(path, response);
+  try {
+    await readFile(path).then(data => {
+      const todoList = JSON.parse(data);
+      todoList[--todoItem].name = todo.item;
+      writeFile(path, todoList);
+    });
+    listToDo(path, response);
+  } catch {
+    response.status(404).send({ error: 'there is an error' });
+  }
 }
 
 module.exports = {
