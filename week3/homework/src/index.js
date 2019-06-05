f7c1dfd431401220a6148b642d3a8ec0060d602e
kani103 <you@example.com>
2019-06-05 16:28:32 +0200

    i made my homework

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,65 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+const express = require ('express');
+const app = express();
+let todos = require('./todos')
+
+
+
+app.get('/', (req, res) => res.json(todos));
+
+
+app.get('/todos/:id', (req, res) => {
+    let found = todos.find(todo => {
+        return todo.id === parseInt(req.params.id);
+    });
+
+    if (found) {
+        res.status(200).json(found);
+    } else {
+        res.send({ msg: '404 Not found'});
+    }
+});
+
+app.delete('/todos', (req, res) => {
+    let found = todos.find(todo => {
+      return todo.id === parseInt(req.params.id);
+    });
+  
+    if (found) {
+      let targetIndex = todo.indexOf(found);
+      todo.splice(targetIndex, 1);
+    }
+      res.json({ result: "item deleted!"})
+      res.send(200);
+
+  });
+
+  app.post('/todos/:id/done', (req, res) => {
+     let todoItem = todos.find(item => item.id === Number(req.params.id))
+     todoItem.done = true;
+     todos = todos.map(item => {
+         if (todoItem.id === item.id) {
+            return todoItem
+         }
+         return item
+     })
+     res.send(todoItem)
+  });
+
+app.delete('/todos/:id/done', (req, res) => {
+    let todoItem = todos.find(item => item.id === Number(req.params.id))
+     todoItem.done = false;
+     todos = todos.filter(item => {
+         if (todoItem.id === item.id) {
+            return todoItem
+         }
+         return item
+     })
+     res.status(200)
+     res.send(todoItem)
+});
+
+
+app.listen(8080, () => console.log(`sever listening on port...`));
---
+++ week3/homework/src/todos.js
@@ -0,0 +1,19 @@
+let todos = [
+  {
+      id: 1,
+      done: false,
+      todo: 'do your homework'
+  },
+  {
+      id: 2,
+      done: false,
+      todo: 'go for a walk'
+  },
+  {
+      id: 3,
+      done: false,
+      todo: 'watch a movie'
+  }
+]
+
+module.exports = todos;
