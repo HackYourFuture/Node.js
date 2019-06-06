f60c115b3c0cec1bad9c0e1bd09261dc526ebaa9
saidcetinkaya <saidcetinkaya@github.com>
2019-06-07 01:03:45 +0200

    week3

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,74 @@
 'use strict';
 
-// TODO: Write the homework code in this file
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+function readToDo(request, response) {
+  fs.readFile('./todos.json', 'utf8', (error, data) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send('Problem Occurred.');
+    } else {
+      const index = request.params.id - 1;
+      response.json(JSON.parse(data)[index]);
+      console.log(data);
+    }
+  });
+}
+
+function clearToDos(response) {
+  fs.truncate('./todos.json', 0, (error, data) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send('Problem Occurred.');
+    } else {
+      console.log('Todo list cleared.');
+      response.json();
+    }
+  });
+}
+
+function markAsDoneOrNotDone(request, response, boolean) {
+  fs.readFile('./todos.json', 'utf8', (error, data) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send('Problem Occurred.');
+    } else {
+      const index = request.params.id - 1;
+      const finalData = JSON.parse(data);
+      finalData[index].done = boolean;
+
+      fs.writeFile('./todos.json', JSON.stringify(finalData), error => {
+        if (error) {
+          console.log(error);
+          response.status(500).send('Problem occurred while writing data.');
+        } else {
+          console.log('Status of todo item changed.');
+          response.json(finalData);
+        }
+      });
+    }
+  });
+}
+
+app.use(bodyParser.json());
+
+app.get('/todos/:id', (request, response) => {
+  readToDo(request, response);
+});
+
+app.delete('/todos/', (request, response) => {
+  clearToDos(response);
+});
+
+app.post('/todos/:id/done', (request, response) => {
+  markAsDoneOrNotDone(request, response, true);
+});
+
+app.delete('/todos/:id/done', (request, response) => {
+  markAsDoneOrNotDone(request, response, false);
+});
+
+app.listen(3000);
