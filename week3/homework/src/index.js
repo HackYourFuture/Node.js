5c83b4ab02d00c2a7b6f8e0a66ab7b5fb972419e
rincemath <mathince1.62@gmail.com>
2019-06-18 07:35:29 +0200

    RamazanInce_NodeHW3_added

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -13,8 +13,8 @@ app.get('/todo/:id', (request, response) => {
     if (error) {
       if (error.code === 'ENOENT') {
         console.log('no data found');
-        response.status(404).send('');
       } else console.error(error);
+      response.status(404).send('');
     } else {
       const newList = todo.split('\n');
       const id = request.params.id;
@@ -29,8 +29,8 @@ app.delete('/todo/:id', (request, response) => {
     if (error) {
       if (error.code === 'ENOENT') {
         console.log('no data found');
-        response.status(404).send('');
       } else console.error(error);
+      response.status(404).send('');
     } else {
       const todos = JSON.parse(todosBinary);
       todos.splice(id - 1, 1);
@@ -46,28 +46,26 @@ app.delete('/todo/:id', (request, response) => {
 
 app.post('/todo', (request, response) => {
   const todoItem = request.body.todo;
-  
+
   fs.readFile('./todos.json', 'utf8', (error, todosBinary) => {
     if (error) {
       if (error.code === 'ENOENT') {
         console.log('no data found');
-        response.status(404).send('');
-      } else console.error(error);
+      } else response.status(404).send('');
+      console.error(error);
     } else {
       const todos = todosBinary.trim() !== '' ? JSON.parse(todosBinary) : [];
       todos.push({
         text: request.body.todo,
         done: false,
       });
-      
+
       fs.writeFile('./todos.json', JSON.stringify(todos), error => {
         console.error(error);
         console.log('Todo item is added');
         response.status(201).json({ result: request.body.todo });
-        
       });
     }
-    
   });
 });
 app.delete('/todo', (request, response) => {
@@ -76,6 +74,32 @@ app.delete('/todo', (request, response) => {
     fs.writeFile('./todos.json', '', err => {
       if (err) throw err;
       console.log('File deleted!');
+    });
+  });
+});
+
+app.put('/todo/done/:id', (request, response) => {
+  fs.readFile('./todos.json', 'utf8', (error, data) => {
+    const todos = JSON.parse(data);
+    const id = request.params.id;
+    todos[id - 1]['done'] = true;
+    fs.writeFile('./todos.json', JSON.stringify(todos), error => {
+      console.error(error);
+      console.log('Item Marked as Done');
+      response.status(201).json({ result: request.body.todo });
+    });
+  });
+});
+
+app.put('/todo/notDone/:id', (request, response) => {
+  fs.readFile('./todos.json', 'utf8', (error, data) => {
+    const todos = JSON.parse(data);
+    const id = request.params.id;
+    todos[id - 1]['done'] = false;
+    fs.writeFile('./todos.json', JSON.stringify(todos), error => {
+      console.error(error);
+      console.log('Item Marked as Not Done');
+      response.status(201).json({ result: request.body.todo });
     });
   });
 });
