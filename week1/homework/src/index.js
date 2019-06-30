a8bc074683d822da4823f778028af7d1a97363c0
irfan <ikrdnz@gmail.com>
2019-06-30 19:56:58 +0200

    Node.js week1- irfankaradeniz

--- week1/homework/src/index.js
+++ week1/homework/src/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const {
-  createServer
-} = require('./server');
+const { createServer } = require('./server');
 
-const PORT = 3000;
+const PORT = 4000;
 
 createServer().listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,16 +5,42 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
-function createServer(port) {
+function createServer() {
   let state = 10;
-
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        response.setHeader('Content-Type', 'application/json');
+        const stringifiedState = JSON.stringify({ state: state });
+        response.write(stringifiedState);
+        break;
+      case '/add':
+        response.setHeader('Content-Type', 'application/json');
+        const stringifiedState2 = JSON.stringify({ state: ++state });
+        response.write(stringifiedState2);
+        break;
+      case '/subtract':
+        response.setHeader('Content-Type', 'application/json');
+        const stringifiedState3 = JSON.stringify({ state: --state });
+        response.write(stringifiedState3);
+        break;
+      case '/reset':
+        response.setHeader('Content-Type', 'application/json');
+        state = 10;
+        const stringifiedState4 = JSON.stringify({ state });
+        response.write(stringifiedState4);
+        break;
+      default:
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        const error = 'Not found';
+        const stringifiedError = JSON.stringify({ error });
+        response.write(stringifiedError);
+    }
+    response.end();
   });
 
   return server;
 }
-
 module.exports = {
   createServer
 };
