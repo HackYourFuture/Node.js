4fa3bb57e65ad8190e6de67bcb654ed9f467520b
Ghufran Deiri <ghufran@Ghufrans-MacBook-Pro.local>
2019-07-02 11:27:49 +0200

    Add and subtract from a state

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,12 +9,36 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    console.log(request.method, request.url);
+
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    switch (request.url) {
+      case '/state':
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/add':
+        state++;
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/subtract':
+        state--;
+        response.write(JSON.stringify({ state }));
+        break;
+      case '/reset':
+        state = 10;
+        response.write(JSON.stringify({ state }));
+        break;
+      default:
+        const error = 'Not found';
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({ error }));
+        break;
+    }
+    response.end();
   });
 
   return server;
 }
-
 module.exports = {
-  createServer
+  createServer,
 };
