7a8b77858475a717b0b00ea949d2b6a04b04dd98
veliataseven <veliataseven@gmail.com>
2019-07-02 16:41:35 +0200

    Veli | Node.js-Week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,6 +10,28 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({ state: state }));
+        break;
+      case '/add':
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({ state: (state += 1) }));
+        break;
+      case '/subtract':
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({ state: (state -= 1) }));
+        break;
+      case '/reset':
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({ state: (state = 10) }));
+        break;
+      default:
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({ error: 'Not found' }));
+    }
   });
 
   return server;
