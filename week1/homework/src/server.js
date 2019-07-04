e0c0dc92f0d3f33b8685932a8a9fd7607b917a06
aalhommada <aalhommada@gmail.com>
2019-07-04 10:04:52 +0200

    aalhommada-node.js-week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,6 +9,29 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
+    if (request.url === '/state') {
+      response.setHeader('Content-Type', 'application/json');
+      response.end(JSON.stringify({ state: state }));
+    }
+    if (request.url === '/add') {
+      response.setHeader('Content-Type', 'application/json');
+      response.end(JSON.stringify({ state: ++state }));
+    }
+    if (request.url === '/subtract') {
+      response.setHeader('Content-Type', 'application/json');
+      response.end(JSON.stringify({ state: --state }));
+    }
+    if (request.url === '/reset') {
+      response.setHeader('Content-Type', 'application/json');
+      response.end(JSON.stringify({ state: state }));
+    }
+    // eslint-disable-next-line indent
+    else {
+      response.setHeader('Content-Type', 'application/json');
+      response.statusCode = 404;
+      response.end(JSON.stringify({ error: 'Not found' }));
+    }
+    response.end();
     // TODO: Write your homework code here
   });
 
