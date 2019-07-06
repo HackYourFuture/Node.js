b20b7a3b2198d9e3cd699bea56d90145e56f7a93
aalhommada <aalhommada@gmail.com>
2019-07-06 15:04:28 +0200

    aalhommada-node.js-week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -7,9 +7,30 @@ const http = require('http');
  */
 function createServer(port) {
   let state = 10;
+  const results = { 'Contest-Type': 'application/json' };
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    if (request.url === '/state') {
+      response.writeHead(200, results);
+      response.write(JSON.stringify({ state: state }));
+    }
+ else if (request.url === '/add') {
+      response.writeHead(200, results);
+      response.write(JSON.stringify({ state: state + 1 }));
+    }
+ else if (request.url === '/subtract') {
+      response.writeHead(200, results);
+      response.write(JSON.stringify({ state: state - 1 }));
+    }
+ else if (request.url === '/reset') {
+      response.writeHead(200, results);
+      response.write(JSON.stringify({ state: state }));
+    }
+ else {
+      response.writeHead(404, results);
+      response.write(JSON.stringify({ error: 'not found' }));
+    }
+    response.end();
   });
 
   return server;
