e28bbdaf5324e3b1793b543662bf45203a876c17
aalhommada <aalhommada@gmail.com>
2019-07-19 22:56:13 +0200

    aalhommada-node.js-week3

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,10 +1,9 @@
-'use strict';
+// 'use strict';
+
 const express = require('express');
-
-const todos = require('./todos');
-const validateTodos=require('./validateTodos')
-
+const Joi = require('joi');
 const app = express();
+const todos = require('./todos');
 app.use(express.json());
 
 app.get('/todos', (req, res) => {
@@ -13,14 +12,16 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   const todo = todos.find(c => c.id === parseInt(req.params.id));
-  if (!todo) return res.status(404).send(` The ID : ${id} is not found.`);
+  if (!todo) return res.status(404).send('the given api not found');
   res.send(todo);
 });
 
 app.post('/todos', (req, res) => {
   const { error } = validateTodos(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
   const todo = {
     id: todos.length + 1,
     name: req.body.name,
@@ -32,29 +33,31 @@ app.post('/todos', (req, res) => {
 
 app.put('/todos/:id', (req, res) => {
   const todo = todos.find(c => c.id === parseInt(req.params.id));
-  if (!todo) return res.status(404).send(` The ID : ${id} is not found.`);
-  const { error } = validateTodos(req.body); 
+  if (!todo) return res.status(404).send('the given api not found');
+
+  const { error } = validateTodos(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
   todo.name = req.body.name;
   res.send(todos);
-  
 });
 
-// mark as done
- app.delete('/todos/:id/true', (req, res) => {
-  const todo = todos.find(c => c.id === parseInt(req.params.id));
-  if (!todo) return res.status(404).send(` The ID : ${id} is not found.`);
-  const { error } = validateTodos(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
-  todo.done = true;
-  res.send(todo);
-  
-// mark as not done
- app.delete('/todos/:id/false', (req, res) => {
+app.delete('/todos/:id/true', (req, res) => {
   const todo = todos.find(c => c.id === parseInt(req.params.id));
   if (!todo) return res.status(404).send('the given api not found');
 
-  const { error } = validateTodos(req.body); 
+  const { error } = validateTodos(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  todo.done = true;
+  res.send(todo);
+});
+
+app.delete('/todos/:id/false', (req, res) => {
+  const todo = todos.find(c => c.id === parseInt(req.params.id));
+  if (!todo) return res.status(404).send('the given api not found');
+
+  const { error } = validateTodos(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   todo.done = false;
@@ -63,12 +66,21 @@ app.put('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
   const todo = todos.find(c => c.id === parseInt(req.params.id));
-  if (!todo) return res.status(404).send(` The ID : ${id} is not found.`);
+  if (!todo) return res.status(404).send('the given api not found');
 
   const index = todos.indexOf(todo);
   todos.splice(index, 1);
   res.send(todo);
 });
+
+function validateTodos(todo) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required(),
+  };
+  return Joi.validate(todo, schema);
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
