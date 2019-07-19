2c3b97aea2b302bd1b1a6051bae812137bdde8ad
irfan <ikrdnz@gmail.com>
2019-07-19 21:03:01 +0200

    Node-Week3-Irfan

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,43 @@
-'use strict';
+const express = require('express');
+const app = express();
+const myFunctions = require('./myFunctions.js');
+const bodyParser = require('body-parser');
+app.use(express.json());
+app.use(bodyParser.json());
+app.use((request, response, next) => {
+  response.setHeader('Content-Type', 'application/json');
+  next();
+});
 
-// TODO: Write the homework code in this file
+//Getting toDoThing with a specific id
+app.get('/todos/:id', (request, response) => {
+  myFunctions.readToDo(request, response);
+});
+
+//Adding new toDoThing
+app.post('/todos', (request, response) => {
+  myFunctions.createToDo(request, response);
+});
+
+//Deleting toDoThing
+app.delete('/todos/:id', (request, response) => {
+  myFunctions.deleteToDo(request, response);
+});
+
+//Clearing All ToDos
+app.delete('/todos', (request, response) => {
+  myFunctions.clearToDos(response);
+});
+
+//Marking Done As True
+app.post('/todos/:id/done', (request, response) => {
+  myFunctions.markAsDoneOrMarkAsNotDone(request, response, true);
+});
+
+//Marking Done As False
+app.delete('/todos/:id/done', (request, response) => {
+  myFunctions.markAsDoneOrMarkAsNotDone(request, response, false);
+});
+
+app.listen(3000);
+console.log('Port Number: 3000 is listening.');
---
+++ week3/homework/src/myFunctions.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+function createToDo(request, response) {
+  fs.readFile('./todos.json', 'utf8', (error, contents) => {
+    if (error) {
+      response.status(500).send('Whats wrong with you');
+    } else {
+      const updatedContent = JSON.parse(contents);
+      updatedContent.push(request.body);
+      const updatedNewContent = JSON.stringify(updatedContent);
+      fs.writeFile('./todos.json', updatedNewContent, err => {
+        if (err) throw err;
+      });
+      response.send(updatedNewContent);
+    }
+  });
+}
+
+function readToDo(request, response) {
+  fs.readFile('./todos.json', 'utf8', (error, file) => {
+    if (error) {
+      response.status(500).send('There is no toDoList file.');
+      return 'An Error Occured';
+    } else {
+      const id = request.params.id;
+      response.json(JSON.parse(file)[id - 1]);
+    }
+  });
+}
+
+function deleteToDo(request, response) {
+  fs.readFile('./todos.json', 'utf8', (error, contents) => {
+    if (error) {
+      response.status(500).send('Whats wrong with you');
+    } else {
+      const updatedContent = JSON.parse(contents);
+      updatedContent.splice(request.params.id - 1, 1);
+      const updatedNewContent = JSON.stringify(updatedContent);
+      fs.writeFile('./todos.json', updatedNewContent, err => {
+        if (err) throw err;
+        console.log(request.params.id, 'is removed from your toDoList');
+      });
+      response.send(updatedNewContent);
+    }
+  });
+}
+
+function clearToDos(response) {
+  fs.readFile('./todos.json', 'utf8', error => {
+    if (error) {
+      console.log('File not found');
+      response.status(500).send('File not found');
+    } else {
+      fs.truncate('./todos.json', 0, function() {
+        console.log('done');
+        response.json();
+      });
+    }
+  });
+}
+
+function markAsDoneOrMarkAsNotDone(request, response, trueOrFalse) {
+  fs.readFile('./todos.json', 'utf8', (error, contents) => {
+    if (error) {
+      response.status(500).send('Oops!I cant get it!');
+    } else {
+      const id = request.params.id;
+      let updatedContent = JSON.parse(contents);
+      updatedContent[id - 1].done = trueOrFalse;
+      fs.writeFile('./todos.json', JSON.stringify(updatedContent), error => {
+        if (error) {
+          response.status(500).send('Something went wrong!');
+        } else {
+          response.json(updatedContent);
+        }
+      });
+    }
+  });
+}
+
+module.exports = { createToDo, readToDo, deleteToDo, clearToDos, markAsDoneOrMarkAsNotDone };
