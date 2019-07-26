ab3a01fce51692b784afbd8c4647ed1f0916b65c
zehra kopar <48477070+zehrakopar@users.noreply.github.com>
2019-07-26 17:40:36 +0200

    Update index.js
    I ve add a list function to get all todos. i ve changed the add todo and update todo by using request.body.

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const add = require('./add');
+const list = require('./list');
 const update = require('./update');
 const remove = require('./remove');
 const reset = require('./reset');
@@ -13,19 +14,30 @@ const markAsNotDone = require('./markAsNotDone');
 app.use(express.json());
 
 try {
+  app.get('/todos', (request, response) => {
+    list();
+    response.status(200).json({ message: `the list of todos is requested` });
+  });
+
   app.get('/todos/:id', (request, response) => {
     readTodo(request.params.id);
     response.status(200).json({ message: `todo with the id of ${request.params.id} is requested` });
   });
 
-  app.post('todos/:id/:text', (request, response) => {
-    add(request.params.id, request.params.text);
-    response.status(201).json({ message: `todo with the id of ${request.params.id} is added` });
+  app.post('/todos', (request, response) => {
+    const addTodo = request.body;
+    const id = addTodo.id;
+    const text = addTodo.text;
+    add(id, text);
+    response.status(201).json({ message: `todo with the id of ${id} is added` });
   });
 
-  app.put('todos/:id/:text', (request, response) => {
-    update(request.params.id, request.params.text);
-    response.status(201).json({ message: `todo with the id of ${request.params.id} is updated` });
+  app.put('/todos', (request, response) => {
+    const updateTodo = request.body;
+    const id = updateTodo.id;
+    const text = updateTodo.text;
+    update(id, text);
+    response.status(201).json({ message: `todo with the id of ${id} is updated` });
   });
 
   app.delete('/todos/:id', (request, response) => {
