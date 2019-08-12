d12228629e622997865c1ee3ee9070934706df01
Oguzhan <oguzhanturgut@outlook.com>
2019-08-12 18:43:45 +0200

    Submit week1 homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -1,15 +1,43 @@
 'use strict';
 
 const http = require('http');
-
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
 function createServer(port) {
   let state = 10;
 
+  function sendState(res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ state }));
+  }
+
+  function sendError(res) {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Not found' }));
+  }
+
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        sendState(response);
+        break;
+      case '/add':
+        state++;
+        sendState(response);
+        break;
+      case '/subtract':
+        state--;
+        sendState(response);
+        break;
+      case '/reset':
+        state = 10;
+        sendState(response);
+        break;
+      default:
+        sendError(response);
+        break;
+    }
   });
 
   return server;
