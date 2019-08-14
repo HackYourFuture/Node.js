dd1f72b285b9993bd4aa6f51e02aee7909358c01
Salih <salihsert18@gmail.com>
2019-08-14 12:59:14 +0200

    changed function name

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -2,7 +2,7 @@
 
 const http = require('http');
 
-const getResponse = (responseStatus, response, responseObj) => {
+const sendResponse = (responseStatus, response, responseObj) => {
   response.writeHead(responseStatus, { 'Content-Type': 'application/json' });
   response.end(JSON.stringify(responseObj));
 };
@@ -10,11 +10,11 @@ const getResponse = (responseStatus, response, responseObj) => {
 function createServer(port) {
   let state = 10;
   const server = http.createServer((request, response) => {
-    if (request.url === '/state') getResponse(200, response, { state: state });
-    else if (request.url === '/add') getResponse(200, response, { state: ++state });
-    else if (request.url === '/subtract') getResponse(200, response, { state: --state });
-    else if (request.url === '/reset') getResponse(200, response, { state: (state = 10) });
-    else getResponse(404, response, { error: 'Not found' });
+    if (request.url === '/state') sendResponse(200, response, { state: state });
+    else if (request.url === '/add') sendResponse(200, response, { state: ++state });
+    else if (request.url === '/subtract') sendResponse(200, response, { state: --state });
+    else if (request.url === '/reset') sendResponse(200, response, { state: (state = 10) });
+    else sendResponse(404, response, { error: 'Not found' });
   });
   return server;
 }
