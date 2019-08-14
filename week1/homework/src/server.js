6db873c393493c31c2078454b424d7a29741de6f
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-08-14 11:05:20 +0200

    if els if solution, test passed, but ESLint error.

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -11,6 +11,30 @@ function createServer(port) {
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
     console.log(request.url);
+    if (request.url === `/state`) {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ state }));
+    }
+ else if (request.url === `/add`) {
+      state++;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ state }));
+    }
+ else if (request.url === `/subtract`) {
+      state--;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ state }));
+    }
+ else if (request.url === `/reset`) {
+      state = 10;
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ state }));
+    }
+ else {
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ error: `Not found` }));
+    }
+
     // Finish writing my code.
     response.end();
   });
