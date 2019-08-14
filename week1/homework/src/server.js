37c6d2625ad8bc6d4daf1f0e852b8aa8caca81b6
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-08-14 11:29:07 +0200

    test adding function:  giveResult. Success

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -11,9 +11,14 @@ function createServer(port) {
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
     console.log(request.url);
+    const giveResult = (statusCode, object) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify(object));
+    };
     if (request.url === `/state`) {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      response.write(JSON.stringify({ state }));
+      giveResult(200, { state });
+      // response.writeHead(200, { 'Content-Type': 'application/json' });
+      // response.write(JSON.stringify({ state }));
     }
  else if (request.url === `/add`) {
       state++;
