07aac9679ae2f6b635eef76173862dacadbfc56d
Oguzhan <oguzhanturgut@outlook.com>
2019-08-15 16:35:27 +0200

    Seperate logic into index.js and server module

--- week1/extra-homework/index.js
+++ week1/extra-homework/index.js
@@ -1,33 +1,9 @@
 'use strict';
 
-const http = require('http');
-const sendIndex = require('./sendIndex');
-const sendJS = require('./sendJS');
-const sendImage = require('./sendImage');
-const sendStyle = require('./sendStyle');
+const { createServer } = require('./server');
 
 const PORT = 3000;
 
-function handleRequest(req, res) {
-  console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
-  switch (req.url) {
-    case '/style.css':
-      sendStyle(res);
-      break;
-    case '/browser.js':
-      sendJS(res);
-      break;
-    case '/image.gif':
-      sendImage(res);
-      break;
-    default:
-      sendIndex(res);
-      break;
-  }
-}
-
-const server = http.createServer(handleRequest);
-
-server.listen(PORT, () => {
-  console.log('Server is up on http://localhost:3000/');
+createServer().listen(PORT, () => {
+  console.log(`Server is up on http://localhost:${PORT}/`);
 });
---
+++ week1/extra-homework/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const http = require('http');
+const sendIndex = require('./sendIndex');
+const sendJS = require('./sendJS');
+const sendImage = require('./sendImage');
+const sendStyle = require('./sendStyle');
+
+function createServer(port) {
+  function handleRequest(req, res) {
+    console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
+    switch (req.url) {
+      case '/style.css':
+        sendStyle(res);
+        break;
+      case '/browser.js':
+        sendJS(res);
+        break;
+      case '/image.gif':
+        sendImage(res);
+        break;
+      default:
+        sendIndex(res);
+        break;
+    }
+  }
+
+  return http.createServer(handleRequest);
+}
+
+module.exports = { createServer };
