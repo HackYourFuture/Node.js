786c0be104cf4df86c3fa2325f2efc87c82a043a
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-08-16 14:39:23 +0200

    submission Node Js Hw Week1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,11 +5,41 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
+
 function createServer(port) {
   let state = 10;
+  function handleRequest(response, fileType) {
+    response.writeHead(200, { 'Content-Type': fileType });
+    response.write(JSON.stringify({ state }));
+  }
+
+  function handleError(response, fileType) {
+    response.writeHead(404, { 'Content-Type': fileType });
+    response.write(JSON.stringify({ error: 'Not found' }));
+  }
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        handleRequest(response, 'application/json');
+        break;
+      case '/add':
+        state++;
+        handleRequest(response, 'application/json');
+        break;
+      case '/subtract':
+        state--;
+        handleRequest(response, 'application/json');
+        break;
+      case '/reset':
+        state = 10;
+        handleRequest(response, 'application/json');
+        break;
+      default:
+        handleError(response, 'application/json');
+        break;
+    }
+    response.end();
   });
 
   return server;
