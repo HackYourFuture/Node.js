229c2854ab7815e00e4adb99c8a2010d60d31a57
Laurelinduilwen <ladylaurelin@hotmail.com>
2019-08-17 16:34:55 +0200

    finished node homework week01

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,16 +5,46 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
+
+function responseHandler(response, status, state) {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  if (status === 200) {
+    response.write(JSON.stringify({ state }));
+  }
+ else {
+    response.write(JSON.stringify({ error: state }));
+  }
+}
 function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        responseHandler(response, 200, state);
+        break;
+      case '/add':
+        state++;
+        responseHandler(response, 200, state);
+        break;
+      case '/subtract':
+        state--;
+        responseHandler(response, 200, state);
+        break;
+      case '/reset':
+        state = 10;
+        responseHandler(response, 200, state);
+        break;
+      default:
+        responseHandler(response, 404, `Not found`);
+    }
+    response.end();
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
+  responseHandler
 };
