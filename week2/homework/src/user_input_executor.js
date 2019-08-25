1a3c7c3fa5df746afa5f718d91c86feab151dcd6
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-08-25 21:34:05 +0200

    done dealing with userInput: add

--- week2/homework/src/user_input_executor.js
+++ week2/homework/src/user_input_executor.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const fs = require('fs');
+
 const help = fs.readFileSync('./instruction.txt', `utf8`);
 const list = x => console.log(x);
 const reset = x => console.log(x);
-const add = x => console.log(x);
+const add = function(x) {
+  let list = [];
+  if (fs.existsSync('./tasks_list.json')) {
+    list = JSON.parse(fs.readFileSync('./tasks_list.json', 'utf8'));
+  }
+  list.push([x]);
+  fs.writeFileSync('./tasks_list.json', JSON.stringify(list));
+};
 const remove = x => console.log(x);
 
 module.exports = { help, list, reset, add, remove };
