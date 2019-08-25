13a70faca7b36b40b4f8b38682d7a0254b6c2502
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-08-25 16:19:55 +0200

    add IF Else If statment to  userinput

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+
+const userInput = process.argv;
+if (userInput[2] === `help` && userInput.length === 3) {
+  console.log(process.argv[2]);
+}
+ else if (userInput[2] === `list` && userInput.length === 3) {
+  console.log(process.argv[2]);
+}
+ else if (userInput[2] === `rest` && userInput.length === 3) {
+  console.log(process.argv[2]);
+}
+ else if (userInput[2] === `add` && userInput.length === 4) {
+  console.log(process.argv[3]);
+}
+ else if (userInput[2] === `remove` && userInput.length === 4) {
+  console.log(process.argv[3]);
+}
+ else {
+  console.log(`Pleas type valid command!`);
+}
