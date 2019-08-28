beae2f83f1d18c67fd4ceed80a8a04d3743008bd
Salih <salihsert18@gmail.com>
2019-08-28 13:13:27 +0200

    fix minor issues

--- week2/homework/src/removeToDo.js
+++ week2/homework/src/removeToDo.js
@@ -4,10 +4,10 @@ const { loadToDos, saveToDo, logScreen } = require('./Util');
 
 const removeToDo = async order => {
   const todos = await loadToDos();
-  const keepToDos = todos.filter(todo => todo.index !== order);
-  if (todos.length > keepToDos.length) logScreen('To-do removed!', 'green');
+  const filtered = todos.filter(todo => todo.index !== order);
+  if (todos.length > filtered.length) logScreen('To-do removed!', 'green');
   else logScreen('To-do not found!', 'red');
-  saveToDo(keepToDos);
+  saveToDo(filtered);
 };
 
 module.exports = removeToDo;
