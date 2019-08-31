4130e71cf5eee9e006ce45a60c0aa734723d9de8
Salih <salihsert18@gmail.com>
2019-09-01 00:41:09 +0200

    dont need body-parser

--- week2/homework/src/counter.js
+++ week2/homework/src/counter.js
@@ -3,11 +3,8 @@
 const fs = require('fs').promises;
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 
 const PORT = app.get('port');
@@ -29,7 +26,7 @@ const saveCounters = async counters => {
     return [];
   }
 };
-// This function might be not needed
+
 const listCounters = async () => {
   try {
     return await loadCounters();
