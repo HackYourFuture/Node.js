d05ef1c7f96a35a5546edc2ac4717c30811caedd
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-02 14:01:56 +0200

    use READ Promise with app,get:/todos

--- week3/homework/src/app.js
+++ week3/homework/src/app.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const dataDealer = require('./data/data_dealer');
+const read = dataDealer.read;
 //  handling endpoints/routing with express
 
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 app.all('/', (req, res, next) => {
   res.status(200).json({ testExpress: 'Express is working!' });
@@ -14,7 +18,9 @@ app.post('/todos', (req, res, next) => {
 });
 
 app.get('/todos', (req, res, next) => {
-  res.status(200).json({ testExpress: 'Get method: Express handler.' });
+  read('./data/todolist.json', 'utf8')
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(404).json({ Error: err.message }));
 });
 
 app.put('/todos/:id', (req, res, next) => {
