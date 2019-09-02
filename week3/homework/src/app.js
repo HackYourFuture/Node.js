b93b384e447ad2f6a83a728485a37879c69e5de9
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-02 12:06:16 +0200

    handle endpoints with Express

--- week3/homework/src/app.js
+++ week3/homework/src/app.js
@@ -1,12 +1,34 @@
 'use strict';
 
-//  routing with express
+//  handling endpoints/routing with express
 
 const express = require('express');
 const app = express();
 
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.status(200).json({ testExpress: 'Express is working!' });
+});
+
+app.post('/todos', (req, res, next) => {
+  res.status(200).json({ testExpress: 'POST method: Express handler.' });
+});
+
+app.get('/todos', (req, res, next) => {
+  res.status(200).json({ testExpress: 'Get method: Express handler.' });
+});
+
+app.put('/todos/:id', (req, res, next) => {
+  res.status(200).json({
+    testExpress: 'Put method: Express handler.',
+    id: req.params.id
+  });
+});
+
+app.delete('/todos/:id', (req, res, next) => {
+  res.status(200).json({
+    testExpress: 'Delete method: Express handler.',
+    id: req.params.id
+  });
 });
 
 module.exports = app;
