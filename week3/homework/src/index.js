f36b78c1766ad0d1a2735aa1a07c2a26f5cc8963
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-09-02 11:18:47 +0200

    create HTTP server

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify({ test: 'It is working!' }));
+  res.end();
+});
+
+server.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
