e343a22ac59bd49a215e0c0275c16f5d999663c9
elicarcar <elisabetta.carcar@hotmail.com>
2019-09-07 14:33:04 +0200

    Fixed issues with getting the specific id

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,150 @@
 'use strict';
 
-// TODO: Write the homework code in this file
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const uuidv4 = require('uuid/v4');
+const port = 3041;
+
+app.use(express.json());
+
+async function deleteTodo(req, res) {
+  let todos = await readTodoFiles();
+  let todo = todos.find(todo => todo.id == req.params.id);
+  let indexOfTodo = todos.indexOf(todo);
+  todos.splice(indexOfTodo, 1);
+  todos.push(todo);
+  await saveTodos(todo);
+  res.status = 201;
+  res.send();
+}
+
+async function deleteAllTodos(req, res) {
+  let allData = await readTodoFiles();
+  allData = [];
+  await saveTodos(allData);
+  res.status = 201;
+  res.send();
+}
+
+async function createTodo(req, res) {
+  const todo = validateAndParseTodo(req);
+  const todos = await readTodoFiles();
+  todo.id = uuidv4();
+  todo.done = false;
+  todos.push(todo);
+  await saveTodos(todos);
+  res.status = 201;
+  res.send();
+}
+
+async function updateTodo(req, res) {
+  try {
+    const updateTodo = validateAndParseTodo(req);
+    const todos = await readTodoFiles();
+    const originalTodo = todos.find(todo => todo.id === req.params.id);
+    originalTodo.description = updateTodo.description;
+    await saveTodos(todos);
+    res.send();
+  } catch (err) {
+    new Error(console.log(err.message));
+  }
+}
+
+function readTodoFiles() {
+  return new Promise(function(resolve, reject) {
+    fs.readFile('todo.json', 'utf8', (err, data) => {
+      if (err) reject(err);
+      else resolve(JSON.parse(data));
+    });
+  });
+}
+
+function saveTodos(savePath) {
+  return new Promise(function(resolve, reject) {
+    fs.writeFile('todo.json', JSON.stringify(savePath), 'utf8', err => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
+async function markAsDone(req, res) {
+  try {
+    const todos = await readTodoFiles();
+    const targetedTodo = todos.find(todo => todo.id === req.params.id);
+    targetedTodo.done = true;
+    await saveTodos(todos);
+    res.end('this task is done');
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function markAsNotDone(req, res) {
+  const todos = await readTodoFiles();
+  const todo = todos.find(todo => todo.id === req.params.id);
+  todo.done = false;
+  await saveTodos(todos);
+  res.end('this task is not done');
+}
+
+function validateAndParseTodo(req) {
+  const { todo } = req.body;
+  if (todo == null) throw Error('todo not set');
+
+  if (todo.description != null) todo.description = todo.description.trim();
+
+  if (todo.description == null || todo.description.length === 0)
+    throw new Error('description not set');
+  return todo;
+}
+
+async function readTodos(req, res) {
+  const data = await readTodoFiles();
+  res.send(data);
+}
+
+async function readSpecificTodo(req, res) {
+  const todos = await readTodoFiles();
+  const task = todos.find(todo => todo.id === req.params.id);
+  res.send(task);
+}
+
+app.get('/', (req, res) => {
+  res.send('Yo people!');
+});
+
+app.get('/todos', (req, res) => {
+  readTodos(req, res);
+});
+
+app.get('/todos/:id', (req, res) => {
+  readSpecificTodo(req, res);
+});
+
+app.post('/todos', (req, res) => {
+  createTodo(req, res);
+});
+
+app.post('/todos/:id/done', (req, res) => {
+  markAsDone(req, res);
+});
+
+app.put('/todo/:id', (req, res) => {
+  updateTodo(req, res);
+});
+
+app.delete('/todo/:id', (req, res) => {
+  deleteTodo(req, res);
+});
+
+app.delete('/todos', (req, res) => {
+  deleteAllTodos(req, res);
+});
+
+app.delete('/todos/:id/done', (req, res) => {
+  markAsNotDone(req, res);
+});
+
+app.listen(port, () => console.log(`You are listening to port ${port}`));
