8202508a1a2b633d56660b48533d9ea02174f029
Rabee <rabeerd@gmail.com>
2019-10-09 01:20:37 +0200

    NodeJS week1 homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,11 +10,36 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    const sendRes = (status, object) => {
+      response
+        .writeHead(status, { 'Content-Type': 'application/json' })
+        .write(JSON.stringify(object));
+    };
+    switch (request.url) {
+      case '/state':
+        sendRes(200, { state });
+        break;
+      case '/add':
+        state++;
+        sendRes(200, { state });
+        break;
+      case '/subtract':
+        state--;
+        sendRes(200, { state });
+        break;
+      case '/reset':
+        state = 10;
+        sendRes(200, { state });
+        break;
+      default:
+        sendRes(404, { error: 'Not found' });
+    }
+    response.end();
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
