d53486376a49fd1dab1fd5031af20c3e4b270910
msnmz <snmez.muharrem@gmail.com>
2019-10-09 04:19:48 +0200

    Implements API Endpoints

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,7 +9,34 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    let responseObject = {};
+    switch (request.url) {
+      case '/state': {
+        responseObject.state = state;
+        break;
+      }
+      case '/add': {
+        responseObject.state = ++state;
+        break;
+      }
+      case '/subtract': {
+        responseObject.state = --state;
+        break;
+      }
+      case '/reset': {
+        state = 10;
+        responseObject.state = state;
+        break;
+      }
+      default: {
+        response.statusCode = 404;
+        responseObject.error = 'Not found';
+        break;
+      }
+    }
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(responseObject));
+    response.end();
   });
 
   return server;
