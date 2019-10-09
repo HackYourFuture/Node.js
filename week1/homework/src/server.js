42cce326e512e5eebf56d1e48f37b3129e04c886
ihk2018 <karatasibrahimhalil@gmail.com>
2019-10-09 19:14:39 +0200

    Node.js Week1 Homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,11 +5,37 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
+
 function createServer(port) {
   let state = 10;
 
+  function getResponse(response, state) {
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(state));
+    response.end();
+  }
+
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        getResponse(response, { state });
+        break;
+      case '/add':
+        state++;
+        getResponse(response, { state });
+        break;
+      case '/subtract':
+        state--;
+        getResponse(response, { state });
+        break;
+      case '/reset':
+        state = 10;
+        getResponse(response, { state });
+        break;
+      default:
+        response.statusCode = 404;
+        getResponse(response, { error: 'Not found' });
+    }
   });
 
   return server;
--- week1/homework/test/server.test.js
+++ week1/homework/test/server.test.js
@@ -1,15 +1,13 @@
 'use strict';
 
 /* Using `supertest` purely for simplified request structure but not for its
-* `expect` methods since they don't provide descriptive-enough messages for the
-* purpose of this project
-*/
+ * `expect` methods since they don't provide descriptive-enough messages for the
+ * purpose of this project
+ */
 const request = require('supertest');
-const test    = require('ava');
+const test = require('ava');
 
-const {
-  createServer
-} = require('../src/server');
+const { createServer } = require('../src/server');
 
 const TIMEOUT = 100;
 
@@ -32,14 +30,14 @@ function testCmd(state, ...methods) {
           .get(`/${method}`)
           .timeout(TIMEOUT);
       }
-      catch (err) {
-        return t.fail(
-          `Expected server to respond without an error, got: ${err.message}`
-        );
+ catch (err) {
+        return t.fail(`Expected server to respond without an error, got: ${err.message}`);
       }
 
     t.is(response.status, 200, 'Expected status code to be 200');
-    t.is(response.headers['content-type'], 'application/json',
+    t.is(
+      response.headers['content-type'],
+      'application/json',
       'Expected content type to be application/json'
     );
     t.deepEqual(response.body, { state }, `Expected state to be ${state}`);
@@ -50,36 +48,31 @@ const range = x => new Array(x).fill();
 
 testCmd(10, 'state');
 testCmd(11, 'add');
-testCmd(9,  'subtract');
+testCmd(9, 'subtract');
 testCmd(10, 'reset');
 testCmd(10, 'add', 'reset');
 testCmd(10, 'subtract', 'reset');
 testCmd(12, 'add', 'add', 'state', 'add', 'subtract');
-testCmd(11, 'subtract', 'subtract', 'reset', 'add',  'state', 'subtract', 'add');
+testCmd(11, 'subtract', 'subtract', 'reset', 'add', 'state', 'subtract', 'add');
 testCmd(20, ...range(10).map(() => 'add'), 'state');
-testCmd(0,  ...range(10).map(() => 'subtract'), 'state');
+testCmd(0, ...range(10).map(() => 'subtract'), 'state');
 
-test(
-  'querying undefined URL returns 404 Not Found',
-  async t => {
-    let response;
-    try {
-      response = await request(t.context.server)
-        .get(`/random-bad-url`)
-        .timeout(TIMEOUT);
-    }
-    catch (err) {
-      return t.fail(
-        `Expected server to respond without an error, got: ${err.message}`
-      );
-    }
-
-    t.is(response.status, 404, 'Expected status code to be 404');
-    t.is(response.headers['content-type'], 'application/json',
-      'Expected content type to be application/json'
-    );
-    t.deepEqual(response.body, { error: 'Not found' },
-      'Expected response to be 404 Not found'
-    );
+test('querying undefined URL returns 404 Not Found', async t => {
+  let response;
+  try {
+    response = await request(t.context.server)
+      .get(`/random-bad-url`)
+      .timeout(TIMEOUT);
   }
-);
+ catch (err) {
+    return t.fail(`Expected server to respond without an error, got: ${err.message}`);
+  }
+
+  t.is(response.status, 404, 'Expected status code to be 404');
+  t.is(
+    response.headers['content-type'],
+    'application/json',
+    'Expected content type to be application/json'
+  );
+  t.deepEqual(response.body, { error: 'Not found' }, 'Expected response to be 404 Not found');
+});
