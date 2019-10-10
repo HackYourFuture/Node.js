bae9a60e0278ebc2b32843f15c3d2598a6bd3b4b
ahmthsrvsmz <ahmethusrevsemiz>
2019-10-10 13:51:37 +0200

    homework1

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,16 +5,37 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
+
 function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    let serverResponse = {};
+
+    if (request.url === '/state') {
+      serverResponse.state = state;
+    } else if (request.url === '/add') {
+      state++;
+      serverResponse.state = state;
+    } else if (request.url === '/subtract') {
+      state--;
+      serverResponse.state = state;
+    } else if (request.url === '/reset') {
+      state = 10;
+      serverResponse.state = state;
+    } else {
+      response.statusCode = 404;
+      serverResponse.error = 'Not found';
+    }
+
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(serverResponse));
+    response.end();
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
