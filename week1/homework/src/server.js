c296a598c45257f67efeef2bb2f81ade1c356544
es <esrazengn00@gmail.com>
2019-10-10 17:41:39 +0200

    Add Node.js-Week1-Homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -1,20 +1,84 @@
 'use strict';
 
-const http = require('http');
+{
+  const http = require('http');
 
-/* `createServer` MUST return an instance of `http.Server` otherwise the tests
- * will fail.
- */
-function createServer(port) {
-  let state = 10;
+  /* `createServer` MUST return an instance of `http.Server` otherwise the tests
+   * will fail.
+   */
+  function createServer(port) {
+    let state = 10;
+    const stateObj = {};
 
-  const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
-  });
+    const server = http.createServer((request, response) => {
+      if (request.url === '/state') {
+        stateObj.state = state;
+        response.setHeader('content-type', 'application/json');
+        response.write(`${JSON.stringify(stateObj, null, ' ')}`);
+      } else if (request.url === '/add') {
+        state += 1;
+        stateObj.state = state;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(`${JSON.stringify(stateObj, null, ' ')}`);
+      } else if (request.url === `/subtract`) {
+        state -= 1;
+        stateObj.state = state;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(`${JSON.stringify(stateObj, null, ' ')}`);
+      } else if (request.url === `/reset`) {
+        state = 10;
+        stateObj.state = state;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(`${JSON.stringify(stateObj, null, ' ')}`);
+      } else {
+        const errorObj = {};
+        errorObj.error = 'Not found';
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(`${JSON.stringify(errorObj, null, ' ')}`);
+      }
 
-  return server;
+      // I prefer if-else statement, but I also wanted to try switch statement as a future reference.
+      /* 
+      switch (request.url) {
+        case '/state':
+          stateObj.state = state;
+          response.setHeader('Content-Type', 'application/json');
+          response.write(`${JSON.stringify(stateObj, null, ' ')}`);
+          break;
+        case '/add':
+          state += 1;
+          stateObj.state = state;
+          response.setHeader('Content-Type', 'application/json');
+          response.write(`${JSON.stringify(stateObj, null, ' ')}`);
+          break;
+        case '/subtract':
+          state -= 1;
+          stateObj.state = state;
+          response.setHeader('Content-Type', 'application/json');
+          response.write(`${JSON.stringify(stateObj, null, ' ')}`);
+          break;
+        case '/reset':
+          state = 10;
+          stateObj.state = state;
+          response.setHeader('Content-Type', 'application/json');
+          response.write(`${JSON.stringify(stateObj, null, ' ')}`);
+          break;
+        default:
+          const errorObj = {};
+          errorObj.error = 'Not found';
+          response.statusCode = 404;
+          response.setHeader('Content-Type', 'application/json');
+          response.write(`${JSON.stringify(errorObj, null, ' ')}`);
+      } */
+
+      response.end();
+    });
+
+    return server;
+  }
+
+  module.exports = {
+    createServer,
+  };
 }
-
-module.exports = {
-  createServer
-};
