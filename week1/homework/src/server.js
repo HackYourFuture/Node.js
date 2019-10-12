aff5aab51c4cedbd39e132707c188c1a6d598158
mosleh <mosleh.alnaqeeb@gmail.com>
2019-10-12 17:24:47 +0200

    feedback implementation

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,7 +5,7 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
-function  getResult(status, response, result){
+function  buildResponse(status, response, result) {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.end(JSON.stringify(result));
 }
@@ -14,27 +14,26 @@ function createServer(port) {
   let state = 10;
   const server = http.createServer((request, response) => {
   // TODO: Write your homework code here
-  switch(request.url){
-    case'/state':
-    getResult(200, response, { state: state });
-    break;
-    case'/add':
-    getResult(200, response, { state: ++state});
-    break;
-    case'/subtract':
-    getResult(200, response, { state: --state });
-    break;
-    case'/reset':
-    state = 10;
-    getResult(200, response, { state: state});
-    break;
-    default:
-    getResult(404, response, { error: 'Not found'});
-  }
- });
+    switch (request.url) {
+      case '/state':
+        buildResponse(200, response, { state: state });
+        break;
+      case '/add':
+        buildResponse(200, response, { state: ++state});
+        break;
+      case '/subtract':
+        buildResponse(200, response, { state: --state });
+        break;
+      case '/reset':
+        state = 10;
+        buildResponse(200, response, { state: state});
+        break;
+      default:
+        buildResponse(404, response, { error: 'Not found'});
+    }
+  });
   return server;
 }
-
 module.exports = {
   createServer
 };
