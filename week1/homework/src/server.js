ccf4c21a533d086f6bc467cf500061c23ada4086
faruktercan <trcnfaruk@gmail.com>
2019-10-16 16:59:18 +0200

    Node.js Week1 Homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -2,14 +2,35 @@
 
 const http = require('http');
 
-/* `createServer` MUST return an instance of `http.Server` otherwise the tests
- * will fail.
- */
 function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    let serverResponseObject = {};
+    response.setHeader('Content-Type', 'application/json');
+
+    if (request.url === '/state') {
+      serverResponseObject.state = state;
+    }
+ else if (request.url === '/add') {
+      state++;
+      serverResponseObject.state = state;
+    }
+ else if (request.url === '/subtract') {
+      state--;
+      serverResponseObject.state = state;
+    }
+ else if (request.url === '/reset') {
+      state = 10;
+      serverResponseObject.state = state;
+    }
+ else {
+      response.statusCode = 404;
+      serverResponseObject.error = 'Not found';
+    }
+
+    response.write(JSON.stringify(serverResponseObject));
+    response.end();
   });
 
   return server;
