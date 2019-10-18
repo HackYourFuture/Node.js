cc651e85d267347de5026ffaec2be847e8745216
haha1906 <haha1906@hotmail.com>
2019-10-18 21:21:15 +0200

    Node-week1-HasanAkkus

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,7 +9,32 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    const myResponseObj = {};
+    if (request.url === '/' || request.url === '/state') {
+      myResponseObj.state = state; // { state: 10 }
+    }
+    else if (request.url === '/add') {
+    // add
+      state += 1;
+      myResponseObj.state = state; // {state: 11}
+    }
+    else if (request.url === '/subtract') {
+    // subtract
+      state -= 1;
+      myResponseObj.state = state;
+    }
+    else if (request.url === '/reset') {
+    // reset
+      state = 10;
+      myResponseObj.state = state;
+    }
+    else {
+      myResponseObj.error = 'Not found';
+      response.statusCode = 404;
+    }
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(myResponseObj));
+    response.end();
   });
 
   return server;
