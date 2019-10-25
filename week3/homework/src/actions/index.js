a86ce2f5210db5cf86ff87379950b5eddc47213e
msnmz <snmez.muharrem@gmail.com>
2019-10-26 00:11:58 +0200

    Exports newly created actions

--- week3/homework/src/actions/index.js
+++ week3/homework/src/actions/index.js
@@ -3,7 +3,10 @@
 // CRUD actions
 module.exports = {
   createTodo: require('./createTodo'),
-  readTodos:  require('./readTodos'),
+  readTodos: require('./readTodos'),
   updateTodo: require('./updateTodo'),
-  deleteTodo: require('./deleteTodo')
+  deleteTodo: require('./deleteTodo'),
+  readTodo: require('./readTodo'),
+  clearTodos: require('./clearTodos'),
+  markTodo: require('./markDoneStatus'),
 };
