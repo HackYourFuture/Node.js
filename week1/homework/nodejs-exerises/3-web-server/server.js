737fa108bbd58a0acb11f9af67601c41097ce239
Ömer <omerbkk06@gmail.com>
2020-06-01 12:47:09 +0200

    changes applied on mentor's review, Content-Type added

--- week1/homework/nodejs-exerises/3-web-server/server.js
+++ week1/homework/nodejs-exerises/3-web-server/server.js
@@ -5,21 +5,21 @@ const path = require('path');
 let server = http.createServer(function (req, res) {
       if (req.url === '/') {
         fs.readFile(path.join(__dirname,'index.html'), (err, content) => {
-          res.writeHead(200, 'text/html')
+          res.writeHead(200, {'Content-Type' : 'text/html'})
           res.write(content);
           res.end()
         })
       }
         if (req.url === '/script.js') {
           fs.readFile(path.join(__dirname,'script.js'), (err, content) => {
-            res.writeHead(200, 'text/javascript')
+            res.writeHead(200, {'Content-Type' : 'application/javascript')
             res.write(content);
             res.end()
           })
         }
         if (req.url === '/style.css') {
           fs.readFile(path.join(__dirname,'style.css'), (err, content) => {
-            res.writeHead(200, 'text/css')
+            res.writeHead(200, {'Content-Type' : 'text/css')
             res.write(content);
             res.end()
           })
