b117e19484ed5c91db6dd3c4793559a43b93c16e
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-10-24 15:33:29 +0200

    Finished Exercise 3: Create an HTTP web server

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -2,12 +2,31 @@
  * Exercise 3: Create an HTTP web server
  */
 
-var http = require('http');
+let http = require('http');
+let fs = require('fs');
 
 //create a server
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
+	// YOUR CODE GOES IN HERE
+	//res.writeHead(200, {'Content-Type': 'text/html'});
+  	//res.write('Hello World!');
+  	//res.end();
+	
+	if(req.url === '/'){
+		let file = fs.readFileSync('./index.html');
+		res.writeHead (200, { 'Content-Type': 'text/html' });
+		res.write(file);
+	}
+	else if (req.url === '/index.js'){
+		let file = fs.readFileSync('./index.js');
+		res.writeHead (200, { 'Content-Type': 'application/json' });
+		res.write(file);
+	}
+	else if (req.url === '/style.css'){
+		let file = fs.readFileSync('./style.css');
+		res.writeHead (200, { 'Content-Type': 'text/css' });
+		res.write(file);
+	}
 	res.end(); // Ends the response
 });
 
