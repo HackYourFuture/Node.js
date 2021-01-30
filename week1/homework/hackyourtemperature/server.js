2562f7c8002a92ddf0488b1b9d16fcf98dbe15fc
Vaida <93vaida@gmail.com>
2021-01-30 18:44:02 +0100

    fixed port variable

--- week1/homework/hackyourtemperature/server.js
+++ week1/homework/hackyourtemperature/server.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //express server
 app.get("/", (req, res) => {
