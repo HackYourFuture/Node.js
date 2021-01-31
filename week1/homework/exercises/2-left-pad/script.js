32f12b4f8773b28e763c02bc0833ff1150e3bdb8
Nizami Mursudlu <nizamimursudlu@gmail.com>
2021-01-31 19:27:26 +0100

    changed ex02

--- week1/homework/exercises/2-left-pad/script.js
+++ week1/homework/exercises/2-left-pad/script.js
@@ -13,7 +13,20 @@ let numbers = ["12", "846", "2", "1236"];
 // YOUR CODE GOES HERE
 
 const fn = require("./padLeft.js")
+const leftPad = require('left-pad')
 
 numbers.forEach(number => {
-  console.log(fn.padLeft(number, 8, '_'))
+  console.log(fn.padLeft(number, 5, '_'))
 })
+
+console.log(leftPad('foo', 5))
+// => "  foo"
+
+console.log(leftPad('foobar', 6))
+// => "foobar"
+
+console.log(leftPad(1, 2, '0'))
+// => "01"
+
+console.log(leftPad(17, 5, 0))
+// => "00017"
