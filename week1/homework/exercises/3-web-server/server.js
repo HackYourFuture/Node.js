5c1b6d413d6a410552fc351383696f3e1f10f123
Nizami Mursudlu <nizamimursudlu@gmail.com>
2021-04-10 13:55:21 +0200

    redid homework

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -2,30 +2,27 @@
  * Exercise 3: Create an HTTP web server
  */
 
-var http = require('http');
-var fs = require('fs');
-
-
-
+const http = require('http');
+const fs = require('fs');
 
 //create a server
 let server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-  fs.readFile(
-    './index.html', (err, html) => {
-      if (err) {
-        throw err;
-      }
-      if (req.url === "/") {
-        res.writeHeader(200, { "Content-Type": "text/html" })
-        res.write(html)
-        res.end()
-      }
-      if (req.url === "/script.js") {
-        res.writeHeader(200, { "Content-Type": "text/html" })
-        res.write("Hello world!!!");
-        res.end()
-      }
-    });
+  fs.readFile('./index.html', (err, data) => {
+    if (err) throw err;
+
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+
+    if (req.url === '/index.js') {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end('Hello World!');
+    }
+  });
 });
-server.listen(3000); // The server starts to listen on port 3000
+
+server.listen(5000); // The server starts to listen on port 3000
+
+// PORT 3000 DIDN'T WORK ON MY LAPTOP
