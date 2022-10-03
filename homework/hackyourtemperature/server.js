8003ee1c11597fdd9127c0cf1f8659c7bcdbbfc9
Hikmet Dag <temkihgad@gmail.com>
2022-10-03 09:06:31 +0200

    HW is done

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+app.use(express.json())
+app.get("/", (req, res) => {
+    res.setHeader('Content-Type', "text/plain")
+    res.send('hello from backend to frontend!')
+})
+
+app.post('/weather', (req, res) => {
+
+    const cityName = req.body.cityName;
+    res.setHeader('Content-Type', "text/plain")
+    res.send('cityName')
+})
+
+app.listen(3000, () => {
+    console.log('Running..');
+})
