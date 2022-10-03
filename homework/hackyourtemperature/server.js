bc677b9ecc73f30e66ac80e7828ccda8b4f7cf27
Hikmet Dag <temkihgad@gmail.com>
2022-10-03 09:13:49 +0200

    last editted

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -5,14 +5,11 @@ app.get("/", (req, res) => {
     res.setHeader('Content-Type', "text/plain")
     res.send('hello from backend to frontend!')
 })
-
 app.post('/weather', (req, res) => {
-
     const cityName = req.body.cityName;
     res.setHeader('Content-Type', "text/plain")
     res.send('cityName')
 })
-
 app.listen(3000, () => {
     console.log('Running..');
 })
