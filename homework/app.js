afdc80c74b28d5ef4b3382d13c8c098f3a6cf203
necati <necati.arslan2018@gmail.com>
2022-11-29 23:31:04 +0100

    try chatch delete

--- homework/app.js
+++ homework/app.js
@@ -18,24 +18,21 @@ app.get("/", (req, res) => {
 
 app.post("/weather", async (req, res) => {
   const cityName = req.body.cityName;
-  try {
-    const externalApiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${API_KEY}&q=${cityName}`;
-    const tempWeather = await fetch(externalApiURL).then((response) =>
-      response.json()
-    );
-    const dataCity = tempWeather.main;
-    if (!dataCity) {
-      res.render("index", { cityNotFound: "City you searched is not found" });
-      return;
-    }
 
-    res.render("index", {
-      cityName,
-      dataCity,
-    });
-  } catch (error) {
-    console.log(error);
+  const externalApiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${API_KEY}&q=${cityName}`;
+  const tempWeather = await fetch(externalApiURL).then((response) =>
+    response.json()
+  );
+  const dataCity = tempWeather.main;
+  if (!dataCity) {
+    res.render("index", { cityNotFound: "City you searched is not found" });
+    return;
   }
+
+  res.render("index", {
+    cityName,
+    dataCity,
+  });
 });
 
 export default app;
