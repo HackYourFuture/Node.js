4dcd9d0d0908376cf9385fb407e9973d74db7d5e
Daniil <daniil.kravchenko.92@yandex.ru>
2023-03-11 18:27:09 +0100

    first test

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -15,25 +15,25 @@ app.use(express.json());
 app.post('/weather',async (req, res) => {
   const cityName = req.body.cityName
   try{
-  const cityRes = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`);
-  if(!cityRes.ok){
-    throw new Error('http error 1')
-  }
-  const cityData = await cityRes.json()
-  if(cityData.length === 0){
-    res.send({ weatherText: "City is not found!" })
-  }
-  const lat = cityData[0].lat
-  const lon = cityData[0].lon
+    const cityRes = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`);
+    if(!cityRes.ok){
+      throw new Error('http error 1')
+    }
+    const cityData = await cityRes.json()
+    if(cityData.length === 0){
+      res.send({ weatherText: "City is not found!" })
+    }
+    const lat = cityData[0].lat
+    const lon = cityData[0].lon
 
-  const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-  if(!weatherRes.ok){
-    throw new Error('http error 2')
-  }
-  const weatherData = await weatherRes.json()
-  const {name, main} = weatherData
-  
-  res.send({name, main})} catch(error){
+    const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    if(!weatherRes.ok){
+      throw new Error('http error 2')
+    }
+    const weatherData = await weatherRes.json()
+    const {name, main} = weatherData
+    
+    res.send({name, main})} catch(error){
     console.log(error.message)
   }
 });
