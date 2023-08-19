28bc90f2f3c6eb9f4d1a100f6a2b442286dc826b
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2023-08-19 14:28:18 +0000

    week1 homework

--- hackyouretemperature/server.js
+++ hackyouretemperature/server.js
@@ -1,43 +1,23 @@
 import express from "express";
 import { json } from "express";
-import keys from "./sources/Keys.js";
-import fetch from "node-fetch";
 
 const app = express();
 app.use(json());
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello from backend to frontend!");
 });
 
-app.post("/weather", async (req, res) => {
+app.post("/weather", (req, res) => {
   const cityName = req.body.cityName;
   if (!cityName) {
     console.log("City name is empty.");
     return res.status(400).json({ error: "City name is required." });
   }
-  try {
-    const cityData = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${keys.API_KEY}&units=metric`
-    );
-
-    if (cityData.ok) {
-      const weatherData = await cityData.json();
-      const temp = weatherData.main.temp;
-      res
-        .status(200)
-        .json({ weatherText: `it is ${temp} celsius in ${cityName}` });
-    } else if (cityData.status === 404) {
-      res.status(404).json({ weatherText: `${cityName} City is not found!` });
-    } else {
-      res.status(cityData.status).json({ weatherText: "Server Error" });
-    }
-  } catch (error) {
-    res.status(500).json({ weatherText: "Internal Server Error" });
-  }
+  console.log(cityName);
+  res.status(201).json({ cityName: cityName });
 });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("server is running on port " + 3000);
 });
-export default app;
