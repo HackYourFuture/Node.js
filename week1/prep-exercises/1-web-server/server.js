5439f6290c278cb679d6499b7c3b07e1fe1efc9a
burhan1997 <burhanelaldi97@gmail.com>
2024-01-27 23:31:24 +0100

    prep ex is completed

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,12 +3,40 @@
  */
 
 const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
 
-//create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+const server = http.createServer(async function (req, res) {
+  if (req.url === '/') {
+    // Read and send HTML file
+    try {
+      const htmlContent = await fs.readFile(path.join(__dirname, 'index.html'), 'utf-8');
+      res.setHeader('Content-Type', 'text/html');
+      res.write(htmlContent);
+      res.end();
+    } catch (error) {
+      console.error(error);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    }
+  } else if (req.url === '/index.js') {
+    // Read and send JavaScript file
+    try {
+      const jsContent = await fs.readFile(path.join(__dirname, 'index.js'), 'utf-8');
+      res.setHeader('Content-Type', 'application/javascript');
+      res.write(jsContent);
+      res.end();
+    } catch (error) {
+      console.error(error);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
