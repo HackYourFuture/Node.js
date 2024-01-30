d3d4408aac312e71fb7a73cbf146fa4afea403a3
burhan1997 <burhanelaldi97@gmail.com>
2024-01-30 22:39:15 +0100

    nodejs week1 assignment is completed

---
+++ hackyourtemperature/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.post('/weather', (req, res) => {
+  const { cityName } = req.body;
+  res.send(`You submitted: ${cityName}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+
+
