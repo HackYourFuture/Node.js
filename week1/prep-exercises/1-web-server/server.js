af0501e2cf68f652a5aa333f561a2909151bdab0
xrustik666 <kilorust@gmail.com>
2024-04-06 22:48:13 +0200

    prep exercise is done

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,12 +3,58 @@
  */
 
 const http = require('http');
+const fs = require('fs');
+const PORT = 3000;
 
-//create a server
+// Create a server
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  const url = req.url;
+
+  res.setHeader(
+    "Content-Type", 
+    "text/html; charset=utf-8;"
+  );
+
+  switch(url) {
+    case '/':
+      fs.readFile('./index.html', {}, function(error, data) {
+        if (error) {
+          res.write("Error reading HTML file");
+        } else {
+          res.write(data);
+        }
+        res.end();
+      });
+      break;
+
+    case '/index.js':
+      fs.readFile('./index.js', {}, function(error, data) {
+        if (error) {
+          res.write("Error reading JavaScript file");
+        } else {
+          res.setHeader("Content-Type", "text/javascript");
+          res.write(data);
+        }
+        res.end();
+      });
+      break;
+
+    case '/styles.css':
+      fs.readFile('./styles.css', {}, function(error, data) {
+        if (error) {
+          res.write("Error reading CSS file");
+        } else {
+          res.setHeader("Content-Type", "text/css");
+          res.write(data);
+        }
+        res.end();
+      });
+      break;
+
+    default:
+      res.write("<h1>!!! 404 !!!</h1>");
+      res.end();
+  };
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(PORT);
