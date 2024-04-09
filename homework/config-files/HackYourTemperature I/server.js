ca053e3cfc49c8f8ba476e9b1aad83d9f0b1a203
hhana1-a <hhana1@live.com>
2024-04-09 21:44:08 +0200

    homework

--- homework/config-files/HackYourTemperature I/server.js
+++ homework/config-files/HackYourTemperature I/server.js
@@ -1,12 +1,31 @@
-const express = require('express');
-const server = express ();
+import express from 'express';
+
+const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
 
-server.get ('/', (req, res) => {
-  res.send ('hello from backend to frontend!')
+app.get('/', (req, res) => {
+  res.send(`
+    
+  <form action="/weather" method="post">
+  <label for="cityName">Enter a city name:</label>
+  <input type="text" id="cityName" name="cityName">
+  <button type="submit">Submit</button>
+</form>
+  
+  ` )
+});
+
+
+app.post('/weather', (req, res) => {
+
+ // const { cityName } = req.body;
+
+  const cityName = req.body.cityName;
+  res.setHeader("Content-Type", "text/css");
+  res.send(`City you entered is: ${cityName}.`)
 })
 
-server.listen(PORT)
-
-
+app.listen(PORT);
