ba35f2ae78c6734b2b6c441f23292d2f29ada53e
mustafa durmus <mdurmusnl@gmail.com>
2024-06-29 14:30:02 +0200

    prep-exercise-w1

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,12 +3,42 @@
  */
 
 const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
 
-//create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+let server = http.createServer(async function (req, res) {
+  
+	try{
+		if(req.url === '/'){
+			res.writeHead(200,{'Content-Type':'text/html'});
+			let data	= await fs.readFile(path.join(__dirname,'index.html'));
+			res.end(data);
+
+	}else if(req.url ==='/index.js'){
+		res.writeHead(200,{'Content-Type':'text/javascript'});
+		let data = await fs.readFile(path.join(__dirname,'index.js'));
+		res.end(data);
+
+	}else if (req.url === '/style.css') {
+		// Serve the CSS file
+		const data = await fs.readFile(path.join(__dirname, 'style.css'));
+		res.writeHead(200, {'Content-Type': 'text/css'}); 
+		res.end(data);
+	} 
+	else{
+		res.writeHead(404,{'Content-Type':'text/html'});
+		res.end('<h1>404 Not Found</h1>');
+	
+	}
+}
+catch(err){
+	res.writeHead(500,{'Content-Type':'text/html'});
+	res.end('<h1>500 Internal Server Error</h1>');
+}
+	
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000,()=>{
+	console.log('Server is listening on port 3000');
+
+}); // The server starts to listen on port 3000
