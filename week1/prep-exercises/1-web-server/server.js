835efaf7ac83f9af661a800a86abf5f8c5ac49b4
Aziz <103178186+azizsigar@users.noreply.github.com>
2024-07-08 20:40:55 +0200

    prep w1 done

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -1,14 +1,34 @@
-/**
- * Exercise 3: Create an HTTP web server
- */
-
 const http = require('http');
-
-//create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+const fs = require('fs');
+const path = require('path');
+let server = http.createServer( function (req, res) {
+	if (req.url === '/' || req.url === '/index.html') {
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, function(err, data) {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('500 Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/index.js') {
+        const filePath = path.join(__dirname, 'index.js');
+        fs.readFile(filePath, function(err, data) {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('500 Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+	
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000 , ()=>{console.log('server i listen on port http://localhost:3000');}); // The server starts to listen on port 3000
