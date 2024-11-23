b157239a10d3778695addc79230bd4a5b4005d56
ruba <rubamansour73@gmail.com>
2024-11-23 17:16:13 +0100

    prep-exercis-Node.js

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,12 +3,30 @@
  */
 
 const http = require('http');
-
+var fs = require('fs').promises;
 //create a server
-let server = http.createServer(function (req, res) {
+let server = http.createServer(async function (req, res) {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  try {
+    if (req.url === "/") {
+      const data = await fs.readFile("index.html");
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+	  res.write(data);
+      res.end();
+    } else if (req.url === "/index.js") {
+      const data = await fs.readFile("index.js");
+      res.writeHead(200, { 'Content-Type': 'application/javascript' });
+	  res.write(data);
+      res.end();
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end("Contents you are looking are Not Found");
+    }
+  } catch (err) {
+    console.error(err);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end();
+  }
 });
 
 server.listen(3000); // The server starts to listen on port 3000
