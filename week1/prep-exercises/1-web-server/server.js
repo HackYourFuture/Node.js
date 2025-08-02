2606f1c5ae2e5931d013c0cfdce57cd1e54960ac
VLADISLAV NACHIKOV <vlnachikov@gmail.com>
2025-08-02 22:23:32 +0200

    Finished prep

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -2,13 +2,45 @@
  * Exercise 3: Create an HTTP web server
  */
 
-const http = require('http');
+const http = require("http");
+const fs = require("fs").promises;
+const path = require("path");
 
 //create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+const server = http.createServer(async (req, res) => {
+  try {
+    if (req.url === "/") {
+      const html = await fs.readFile(
+        path.join(__dirname, "index.html"),
+        "utf-8"
+      );
+      res.setHeader("Content-Type", "text/html");
+      res.write(html);
+      res.end();
+    } else if (req.url === "/index.js") {
+      const js = await fs.readFile(path.join(__dirname, "index.js"), "utf-8");
+      res.setHeader("Content-Type", "application/javascript");
+      res.write(js);
+      res.end();
+    } else if (req.url === "/style.css") {
+      const css = await fs.readFile(path.join(__dirname, "style.css"), "utf-8");
+      res.setHeader("Content-Type", "text/css");
+      res.write(css);
+      res.end();
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.write("404 Not Found");
+      res.end();
+    }
+  } catch (err) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/plain");
+    res.write("Internal Server Error");
+    res.end();
+  }
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+}); // The server starts to listen on port 3000
