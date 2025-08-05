3aec070052b6f125d57d4f3cfb0dfaa7f71d4dc6
VLADISLAV NACHIKOV <vlnachikov@gmail.com>
2025-08-05 22:55:45 +0200

    chore: add package.json for 2-left-pad exercise

--- week1/practice-exercises/1-pad-numbers/script.js
+++ week1/practice-exercises/1-pad-numbers/script.js
@@ -1,21 +1,22 @@
-
 /**
- ** Exercise 1: Pad numbers 
- * 
+ ** Exercise 1: Pad numbers
+ *
  * In this file use the padLeft function from padLeft.js to
  * pad the numbers to exactly 5 spaces and log them to the console
- * 
+ *
  * Expected output (replace the underscore with spaces):
- * 
+ *
  *  ___12;
  *  __846;
  *  ____2;
  *  _1236;
- * 
+ *
  * Tips:
  *   where to use `exports` and where `require`?
  */
+import { padLeft } from "./padLeft.js";
 
-let numbers = [ "12", "846", "2", "1236" ];
-
-// YOUR CODE GOES HERE
+let numbers = ["12", "846", "2", "1236"];
+numbers.forEach((num) => {
+  console.log(padLeft(num, 5, " "));
+});
--- week1/practice-exercises/2-left-pad/script.js
+++ week1/practice-exercises/2-left-pad/script.js
@@ -1,13 +1,15 @@
 /**
  ** Exercise 2: To the left, to the left...
- * 
+ *
  * Copy and paste your code from the previous exercise.
  * Replace the function `padLeft` to use
  * this new NPM package called `left-pad` instead then
  * Pad the numbers to 8 characters to confirm that it works correctly
  *
  */
+import padLeft from "left-pad";
 
-let numbers = [ "12", "846", "2", "1236" ];
-
-// YOUR CODE GOES HERE
+let numbers = ["12", "846", "2", "1236"];
+numbers.forEach((num) => {
+  console.log(padLeft(num, 5, " "));
+});
