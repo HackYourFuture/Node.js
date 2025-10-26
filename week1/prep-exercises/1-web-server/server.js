3aedcf8b360791a90896902d7f595bccb62bda51
Sky-A-Fox <Fox13613@gmail.com>
2025-10-26 10:54:41 +0100

    comments update

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 //create a server
 let server = http.createServer(async function (req, res) {
-  // YOUR CODE GOES IN HERE
+  // my code below
       console.log(`Request: ${req.method} ${req.url}`);
   
 	try {
@@ -37,7 +37,7 @@ let server = http.createServer(async function (req, res) {
     }
 });
 
-server.listen(3000, () => { // ДОБАВИЛ функцию обратного вызова
- 	console.log(`Server running at http://localhost:3000`); // ДОБАВИЛ сообщение о запуске
-
+server.listen(3000, () => { // функция обратного вызова // callback function
+ 	console.log(`Server running at http://localhost:3000`); // функция сообщение о запуске	// launch message function
+	
 });
