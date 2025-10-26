52d0c3dbe5a34ac728eea8119b25fb59146524e0
majdjadalhaq <majdhamde1901@gmail.com>
2025-10-26 18:15:31 +0100

    Update server.js

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,12 +3,35 @@
  */
 
 const http = require('http');
+const fs = require('fs')
+const path = require('path')
 
-//create a server
+//create 
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
-});
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' })
+                res.end('Server Error')
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end(data)
+            }
+        })
+    } else if (req.url === '/index.js') {
+        fs.readFile(path.join(__dirname, 'index.js'), (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' })
+                res.end('File not found')
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/javascript' })
+                res.end(data)
+            }
+        })
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' })
+        res.end('Not Found')
+    }
+})
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000); // The server starts on port 3000
